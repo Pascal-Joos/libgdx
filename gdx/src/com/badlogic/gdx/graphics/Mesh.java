4dd983e6ecb42e523596a358a5608aecb5fccd37
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:02:15 +0000

    fix: 25 - PASS_NULLABLE - render(shader, primitiveType, offset, count, autoBind); - passing @Nullable parameter 'shader' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -708,6 +708,9 @@ public class Mesh implements Disposable {
    * @param count number of vertices or indices to use
    */
   public void render(@Nullable ShaderProgram shader, int primitiveType, int offset, int count) {
+    if (shader == null) {
+      throw new GdxRuntimeException("shader must not be null");
+    }
     render(shader, primitiveType, offset, count, autoBind);
   }
 
