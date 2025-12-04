b740321ab696539c4725a8283af0ab1839b8e8c2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 17:42:43 +0000

    fix: 170 - DEREFERENCE_NULLABLE - final int idx = unitsLRU[i]; - dereferenced expression unitsLRU is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultTextureBinder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultTextureBinder.java
@@ -160,9 +160,14 @@ public final class DefaultTextureBinder implements TextureBinder {
   }
 
   private final int bindTextureLRU(final GLTexture texture) {
+    if (unitsLRU == null) {
+      throw new IllegalStateException("LRU method requires unitsLRU to be initialized");
+    }
+    final int[] unitsLRULocal = unitsLRU;
+
     int i;
     for (i = 0; i < count; i++) {
-      final int idx = unitsLRU[i];
+      final int idx = unitsLRULocal[i];
       if (textures[idx] == texture) {
         reused = true;
         break;
@@ -172,12 +177,12 @@ public final class DefaultTextureBinder implements TextureBinder {
       }
     }
     if (i >= count) i = count - 1;
-    final int idx = unitsLRU[i];
+    final int idx = unitsLRULocal[i];
     while (i > 0) {
-      unitsLRU[i] = unitsLRU[i - 1];
+      unitsLRULocal[i] = unitsLRULocal[i - 1];
       i--;
     }
-    unitsLRU[0] = idx;
+    unitsLRULocal[0] = idx;
     if (!reused) {
       textures[idx] = texture;
       texture.bind(offset + idx);
