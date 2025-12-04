1d5e1907cb7c81699c076b76ab188cf13142fffb
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:37:50 +0000

    fix: 225 - FIELD_NO_INIT - private @Null Color color; - @NonNull field color not initialized

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/ColorAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/ColorAction.java
@@ -28,7 +28,7 @@ import com.badlogic.gdx.utils.Null;
  */
 public class ColorAction extends TemporalAction {
   private float startR, startG, startB, startA;
-  private @Null Color color;
+  private @Null Color color = null;
   private final Color end = new Color();
 
   protected void begin() {
