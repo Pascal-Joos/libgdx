550184b6c8d60a12935d7b3834d11618e1ddcb32
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 23:11:52 +0000

    fix: 338 - METHOD_NO_INIT - public TextButton(@Null String text, TextButtonStyle style) { - initializer method does not guarantee @NonNull field style (line 35) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextButton.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextButton.java
@@ -46,6 +46,7 @@ public class TextButton extends Button {
 
   public TextButton(@Null String text, TextButtonStyle style) {
     super();
+    this.style = style;
     setStyle(style);
     label = newLabel(text, new LabelStyle(style.font, style.fontColor));
     label.setAlignment(Align.center);
