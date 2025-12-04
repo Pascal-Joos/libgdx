718c0dfe412eac5a3d78dcf1bdcc2cf85a5bb39b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 23:34:28 +0000

    fix: 349 - METHOD_NO_INIT - public Touchpad(float deadzoneRadius, TouchpadStyle style) { - initializer method does not guarantee @NonNull field style (line 45) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Touchpad.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Touchpad.java
@@ -74,11 +74,13 @@ public class Touchpad extends Widget {
    */
   public Touchpad(float deadzoneRadius, TouchpadStyle style) {
     if (deadzoneRadius < 0) throw new IllegalArgumentException("deadzoneRadius must be > 0");
+    if (style == null) throw new IllegalArgumentException("style cannot be null");
     this.deadzoneRadius = deadzoneRadius;
+    this.style = style;
 
     knobPosition.set(getWidth() / 2f, getHeight() / 2f);
 
-    setStyle(style);
+    invalidateHierarchy();
     setSize(getPrefWidth(), getPrefHeight());
 
     addListener(
