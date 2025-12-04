cec3d6194c83e0e2b6c8ca3fa9f93261327c5579
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:44:19 +0000

    Revert "fix: 145 - DEREFERENCE_NULLABLE - AspectTextureRegion region = regions.get((int) (lifeChannel.data[l] * (regions.size - 1))); - dereferenced expression lifeChannel is @Nullable"
    This reverts commit 5345003fee69d15161ab518e524541e19f8c2095.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
@@ -139,13 +139,11 @@ public abstract class RegionInfluencer extends Influencer {
 
     @Override
     public void update() {
-      if (lifeChannel == null) return;
-      final int lifeStrideSize = lifeChannel.strideSize;
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               c = controller.particles.size * regionChannel.strideSize;
           i < c;
-          i += regionChannel.strideSize, l += lifeStrideSize) {
+          i += regionChannel.strideSize, l += lifeChannel.strideSize) {
         AspectTextureRegion region = regions.get((int) (lifeChannel.data[l] * (regions.size - 1)));
         regionChannel.data[i + ParticleChannels.UOffset] = region.u;
         regionChannel.data[i + ParticleChannels.VOffset] = region.v;
