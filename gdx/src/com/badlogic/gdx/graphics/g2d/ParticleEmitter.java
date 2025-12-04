a359d8b44e99b9fca39f85ae36f5ac821f79c697
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:43:56 +0000

    Revert "fix: 42 - RETURN_NULLABLE - return name; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 174d8beda03016600bbce6d0a6ccd21d3a1c41b7.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -69,7 +69,7 @@ public class ParticleEmitter {
   private Particle[] particles;
   private int minParticleCount, maxParticleCount = 4;
   private float x, y;
-  private String name = "";
+  @Nullable private String name;
   private Array<String> imagePaths;
   private int activeCount;
   private boolean[] active;
