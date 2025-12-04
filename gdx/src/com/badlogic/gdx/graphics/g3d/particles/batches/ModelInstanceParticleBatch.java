e81b5233ec98d52653add5abb7ce9aa5d8ef25e9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:57:33 +0000

    Revert "fix: 150 - FIELD_NO_INIT - public ObjectChannel<ModelInstance> modelInstanceChannel; - @NonNull field modelInstanceChannel not initialized"
    This reverts commit 188a33ba64f088395724c9c1dbb740ba9c5f14ba.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/ModelInstanceParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/ModelInstanceParticleBatch.java
@@ -38,7 +38,6 @@ public class ModelInstanceParticleBatch
   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool) {
     for (ModelInstanceControllerRenderData data : controllersRenderData) {
       for (int i = 0, count = data.controller.particles.size; i < count; ++i) {
-        if (data.modelInstanceChannel == null) continue;
         data.modelInstanceChannel.data[i].getRenderables(renderables, pool);
       }
     }
