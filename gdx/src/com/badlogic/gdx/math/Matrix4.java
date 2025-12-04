1318a20e66cad2c49d682449f144c92a4ed3249e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 18:48:05 +0000

    Revert "fix: 200 - DEREFERENCE_NULLABLE - return set(matrix.val); - dereferenced expression matrix is @Nullable"
    This reverts commit 0d7aa402979a4c6bb301848e664f051e3d158638.

--- gdx/src/com/badlogic/gdx/math/Matrix4.java
+++ gdx/src/com/badlogic/gdx/math/Matrix4.java
@@ -192,7 +192,6 @@ public class Matrix4 implements Serializable {
    * @return This matrix for the purpose of chaining methods together.
    */
   public Matrix4 set(@Nullable Matrix4 matrix) {
-    if (matrix == null) return idt();
     return set(matrix.val);
   }
 
