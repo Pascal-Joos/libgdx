6e9a6447122b518cd2d98f9bc1b11e60df1c496d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 18:18:26 +0000

    Revert "fix: 185 - METHOD_NO_INIT - public GLFrameBufferBuilder(int width, int height) { - initializer method does not guarantee @NonNull fields stencilRenderBufferSpec (line 525), depthRenderBufferSpec (line 526), packedStencilDepthRenderBufferSpec (line 527) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit f661d5fd50fc0ad26b1fbabc075914981b1991a8.

--- gdx/src/com/badlogic/gdx/graphics/glutils/GLFrameBuffer.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/GLFrameBuffer.java
@@ -33,7 +33,6 @@ import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Nullable;
 
 /**
  * Encapsulates OpenGL ES 2.0 frame buffer objects. This is a simple helper class which should cover
@@ -523,9 +522,9 @@ public abstract class GLFrameBuffer<T extends GLTexture> implements Disposable {
     protected Array<FrameBufferTextureAttachmentSpec> textureAttachmentSpecs =
         new Array<FrameBufferTextureAttachmentSpec>();
 
-    @Nullable protected FrameBufferRenderBufferAttachmentSpec stencilRenderBufferSpec;
-    @Nullable protected FrameBufferRenderBufferAttachmentSpec depthRenderBufferSpec;
-    @Nullable protected FrameBufferRenderBufferAttachmentSpec packedStencilDepthRenderBufferSpec;
+    protected FrameBufferRenderBufferAttachmentSpec stencilRenderBufferSpec;
+    protected FrameBufferRenderBufferAttachmentSpec depthRenderBufferSpec;
+    protected FrameBufferRenderBufferAttachmentSpec packedStencilDepthRenderBufferSpec;
 
     protected boolean hasStencilRenderBuffer;
     protected boolean hasDepthRenderBuffer;
