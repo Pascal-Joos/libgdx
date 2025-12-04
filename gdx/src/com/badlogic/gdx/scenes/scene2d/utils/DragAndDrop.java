1efb3eb1babda03706696b4e9c717c5ea5809c72
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 23:58:05 +0000

    Revert "fix: 362 - DEREFERENCE_NULLABLE - actor.getStage() == null; // Only remove later if not already in the stage now. - dereferenced expression actor is @Nullable"
    This reverts commit 41abf016749e0bc6c40c768f638fe6f36ee32f60.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/DragAndDrop.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/DragAndDrop.java
@@ -132,13 +132,9 @@ public class DragAndDrop {
             if (actor != oldDragActor) {
               if (oldDragActor != null && removeDragActor) oldDragActor.remove();
               dragActor = actor;
-              if (actor != null) {
-                removeDragActor =
-                    actor.getStage() == null; // Only remove later if not already in the stage now.
-                if (removeDragActor) stage.addActor(actor);
-              } else {
-                removeDragActor = false;
-              }
+              removeDragActor =
+                  actor.getStage() == null; // Only remove later if not already in the stage now.
+              if (removeDragActor) stage.addActor(actor);
             }
             if (actor == null) return;
 
@@ -169,7 +165,7 @@ public class DragAndDrop {
               isValidTarget = target.drag(source, payload, tmpVector.x, tmpVector.y, pointer);
             }
             if (dragActor != null && removeDragActor) dragActor.remove();
-            if (isValidTarget && target != null) {
+            if (isValidTarget) {
               float stageX = event.getStageX() + touchOffsetX,
                   stageY = event.getStageY() + touchOffsetY;
               target.actor.stageToLocalCoordinates(tmpVector.set(stageX, stageY));
