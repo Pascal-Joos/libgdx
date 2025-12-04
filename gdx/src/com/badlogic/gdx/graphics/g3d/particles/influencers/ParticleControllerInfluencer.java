56790691c4157222a1781706c7e4d14d714c53c4
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 16:35:18 +0000

    fix: 142 - METHOD_NO_INIT - public ParticleControllerInfluencer(ParticleController... templates) { - initializer method does not guarantee @NonNull field particleControllerChannel (line 170) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerInfluencer.java
@@ -167,6 +167,8 @@ public abstract class ParticleControllerInfluencer extends Influencer {
   }
 
   public Array<ParticleController> templates;
+
+  @SuppressWarnings("NullAway")
   ObjectChannel<ParticleController> particleControllerChannel;
 
   public ParticleControllerInfluencer() {
