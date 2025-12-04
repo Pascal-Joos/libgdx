76b465d0f2c5bf9eb20cd60f37b20b8475335200
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 20:15:47 +0000

    Revert "fix: 244 - ASSIGN_FIELD_NULLABLE - spaceRight = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 60b957a8a1d7c09f315653ac41f6e3e8278cdab9.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -30,8 +30,7 @@ public class Cell<T extends Actor> implements Poolable {
   Value minWidth, minHeight;
   Value prefWidth, prefHeight;
   Value maxWidth, maxHeight;
-  Value spaceTop, spaceLeft, spaceBottom;
-  @Nullable Value spaceRight;
+  Value spaceTop, spaceLeft, spaceBottom, spaceRight;
   Value padTop, padLeft, padBottom, padRight;
   @Nullable Float fillX, fillY;
   @Nullable Integer align;
@@ -990,6 +989,8 @@ public class Cell<T extends Actor> implements Poolable {
     spaceTop = null;
     spaceLeft = null;
     spaceBottom = null;
+    spaceRight = null;
+    padTop = null;
     padLeft = null;
     padBottom = null;
     padRight = null;
