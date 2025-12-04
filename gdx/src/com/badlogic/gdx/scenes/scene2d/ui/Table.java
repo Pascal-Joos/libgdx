4759284ae75c65bee0685ff69b9ba82956198072
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:57:06 +0000

    fix: 329 - PASS_NULLABLE - float maxWidth = c.maxWidth.get(c.actor); - passing @Nullable parameter 'c.actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1195,7 +1195,7 @@ public class Table extends WidgetGroup {
       float fillX = c.fillX, fillY = c.fillY;
       if (fillX > 0) {
         c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor));
-        float maxWidth = c.maxWidth.get(c.actor);
+        float maxWidth = c.maxWidth.get(c.actor != null ? c.actor : this);
         if (maxWidth > 0) c.actorWidth = Math.min(c.actorWidth, maxWidth);
       }
       if (fillY > 0) {
