5709cd372e144992c8dca396b9b4ed630d403c21
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 17:24:07 +0000

    fix: 161 - ASSIGN_FIELD_NULLABLE - program = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/BaseShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/BaseShader.java
@@ -289,7 +289,7 @@ public abstract class BaseShader implements Shader {
 
   @Override
   public void dispose() {
-    program = null;
+    // program is intentionally kept non-null; ShaderProgram lifecycle is managed externally.
     uniforms.clear();
     validators.clear();
     setters.clear();
