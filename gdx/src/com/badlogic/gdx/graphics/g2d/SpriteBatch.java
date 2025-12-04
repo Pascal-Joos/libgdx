abcf8741a143d3add04c96d85c638b8f2ad94f62
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 13:02:55 +0000

    Revert "fix: 51 - DEREFERENCE_NULLABLE - lastTexture.bind(); - dereferenced expression lastTexture is @Nullable"
    This reverts commit f59c79dd45bc474f7179a83082639580fad14070.

--- gdx/src/com/badlogic/gdx/graphics/g2d/SpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/SpriteBatch.java
@@ -1042,7 +1042,6 @@ public class SpriteBatch implements Batch {
     if (spritesInBatch > maxSpritesInBatch) maxSpritesInBatch = spritesInBatch;
     int count = spritesInBatch * 6;
 
-    if (lastTexture == null) return;
     lastTexture.bind();
     Mesh mesh = this.mesh;
     mesh.setVertices(vertices, 0, idx);
