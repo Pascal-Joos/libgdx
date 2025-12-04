f10c4b804ccc092d379604076e74e549e0d4d01e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:35:31 +0000

    Revert "fix: 38 - METHOD_NO_INIT - public Animation(float frameDuration, T... keyFrames) { - initializer method does not guarantee @NonNull field keyFrames (line 53) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 1cbab9e8125a9c0e84b302be99c8ccbe786f562d.

--- gdx/src/com/badlogic/gdx/graphics/g2d/Animation.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/Animation.java
@@ -96,12 +96,7 @@ public class Animation<T> {
    */
   public Animation(float frameDuration, T... keyFrames) {
     this.frameDuration = frameDuration;
-    if (keyFrames == null) {
-      this.keyFrames = (T[]) new Object[0];
-    } else {
-      this.keyFrames = keyFrames;
-    }
-    this.animationDuration = this.keyFrames.length * frameDuration;
+    setKeyFrames(keyFrames);
   }
 
   /**
