f4d05492e00381dfa3112f954dc80e9f4fc6bec9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 21:22:27 +0000

    fix: 282 - METHOD_NO_INIT - public List(ListStyle style) { - initializer method does not guarantee @NonNull field style (line 53) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/List.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/List.java
@@ -36,6 +36,7 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
+import java.util.Objects;
 import javax.annotation.Nullable;
 
 /**
@@ -70,6 +71,7 @@ public class List<T> extends Widget implements Cullable {
   }
 
   public List(ListStyle style) {
+    Objects.requireNonNull(style, "style cannot be null");
     selection.setActor(this);
     selection.setRequired(true);
 
