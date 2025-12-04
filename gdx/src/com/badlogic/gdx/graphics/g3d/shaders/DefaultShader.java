50a4bdb443bc3f65f81df8f99ffa1ed9e13304c7
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 17:29:29 +0000

    Revert "fix: 164 - ASSIGN_FIELD_NULLABLE - this.program = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit c8cc7027e31e854645c136b2221ce644ec813d48.

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
@@ -847,6 +847,7 @@ public class DefaultShader extends BaseShader {
   @Override
   public void init() {
     final ShaderProgram program = this.program;
+    this.program = null;
     init(program, renderable);
     renderable = null;
 
