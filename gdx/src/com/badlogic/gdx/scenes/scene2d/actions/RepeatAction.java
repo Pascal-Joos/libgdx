216ac3e6b642bde2ffd5e499f7021d7fac3fa95b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:40:47 +0000

    fix: 227 - DEREFERENCE_NULLABLE - if (action.act(delta)) { - dereferenced expression action is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/RepeatAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/RepeatAction.java
@@ -28,12 +28,11 @@ public class RepeatAction extends DelegateAction {
   private boolean finished;
 
   protected boolean delegate(float delta) {
-    if (executedCount == repeatCount) return true;
-    if (action.act(delta)) {
+    if (action != null && action.act(delta)) {
       if (finished) return true;
       if (repeatCount > 0) executedCount++;
       if (executedCount == repeatCount) return true;
-      if (action != null) action.restart();
+      action.restart();
     }
     return false;
   }
