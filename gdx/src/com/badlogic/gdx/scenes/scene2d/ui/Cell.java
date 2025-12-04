a8beed0d88bbfce61929e142aafd2bcf98f3aa54
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 20:20:58 +0000

    Revert "fix: 247 - ASSIGN_FIELD_NULLABLE - padBottom = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 3b72ffb4955769b97a58cd80f83b8af1966b7cde.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -980,8 +980,28 @@ public class Cell<T extends Actor> implements Poolable {
 
   /** Sets all constraint fields to null. */
   void clear() {
-    Cell defaults = defaults();
-    if (defaults != null) set(defaults);
+    minWidth = null;
+    minHeight = null;
+    prefWidth = null;
+    prefHeight = null;
+    maxWidth = null;
+    maxHeight = null;
+    spaceTop = null;
+    spaceLeft = null;
+    spaceBottom = null;
+    spaceRight = null;
+    padTop = null;
+    padLeft = null;
+    padBottom = null;
+    padRight = null;
+    fillX = null;
+    fillY = null;
+    align = null;
+    expandX = null;
+    expandY = null;
+    colspan = null;
+    uniformX = null;
+    uniformY = null;
   }
 
   /**
