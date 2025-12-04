a26f461a631c8b437d9d92ec4ae50c2667e513fd
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 21:46:05 +0000

    Revert "fix: 295 - NONNULL_FIELD_READ_BEFORE_INIT - float[] columnMinWidth = this.columnMinWidth = ensureSize(this.columnMinWidth, columns); - read of @NonNull field columnMinWidth before initialization"
    This reverts commit 1ce2cde096d8b55396330d5a8afc497991716e5e.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -869,7 +869,7 @@ public class Table extends WidgetGroup {
     }
 
     int columns = this.columns, rows = this.rows;
-    float[] columnMinWidth = this.columnMinWidth = ensureSize(null, columns);
+    float[] columnMinWidth = this.columnMinWidth = ensureSize(this.columnMinWidth, columns);
     float[] rowMinHeight = this.rowMinHeight = ensureSize(this.rowMinHeight, rows);
     float[] columnPrefWidth = this.columnPrefWidth = ensureSize(this.columnPrefWidth, columns);
     float[] rowPrefHeight = this.rowPrefHeight = ensureSize(this.rowPrefHeight, rows);
