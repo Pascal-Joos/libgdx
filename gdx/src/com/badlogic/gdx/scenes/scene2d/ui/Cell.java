dd346432d83651e906af25e4be39717ababfc0d5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:56:08 +0000

    fix: 235 - ASSIGN_FIELD_NULLABLE - minWidth = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -980,12 +980,12 @@ public class Cell<T extends Actor> implements Poolable {
 
   /** Sets all constraint fields to null. */
   void clear() {
-    minWidth = null;
-    minHeight = null;
-    prefWidth = null;
-    prefHeight = null;
-    maxWidth = null;
-    maxHeight = null;
+    minWidth = Fixed.valueOf(0);
+    minHeight = Fixed.valueOf(0);
+    prefWidth = Fixed.valueOf(0);
+    prefHeight = Fixed.valueOf(0);
+    maxWidth = Fixed.valueOf(0);
+    maxHeight = Fixed.valueOf(0);
     spaceTop = null;
     spaceLeft = null;
     spaceBottom = null;
