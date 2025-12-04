fc226132755ffea604f3acecf873bb626ccba20b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 13:42:54 +0000

    fix: 69 - DEREFERENCE_NULLABLE - int numVertices = modelMesh.vertices.length / (attributes.vertexSize / 4); - dereferenced expression modelMesh.vertices is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
@@ -265,13 +265,15 @@ public class Model implements Disposable {
     }
     boolean hasIndices = numIndices > 0;
     VertexAttributes attributes = new VertexAttributes(modelMesh.attributes);
-    int numVertices = modelMesh.vertices.length / (attributes.vertexSize / 4);
+    int numVertices =
+        (modelMesh.vertices == null ? 0 : modelMesh.vertices.length) / (attributes.vertexSize / 4);
 
     Mesh mesh = new Mesh(true, numVertices, numIndices, attributes);
     meshes.add(mesh);
     disposables.add(mesh);
 
-    BufferUtils.copy(modelMesh.vertices, mesh.getVerticesBuffer(), modelMesh.vertices.length, 0);
+    if (modelMesh.vertices != null)
+      BufferUtils.copy(modelMesh.vertices, mesh.getVerticesBuffer(), modelMesh.vertices.length, 0);
     int offset = 0;
     ((Buffer) mesh.getIndicesBuffer()).clear();
     for (ModelMeshPart part : modelMesh.parts) {
