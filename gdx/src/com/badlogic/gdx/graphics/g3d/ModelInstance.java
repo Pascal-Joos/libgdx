55e7ea0f19b26078f5bca9c2c2703dca619b381c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:08:25 +0000

    fix: 80 - DEREFERENCE_NULLABLE - this.nodes.add(copy = node.copy()); - dereferenced expression node is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -206,6 +206,9 @@ public class ModelInstance implements RenderableProvider {
     this.model = model;
     this.transform = transform == null ? new Matrix4() : transform;
     Node copy, node = model.getNode(nodeId, recursive);
+    if (node == null) {
+      throw new IllegalArgumentException("Node with id \"" + nodeId + "\" not found in model.");
+    }
     this.nodes.add(copy = node.copy());
     if (mergeTransform) {
       this.transform.mul(parentTransform ? node.globalTransform : node.localTransform);
