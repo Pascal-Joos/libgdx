b370b69b0244de6cd6fb8875ccb06ba1a14f01f2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 21:11:15 +0000

    Revert "fix: 275 - METHOD_NO_INIT - public Container() { - initializer method does not guarantee @NonNull field actor (line 24) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit b7a1066ac0b1b73074a12d5bfc3d6f5ee4968d28.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Container.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Container.java
@@ -21,7 +21,7 @@ import javax.annotation.Nullable;
  * @author Nathan Sweet
  */
 public class Container<T extends Actor> extends WidgetGroup {
-  private @Nullable @Null T actor;
+  private @Null T actor;
   private Value minWidth = Value.minWidth, minHeight = Value.minHeight;
   private Value prefWidth = Value.prefWidth, prefHeight = Value.prefHeight;
   private Value maxWidth = Value.zero, maxHeight = Value.zero;
@@ -189,7 +189,7 @@ public class Container<T extends Actor> extends WidgetGroup {
   /**
    * @return May be null.
    */
-  public @Nullable @Null T getActor() {
+  public @Null T getActor() {
     return actor;
   }
 
@@ -664,7 +664,6 @@ public class Container<T extends Actor> extends WidgetGroup {
   }
 
   public float getMinWidth() {
-    if (actor == null) return padLeft.get(this) + padRight.get(this);
     return minWidth.get(actor) + padLeft.get(this) + padRight.get(this);
   }
 
