0c9cb6529ca3f7e3ac5b4bce14b42e4dbda6cd9f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:22:22 +0000

    Revert "fix: 312 - PASS_NULLABLE - float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a); - passing @Nullable parameter 'a' where @NonNull is required"
    This reverts commit 20de21b83a6df899677f59d2dd92896740b969f9.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -883,7 +883,6 @@ public class Table extends WidgetGroup {
       Cell c = (Cell) cells[i];
       int column = c.column, row = c.row, colspan = c.colspan;
       Actor a = c.actor;
-      Actor nonNullActor = (a != null ? a : this);
 
       // Collect rows that expand and colspan=1 columns that expand.
       if (c.expandY != 0 && expandHeight[row] == 0) expandHeight[row] = c.expandY;
@@ -893,25 +892,21 @@ public class Table extends WidgetGroup {
       // Compute combined padding/spacing for cells.
       // Spacing between actors isn't additive, the larger is used. Also, no spacing around edges.
       c.computedPadLeft =
-          c.padLeft.get(nonNullActor)
-              + (column == 0 ? 0 : Math.max(0, c.spaceLeft.get(nonNullActor) - spaceRightLast));
-      c.computedPadTop = c.padTop.get(nonNullActor);
+          c.padLeft.get(a) + (column == 0 ? 0 : Math.max(0, c.spaceLeft.get(a) - spaceRightLast));
+      c.computedPadTop = c.padTop.get(a);
       if (c.cellAboveIndex != -1) {
         Cell above = (Cell) cells[c.cellAboveIndex];
-        c.computedPadTop +=
-            Math.max(0, c.spaceTop.get(nonNullActor) - above.spaceBottom.get(nonNullActor));
+        c.computedPadTop += Math.max(0, c.spaceTop.get(a) - above.spaceBottom.get(a));
       }
-      float spaceRight = c.spaceRight.get(nonNullActor);
-      c.computedPadRight =
-          c.padRight.get(nonNullActor) + ((column + colspan) == columns ? 0 : spaceRight);
-      c.computedPadBottom =
-          c.padBottom.get(nonNullActor) + (row == rows - 1 ? 0 : c.spaceBottom.get(nonNullActor));
+      float spaceRight = c.spaceRight.get(a);
+      c.computedPadRight = c.padRight.get(a) + ((column + colspan) == columns ? 0 : spaceRight);
+      c.computedPadBottom = c.padBottom.get(a) + (row == rows - 1 ? 0 : c.spaceBottom.get(a));
       spaceRightLast = spaceRight;
 
       // Determine minimum and preferred cell sizes.
-      float prefWidth = c.prefWidth.get(nonNullActor), prefHeight = c.prefHeight.get(nonNullActor);
-      float minWidth = c.minWidth.get(nonNullActor), minHeight = c.minHeight.get(nonNullActor);
-      float maxWidth = c.maxWidth.get(nonNullActor), maxHeight = c.maxHeight.get(nonNullActor);
+      float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
+      float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
+      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
