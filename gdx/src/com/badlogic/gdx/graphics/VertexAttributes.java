75173aab4b1589f4597868a916ac66ae8e662531
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:33:16 +0000

    fix: 37 - METHOD_NO_INIT - public ReadonlyIterable(T[] array) { - initializer method does not guarantee @NonNull fields iterator1 (line 261), iterator2 (line 261) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java
+++ gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java
@@ -258,19 +258,17 @@ public final class VertexAttributes
 
   private static class ReadonlyIterable<T> implements Iterable<T> {
     private final T[] array;
-    private ReadonlyIterator iterator1, iterator2;
+    private final ReadonlyIterator iterator1, iterator2;
 
     public ReadonlyIterable(T[] array) {
       this.array = array;
+      this.iterator1 = new ReadonlyIterator(array);
+      this.iterator2 = new ReadonlyIterator(array);
     }
 
     @Override
     public Iterator<T> iterator() {
       if (Collections.allocateIterators) return new ReadonlyIterator(array);
-      if (iterator1 == null) {
-        iterator1 = new ReadonlyIterator(array);
-        iterator2 = new ReadonlyIterator(array);
-      }
       if (!iterator1.valid) {
         iterator1.index = 0;
         iterator1.valid = true;
