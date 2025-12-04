a3242699c8442a886428d74ad6537d9766e9dc87
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:11:49 +0000

    fix: 81 - DEREFERENCE_NULLABLE - this.transform.mul(node.getParent().globalTransform); - dereferenced expression node.getParent() is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -212,8 +212,9 @@ public class ModelInstance implements RenderableProvider {
       copy.translation.set(0, 0, 0);
       copy.rotation.idt();
       copy.scale.set(1, 1, 1);
-    } else if (parentTransform && copy.hasParent())
-      this.transform.mul(node.getParent().globalTransform);
+    } else if (parentTransform) {
+      this.transform.mul(copy.globalTransform);
+    }
     invalidate();
     copyAnimations(model.animations, shareKeyframes);
     calculateTransforms();
