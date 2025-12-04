d1f6b9ab38075cb1bf161b5ec73b62da4afd76cc
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 23:36:06 +0000

    Revert "fix: 350 - METHOD_NO_INIT - public Tree(TreeStyle style) { - initializer method does not guarantee @NonNull field style (line 54) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit bd9427fee75f756558a08288cf7c9f3899afb9c2.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
@@ -75,7 +75,6 @@ public class Tree<N extends Node, V> extends WidgetGroup {
   }
 
   public Tree(TreeStyle style) {
-    this.style = style;
     selection =
         new Selection<N>() {
           protected void changed() {
