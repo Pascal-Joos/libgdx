283d89e16a0e4664e01aabc30d975cdf239dfc52
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:24:58 +0000

    fix: 218 - RETURN_NULLABLE - return targetActor; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Event.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Event.java
@@ -94,7 +94,7 @@ public class Event implements Poolable {
   }
 
   /** Returns the actor that the event originated from. */
-  public Actor getTarget() {
+  public @Nullable Actor getTarget() {
     return targetActor;
   }
 
