0158e857243c049df802e85b0d78c58246d7e31f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:02:48 +0000

    fix: 78 - PASS_NULLABLE - this(model, (String[]) null); - passing @Nullable parameter '(String[]) null' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -73,7 +73,11 @@ public class ModelInstance implements RenderableProvider {
    * @param model The {@link Model} to create an instance of.
    */
   public ModelInstance(final Model model) {
-    this(model, (String[]) null);
+    this.model = model;
+    this.transform = new Matrix4();
+    copyNodes(model.nodes);
+    copyAnimations(model.animations, defaultShareKeyframes);
+    calculateTransforms();
   }
 
   /**
@@ -222,7 +226,7 @@ public class ModelInstance implements RenderableProvider {
   /**
    * Constructs a new ModelInstance with only the specified nodes and materials of the given model.
    */
-  public ModelInstance(final Model model, final String... rootNodeIds) {
+  public ModelInstance(final Model model, @Nullable final String... rootNodeIds) {
     this(model, null, rootNodeIds);
   }
 
@@ -230,7 +234,7 @@ public class ModelInstance implements RenderableProvider {
    * Constructs a new ModelInstance with only the specified nodes and materials of the given model.
    */
   public ModelInstance(
-      final Model model, @Nullable final Matrix4 transform, final String... rootNodeIds) {
+      final Model model, @Nullable final Matrix4 transform, @Nullable final String... rootNodeIds) {
     this.model = model;
     this.transform = transform == null ? new Matrix4() : transform;
     if (rootNodeIds == null) copyNodes(model.nodes);
