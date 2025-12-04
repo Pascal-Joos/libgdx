bdf71e6daaa1a28aca0ef03d5d917d259c494a57
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 13:54:25 +0000

    fix: 74 - PASS_NULLABLE - currentShader.begin(camera, context); - passing @Nullable parameter 'camera' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
@@ -270,14 +270,16 @@ public class ModelBatch implements Disposable {
    * called after the call to {@link #begin(Camera)} and before the call to {@link #end()}.
    */
   public void flush() {
-    sorter.sort(camera, renderables);
+    if (camera == null) return;
+    final Camera cam = camera;
+    sorter.sort(cam, renderables);
     Shader currentShader = null;
     for (int i = 0; i < renderables.size; i++) {
       final Renderable renderable = renderables.get(i);
       if (currentShader != renderable.shader) {
         if (currentShader != null) currentShader.end();
         currentShader = renderable.shader;
-        currentShader.begin(camera, context);
+        currentShader.begin(cam, context);
       }
       currentShader.render(renderable);
     }
