c16a8524b8bb324e03c6103a0d8b9e53bb8f2de9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 18:03:54 +0000

    Revert "fix: 180 - METHOD_NO_INIT - public ETC1TextureData(FileHandle file, boolean useMipMaps) { - initializer method does not guarantee @NonNull field data (line 31) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 1b807bd4ebcb067bd06f9824d9ce4828a10557cb.

--- gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
@@ -28,7 +28,7 @@ import javax.annotation.Nullable;
 
 public class ETC1TextureData implements TextureData {
   @Nullable FileHandle file;
-  @Nullable ETC1Data data;
+  ETC1Data data;
   boolean useMipMaps;
   int width = 0;
   int height = 0;
@@ -38,7 +38,6 @@ public class ETC1TextureData implements TextureData {
     this(file, false);
   }
 
-  @SuppressWarnings("NullAway.Init")
   public ETC1TextureData(FileHandle file, boolean useMipMaps) {
     this.file = file;
     this.useMipMaps = useMipMaps;
@@ -67,7 +66,6 @@ public class ETC1TextureData implements TextureData {
     if (file != null) {
       data = new ETC1Data(file);
     }
-    if (data == null) throw new GdxRuntimeException("ETC1Data not prepared");
     width = data.width;
     height = data.height;
     isPrepared = true;
@@ -77,7 +75,6 @@ public class ETC1TextureData implements TextureData {
   public void consumeCustomData(int target) {
     if (!isPrepared)
       throw new GdxRuntimeException("Call prepare() before calling consumeCompressedData()");
-    if (data == null) throw new GdxRuntimeException("ETC1Data not prepared");
 
     if (!Gdx.graphics.supportsExtension("GL_OES_compressed_ETC1_RGB8_texture")) {
       Pixmap pixmap = ETC1.decodeImage(data, Format.RGB565);
