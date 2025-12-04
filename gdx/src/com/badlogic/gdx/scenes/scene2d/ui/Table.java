994a6400cfe99e250172dc0bd8604e1cfd3f61d3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:39:43 +0000

    Revert "fix: 321 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 6cfaa3c8833d160ef615e63bfb82102ddbcfcef3.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -61,14 +61,14 @@ public class Table extends WidgetGroup {
           return new Cell();
         }
       };
-  private static float[] columnWeightedWidth, rowWeightedHeight;
+  @Nullable private static float[] columnWeightedWidth, rowWeightedHeight;
   private int columns, rows;
   private boolean implicitEndRow;
 
   private final Array<Cell> cells = new Array(4);
   private final Cell cellDefaults;
   private final Array<Cell> columnDefaults = new Array(2);
-  private Cell rowDefaults;
+  @Nullable private Cell rowDefaults;
 
   private boolean sizeInvalid = true;
   private float[] columnMinWidth, rowMinHeight;
@@ -84,11 +84,11 @@ public class Table extends WidgetGroup {
   int align = Align.center;
 
   Debug debug = Debug.none;
-  Array<DebugRect> debugRects;
+  @Nullable Array<DebugRect> debugRects;
 
-  @Null Drawable background;
+  @Nullable @Null Drawable background;
   private boolean clip;
-  private @Null Skin skin;
+  @Nullable private @Null Skin skin;
   boolean round = true;
 
   public Table() {
@@ -99,7 +99,7 @@ public class Table extends WidgetGroup {
    * Creates a table with a skin, which is required to use {@link #add(CharSequence)} or {@link
    * #add(CharSequence, String)}.
    */
-  public Table(@Null Skin skin) {
+  public Table(@Nullable @Null Skin skin) {
     this.skin = skin;
 
     cellDefaults = obtainCell();
@@ -166,7 +166,7 @@ public class Table extends WidgetGroup {
   /**
    * @param background May be null to clear the background.
    */
-  public void setBackground(@Null Drawable background) {
+  public void setBackground(@Nullable @Null Drawable background) {
     if (this.background == background) return;
     float padTopOld = getPadTop(),
         padLeftOld = getPadLeft(),
@@ -246,7 +246,7 @@ public class Table extends WidgetGroup {
   }
 
   /** Adds a new cell to the table with the specified actor. */
-  public <T extends Actor> Cell<T> add(@Null T actor) {
+  public <T extends Actor> Cell<T> add(@Nullable @Null T actor) {
     Cell<T> cell = obtainCell();
     cell.actor = actor;
 
@@ -468,7 +468,7 @@ public class Table extends WidgetGroup {
   }
 
   /** Returns the cell for the specified actor in this table, or null. */
-  public @javax.annotation.Nullable @Null <T extends Actor> Cell<T> getCell(T actor) {
+  public @Null <T extends Actor> Cell<T> getCell(T actor) {
     if (actor == null) throw new IllegalArgumentException("actor cannot be null.");
     Object[] cells = this.cells.items;
     for (int i = 0, n = this.cells.size; i < n; i++) {
@@ -849,7 +849,7 @@ public class Table extends WidgetGroup {
     return columnPrefWidth[columnIndex];
   }
 
-  private float[] ensureSize(float[] array, int size) {
+  private float[] ensureSize(@Nullable float[] array, int size) {
     if (array == null || array.length < size) return new float[size];
     Arrays.fill(array, 0, size, 0);
     return array;
@@ -1350,7 +1350,7 @@ public class Table extends WidgetGroup {
    */
   public static class DebugRect extends Rectangle {
     static Pool<DebugRect> pool = Pools.get(DebugRect.class);
-    Color color;
+    @Nullable Color color;
   }
 
   /**
