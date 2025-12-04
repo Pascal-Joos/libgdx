6a31713ed8b05a1695302232927764ebffb29364
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:19:45 +0000

    Revert "fix: 32 - PASS_NULLABLE - this(GL20.GL_TEXTURE_2D, Gdx.gl.glGenTexture(), data); - passing @Nullable parameter 'data' where @NonNull is required"
    This reverts commit 6edb656b4d6dd581639908fd1602172740cb5acd.

--- gdx/src/com/badlogic/gdx/graphics/Texture.java
+++ gdx/src/com/badlogic/gdx/graphics/Texture.java
@@ -161,9 +161,8 @@ public class Texture extends GLTexture {
     this(GL20.GL_TEXTURE_2D, Gdx.gl.glGenTexture(), data);
   }
 
-  protected Texture(int glTarget, int glHandle, @Nullable TextureData data) {
+  protected Texture(int glTarget, int glHandle, TextureData data) {
     super(glTarget, glHandle);
-    if (data == null) throw new GdxRuntimeException("TextureData must not be null");
     load(data);
     if (data.isManaged()) addManagedTexture(Gdx.app, this);
   }
