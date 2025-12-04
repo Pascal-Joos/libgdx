ac1af8fbf01c73e67f4739c94d45f7d6d789e335
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 23:55:40 +0000

    Revert "fix: 361 - METHOD_NO_INIT - public ActorGestureListener( - initializer method does not guarantee @NonNull fields actor (line 41), touchDownTarget (line 41) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 680497fcc00a6492a169becc4875c167b8e2d454.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/ActorGestureListener.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/ActorGestureListener.java
@@ -38,7 +38,7 @@ public class ActorGestureListener implements EventListener {
 
   private final GestureDetector detector;
   @Nullable InputEvent event;
-  Actor actor = new Actor(), touchDownTarget = new Actor();
+  Actor actor, touchDownTarget;
 
   /**
    * @see GestureDetector#GestureDetector(com.badlogic.gdx.input.GestureDetector.GestureListener)
