def9b1300a6550ae3009a1f958b30c8bd102a695
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 11:22:39 +0000

    Revert "fix: 9 - DEREFERENCE_NULLABLE - return keyNames.get(keyname, -1); - dereferenced expression keyNames is @Nullable"
    This reverts commit 9425be9437cbeeea98613aea3ebd9c82d21b84c1.

--- gdx/src/com/badlogic/gdx/Input.java
+++ gdx/src/com/badlogic/gdx/Input.java
@@ -637,9 +637,7 @@ public interface Input {
      */
     public static int valueOf(String keyname) {
       if (keyNames == null) initializeKeyNames();
-      ObjectIntMap<String> localKeyNames = keyNames;
-      if (localKeyNames == null) return -1;
-      return localKeyNames.get(keyname, -1);
+      return keyNames.get(keyname, -1);
     }
 
     /** lazily intialized in {@link Keys#valueOf(String)} */
