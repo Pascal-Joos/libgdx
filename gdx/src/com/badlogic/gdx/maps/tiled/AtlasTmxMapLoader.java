ef977bc88decafc7fef8e44b0e1582d0d15e5092
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 18:26:19 +0000

    fix: 189 - DEREFERENCE_NULLABLE - TextureAtlas atlas = atlasResolver.getAtlas(); - dereferenced expression atlasResolver is @Nullable

--- gdx/src/com/badlogic/gdx/maps/tiled/AtlasTmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/AtlasTmxMapLoader.java
@@ -186,7 +186,10 @@ public class AtlasTmxMapLoader
       int imageHeight,
       @Nullable FileHandle image) {
 
-    TextureAtlas atlas = atlasResolver.getAtlas();
+    TextureAtlas atlas =
+        java.util.Objects.requireNonNull(
+                atlasResolver, "atlasResolver must be initialized before addStaticTiles")
+            .getAtlas();
     String regionsName = name;
 
     for (Texture texture : atlas.getTextures()) {
