a32371e1880465947384790ab46f6a0ed19b24ed
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:06:55 +0000

    fix: 209 - DEREFERENCE_NULLABLE - return new NetJavaSocketImpl(server.accept(), hints); - dereferenced expression server is @Nullable

--- gdx/src/com/badlogic/gdx/net/NetJavaServerSocketImpl.java
+++ gdx/src/com/badlogic/gdx/net/NetJavaServerSocketImpl.java
@@ -81,7 +81,11 @@ public class NetJavaServerSocketImpl implements ServerSocket {
   @Override
   public Socket accept(SocketHints hints) {
     try {
-      return new NetJavaSocketImpl(server.accept(), hints);
+      java.net.ServerSocket localServer = server;
+      if (localServer == null) {
+        throw new GdxRuntimeException("Error accepting socket: server socket is disposed.");
+      }
+      return new NetJavaSocketImpl(localServer.accept(), hints);
     } catch (Exception e) {
       throw new GdxRuntimeException("Error accepting socket.", e);
     }
