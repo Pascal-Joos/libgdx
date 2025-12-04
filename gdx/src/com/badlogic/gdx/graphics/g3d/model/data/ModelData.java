9b4259707eadbe2fb03654d3bc78c7871803bc90
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:52:09 +0000

    Revert "fix: 97 - DEREFERENCE_NULLABLE - if (other.id.equals(mesh.id)) { - dereferenced expression other.id is @Nullable"
    This reverts commit bb4a9c5602b38b3f22738b4f78e8a6e00d47a95f.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelData.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelData.java
@@ -38,7 +38,7 @@ public class ModelData {
 
   public void addMesh(ModelMesh mesh) {
     for (ModelMesh other : meshes) {
-      if (java.util.Objects.equals(other.id, mesh.id)) {
+      if (other.id.equals(mesh.id)) {
         throw new GdxRuntimeException("Mesh with id '" + other.id + "' already in model");
       }
     }
