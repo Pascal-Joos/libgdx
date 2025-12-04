5b12715ef3cf060d787312780a74ccf51b87c40b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:17:38 +0000

    Revert "fix: 83 - PASS_NULLABLE - bindPose.keys[j] = getNode(bindPose.keys[j].id); - passing @Nullable parameter 'bindPose.keys[j].id' where @NonNull is required"
    This reverts commit 5476d2c29697a9c5b601ff03efb2c55244916088.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -357,10 +357,7 @@ public class ModelInstance implements RenderableProvider {
       ArrayMap<Node, Matrix4> bindPose = part.invBoneBindTransforms;
       if (bindPose != null) {
         for (int j = 0; j < bindPose.size; ++j) {
-          Node key = bindPose.keys[j];
-          if (key != null && key.id != null) {
-            bindPose.keys[j] = getNode(key.id);
-          }
+          bindPose.keys[j] = getNode(bindPose.keys[j].id);
         }
       }
       if (!materials.contains(part.material, true)) {
