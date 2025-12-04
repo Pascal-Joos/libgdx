f37b3135a0b10ae00a5bc0b551a1c3ffb416ab95
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 23:51:01 +0000

    fix: 358 - DEREFERENCE_NULLABLE - if (clampPosition && windowX + width + amountX > stage.getWidth()) - dereferenced expression stage is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -176,15 +176,23 @@ public class Window extends Table {
             if ((edge & Align.right) != 0) {
               float amountX = x - lastX - width;
               if (width + amountX < minWidth) amountX = minWidth - width;
-              if (clampPosition && windowX + width + amountX > stage.getWidth())
-                amountX = stage.getWidth() - windowX - width;
+              if (clampPosition) {
+                Stage nonNullStage = java.util.Objects.requireNonNull(stage);
+                if (windowX + width + amountX > nonNullStage.getWidth())
+                  amountX = nonNullStage.getWidth() - windowX - width;
+              }
+
               width += amountX;
             }
             if ((edge & Align.top) != 0) {
               float amountY = y - lastY - height;
               if (height + amountY < minHeight) amountY = minHeight - height;
-              if (clampPosition && windowY + height + amountY > stage.getHeight())
-                amountY = stage.getHeight() - windowY - height;
+              if (clampPosition) {
+                Stage nonNullStage = java.util.Objects.requireNonNull(stage);
+                if (windowY + height + amountY > nonNullStage.getHeight())
+                  amountY = nonNullStage.getHeight() - windowY - height;
+              }
+
               height += amountY;
             }
             setBounds(
