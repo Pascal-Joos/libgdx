2ad4645967bac5f2095f37524f22d5f13a386439
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:52:10 +0000

    Revert "fix: 326 - PASS_NULLABLE - float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a); - passing @Nullable parameter 'a' where @NonNull is required"
    This reverts commit 1fdf7f4d024bbe41db66b5feccf6f635ea93fd36.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1089,17 +1089,9 @@ public class Table extends WidgetGroup {
         spannedWeightedWidth += columnWeightedWidth[ii];
       float weightedHeight = rowWeightedHeight[row];
 
-      float prefWidth, prefHeight, minWidth, minHeight, maxWidth, maxHeight;
-      if (a == null) {
-        prefWidth = prefHeight = minWidth = minHeight = maxWidth = maxHeight = 0;
-      } else {
-        prefWidth = c.prefWidth.get(a);
-        prefHeight = c.prefHeight.get(a);
-        minWidth = c.minWidth.get(a);
-        minHeight = c.minHeight.get(a);
-        maxWidth = c.maxWidth.get(a);
-        maxHeight = c.maxHeight.get(a);
-      }
+      float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
+      float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
+      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
