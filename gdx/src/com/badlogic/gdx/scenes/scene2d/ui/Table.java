5a70bda0229215bc9cd5a829469e2e72c0090ad7
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:58:56 +0000

    fix: 330 - PASS_NULLABLE - c.minHeight.get(c.actor)); - passing @Nullable parameter 'c.actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1192,18 +1192,21 @@ public class Table extends WidgetGroup {
 
       currentX += c.computedPadLeft;
 
+      float minWidth = c.actor == null ? 0 : c.minWidth.get(c.actor);
+      float maxWidth = c.actor == null ? 0 : c.maxWidth.get(c.actor);
+      float minHeight = c.actor == null ? 0 : c.minHeight.get(c.actor);
+      float maxHeight = c.actor == null ? 0 : c.maxHeight.get(c.actor);
+
       float fillX = c.fillX, fillY = c.fillY;
       if (fillX > 0) {
         c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor));
-        float maxWidth = c.maxWidth.get(c.actor);
+        // maxWidth precomputed above;
         if (maxWidth > 0) c.actorWidth = Math.min(c.actorWidth, maxWidth);
       }
       if (fillY > 0) {
         c.actorHeight =
-            Math.max(
-                rowHeight[c.row] * fillY - c.computedPadTop - c.computedPadBottom,
-                c.minHeight.get(c.actor));
-        float maxHeight = c.maxHeight.get(c.actor);
+            Math.max(rowHeight[c.row] * fillY - c.computedPadTop - c.computedPadBottom, minHeight);
+        // maxHeight precomputed above;
         if (maxHeight > 0) c.actorHeight = Math.min(c.actorHeight, maxHeight);
       }
 
