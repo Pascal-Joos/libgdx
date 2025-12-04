523ccba189724dc1a6bccaf61876b8dd1caae91d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:39:23 +0000

    fix: 226 - ASSIGN_FIELD_NULLABLE - color = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/ColorAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/ColorAction.java
@@ -53,7 +53,7 @@ public class ColorAction extends TemporalAction {
 
   public void reset() {
     super.reset();
-    color = null;
+    color = target != null ? target.getColor() : new Color();
   }
 
   public @Null Color getColor() {
