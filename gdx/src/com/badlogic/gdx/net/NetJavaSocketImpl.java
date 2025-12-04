d73527e72bb1d354adb911f873d380b83a2b157e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:10:11 +0000

    Revert "fix: 210 - ASSIGN_FIELD_NULLABLE - socket = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 07236bc7bd2902c8b793f5ddbc72bc839a13e0ec.

--- gdx/src/com/badlogic/gdx/net/NetJavaSocketImpl.java
+++ gdx/src/com/badlogic/gdx/net/NetJavaSocketImpl.java
@@ -112,7 +112,7 @@ public class NetJavaSocketImpl implements Socket {
     if (socket != null) {
       try {
         socket.close();
-        // Do not assign null to socket to satisfy NullAway's non-null field contract.
+        socket = null;
       } catch (Exception e) {
         throw new GdxRuntimeException("Error closing socket.", e);
       }
