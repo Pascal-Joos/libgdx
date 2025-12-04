3e31dbb432137ee380cdf476c23156a28e360f96
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:26:22 +0000

    Revert "fix: 86 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field groupStrategy (line 58) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit a6b7c59f92ca89116eb9644962d324ad4b9621f2.

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/DecalBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/DecalBatch.java
@@ -97,9 +97,6 @@ public class DecalBatch implements Disposable {
    */
   @Initializer
   public void initialize(int size) {
-    if (groupStrategy == null) {
-      groupStrategy = new CameraGroupStrategy(new com.badlogic.gdx.graphics.PerspectiveCamera());
-    }
     vertices = new float[size * Decal.SIZE];
 
     Mesh.VertexDataType vertexDataType = Mesh.VertexDataType.VertexArray;
