edf40dc2ca87af6bd36fd604bb94f485a4deb9f4
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:31:47 +0000

    Revert "fix: 317 - PASS_NULLABLE - float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a); - passing @Nullable parameter 'a' where @NonNull is required"
    This reverts commit 954dc6afd3ab6411a619504a22b7affa684fa8a8.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -906,8 +906,7 @@ public class Table extends WidgetGroup {
       // Determine minimum and preferred cell sizes.
       float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
       float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
-      float maxWidth = a == null ? 0 : c.maxWidth.get(a),
-          maxHeight = a == null ? 0 : c.maxHeight.get(a);
+      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
