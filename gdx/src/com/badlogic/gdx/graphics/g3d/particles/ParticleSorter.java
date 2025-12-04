355ce8451e9d4779f4c6ce78be2f9b869a75469f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 15:19:27 +0000

    Revert "fix: 112 - DEREFERENCE_NULLABLE - float[] val = camera.view.val; - dereferenced expression camera is @Nullable"
    This reverts commit 511170c6d8ee3b41d2f7cf7d785358ffcbaff587.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
@@ -57,6 +57,7 @@ public abstract class ParticleSorter {
     private float[] distances;
     private int[] particleIndices, particleOffsets;
     private int currentSize = 0;
+
     @Override
     public void ensureCapacity(int capacity) {
       if (currentSize < capacity) {
@@ -69,9 +70,6 @@ public abstract class ParticleSorter {
 
     @Override
     public <T extends ParticleControllerRenderData> int[] sort(Array<T> renderData) {
-      if (camera == null) {
-        return particleOffsets;
-      }
       float[] val = camera.view.val;
       float cx = val[Matrix4.M20], cy = val[Matrix4.M21], cz = val[Matrix4.M22];
       int count = 0, i = 0;
@@ -120,37 +118,9 @@ public abstract class ParticleSorter {
         // Quick
         float pivot = distances[si];
         int i = si + 1;
-        int j = ei;
-        while (i <= j) {
-          while (i <= ei && distances[i] <= pivot) i++;
-          while (distances[j] > pivot) j--;
-          if (i < j) {
-            tmp = distances[i];
-            distances[i] = distances[j];
-            distances[j] = tmp;
-
-            // Swap indices
-            tmpIndex = particleIndices[i];
-            particleIndices[i] = particleIndices[j];
-            particleIndices[j] = tmpIndex;
-          }
-        }
-
-        // Swap with pivot
-        tmp = distances[si];
-        distances[si] = distances[j];
-        distances[j] = tmp;
-
-        // Swap indices
         particlesPivotIndex = particleIndices[si];
-        particleIndices[si] = particleIndices[j];
-        particleIndices[j] = particlesPivotIndex;
 
-        // Recurse
-        qsort(si, j - 1);
-        qsort(j + 1, ei);
-      }
-    }
+        // partition array
         for (int j = si + 1; j <= ei; j++) {
           if (pivot > distances[j]) {
             if (j > i) {
