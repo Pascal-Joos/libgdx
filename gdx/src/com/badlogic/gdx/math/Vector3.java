07a1faa32ea973ed1b01ca5d82094e6b0e429ced
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 18:58:45 +0000

    Revert "fix: 205 - DEREFERENCE_NULLABLE - return this.set(vector.x, vector.y, vector.z); - dereferenced expression vector is @Nullable"
    This reverts commit 2191bb00760ec105bec63ece2214960e533bc3a7.

--- gdx/src/com/badlogic/gdx/math/Vector3.java
+++ gdx/src/com/badlogic/gdx/math/Vector3.java
@@ -105,7 +105,6 @@ public class Vector3 implements Serializable, Vector<Vector3> {
 
   @Override
   public Vector3 set(@Nullable final Vector3 vector) {
-    if (vector == null) return this;
     return this.set(vector.x, vector.y, vector.z);
   }
 
