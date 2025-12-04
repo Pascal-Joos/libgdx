3a948b1ff84c2d7ad95f025a2179444eb68c940e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 22:35:32 +0000

    fix: 319 - PASS_NULLABLE - prefWidth = c.prefWidth.get(a), - passing @Nullable parameter 'a' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -903,10 +903,17 @@ public class Table extends WidgetGroup {
       c.computedPadBottom = c.padBottom.get(a) + (row == rows - 1 ? 0 : c.spaceBottom.get(a));
       spaceRightLast = spaceRight;
 
-      // Determine minimum and preferred cell sizes.
-      float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
-      float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
-      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
+      float prefWidth, prefHeight, minWidth, minHeight, maxWidth, maxHeight;
+      if (a == null) {
+        prefWidth = prefHeight = minWidth = minHeight = maxWidth = maxHeight = 0f;
+      } else {
+        prefWidth = c.prefWidth.get(a);
+        prefHeight = c.prefHeight.get(a);
+        minWidth = c.minWidth.get(a);
+        minHeight = c.minHeight.get(a);
+        maxWidth = c.maxWidth.get(a);
+        maxHeight = c.maxHeight.get(a);
+      }
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
@@ -983,9 +990,14 @@ public class Table extends WidgetGroup {
       int column = c.column;
 
       Actor a = c.actor;
-      float minWidth = c.minWidth.get(a),
-          prefWidth = c.prefWidth.get(a),
-          maxWidth = c.maxWidth.get(a);
+      float minWidth, prefWidth, maxWidth;
+      if (a == null) {
+        minWidth = prefWidth = maxWidth = 0f;
+      } else {
+        minWidth = c.minWidth.get(a);
+        prefWidth = c.prefWidth.get(a);
+        maxWidth = c.maxWidth.get(a);
+      }
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
       if (round) {
