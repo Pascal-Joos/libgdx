6920dc5d8afa8561669fdd1b939667d99bb93aae
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 19:42:27 +0000

    fix: 228 - DEREFERENCE_NULLABLE - runnable.run(); - dereferenced expression runnable is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/RunnableAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/RunnableAction.java
@@ -43,7 +43,8 @@ public class RunnableAction extends Action {
     Pool pool = getPool();
     setPool(null); // Ensure this action can't be returned to the pool inside the runnable.
     try {
-      runnable.run();
+      Runnable runnable = this.runnable;
+      if (runnable != null) runnable.run();
     } finally {
       setPool(pool);
     }
