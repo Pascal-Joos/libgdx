821c55fa50bc5571ad2085aad7716bd1370c116d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 15:45:31 +0000

    fix: 124 - DEREFERENCE_NULLABLE - vertices[offset + CPU_POSITION_OFFSET] = - dereferenced expression vertices is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
@@ -149,6 +149,11 @@ public class PointSpriteParticleBatch
 
   @Override
   protected void flush(int[] offsets) {
+    float[] localVertices = vertices;
+    if (localVertices == null) {
+      throw new IllegalStateException("vertices must be allocated before flush");
+    }
+
     int tp = 0;
     for (PointSpriteControllerRenderData data : renderData) {
       FloatChannel scaleChannel = data.scaleChannel;
@@ -164,39 +169,40 @@ public class PointSpriteParticleBatch
         int colorOffset = p * colorChannel.strideSize;
         int rotationOffset = p * rotationChannel.strideSize;
 
-        vertices[offset + CPU_POSITION_OFFSET] =
+        localVertices[offset + CPU_POSITION_OFFSET] =
             positionChannel.data[positionOffset + ParticleChannels.XOffset];
-        vertices[offset + CPU_POSITION_OFFSET + 1] =
+        localVertices[offset + CPU_POSITION_OFFSET + 1] =
             positionChannel.data[positionOffset + ParticleChannels.YOffset];
-        vertices[offset + CPU_POSITION_OFFSET + 2] =
+        localVertices[offset + CPU_POSITION_OFFSET + 2] =
             positionChannel.data[positionOffset + ParticleChannels.ZOffset];
-        vertices[offset + CPU_COLOR_OFFSET] =
+        localVertices[offset + CPU_COLOR_OFFSET] =
             colorChannel.data[colorOffset + ParticleChannels.RedOffset];
-        vertices[offset + CPU_COLOR_OFFSET + 1] =
+        localVertices[offset + CPU_COLOR_OFFSET + 1] =
             colorChannel.data[colorOffset + ParticleChannels.GreenOffset];
-        vertices[offset + CPU_COLOR_OFFSET + 2] =
+        localVertices[offset + CPU_COLOR_OFFSET + 2] =
             colorChannel.data[colorOffset + ParticleChannels.BlueOffset];
-        vertices[offset + CPU_COLOR_OFFSET + 3] =
+        localVertices[offset + CPU_COLOR_OFFSET + 3] =
             colorChannel.data[colorOffset + ParticleChannels.AlphaOffset];
-        vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET] =
+        localVertices[offset + CPU_SIZE_AND_ROTATION_OFFSET] =
             scaleChannel.data[p * scaleChannel.strideSize];
-        vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 1] =
+        localVertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 1] =
             rotationChannel.data[rotationOffset + ParticleChannels.CosineOffset];
-        vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 2] =
+        localVertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 2] =
             rotationChannel.data[rotationOffset + ParticleChannels.SineOffset];
-        vertices[offset + CPU_REGION_OFFSET] =
+        localVertices[offset + CPU_REGION_OFFSET] =
             regionChannel.data[regionOffset + ParticleChannels.UOffset];
-        vertices[offset + CPU_REGION_OFFSET + 1] =
+        localVertices[offset + CPU_REGION_OFFSET + 1] =
             regionChannel.data[regionOffset + ParticleChannels.VOffset];
-        vertices[offset + CPU_REGION_OFFSET + 2] =
+        localVertices[offset + CPU_REGION_OFFSET + 2] =
             regionChannel.data[regionOffset + ParticleChannels.U2Offset];
-        vertices[offset + CPU_REGION_OFFSET + 3] =
+        localVertices[offset + CPU_REGION_OFFSET + 3] =
             regionChannel.data[regionOffset + ParticleChannels.V2Offset];
       }
     }
 
     renderable.meshPart.size = bufferedParticlesCount;
-    renderable.meshPart.mesh.setVertices(vertices, 0, bufferedParticlesCount * CPU_VERTEX_SIZE);
+    renderable.meshPart.mesh.setVertices(
+        localVertices, 0, bufferedParticlesCount * CPU_VERTEX_SIZE);
     renderable.meshPart.update();
   }
 
