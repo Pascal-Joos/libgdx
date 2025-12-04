ed1a8ee45688ba582d583254ec391f40b782cae0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 11:17:36 +0000

    Revert "fix: 7 - FIELD_NO_INIT - public static Graphics graphics; - @NonNull static field graphics not initialized"
    This reverts commit 8b2c66d98fe6c2d2b8e298023db6144048a104e7.

--- gdx/src/com/badlogic/gdx/Gdx.java
+++ gdx/src/com/badlogic/gdx/Gdx.java
@@ -31,8 +31,8 @@ import javax.annotation.Nullable;
  * @author mzechner
  */
 public class Gdx {
-  @Nullable public static Application app;
-  @Nullable public static Graphics graphics;
+  public static Application app;
+  public static Graphics graphics;
   public static Audio audio;
   public static Input input;
   public static Files files;
