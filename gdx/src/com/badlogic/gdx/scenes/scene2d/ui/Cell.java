5dd482dee4d7bb8bf15b5a3bfc95e6d52adb60aa
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 20:42:24 +0000

    Revert "fix: 259 - PASS_NULLABLE - return padRight.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required"
    This reverts commit 5c260d2e5b1d1cb439111322b0f5e0f1773ed660.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -895,7 +895,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPadRight() {
-    return actor == null ? 0f : padRight.get(actor);
+    return padRight.get(actor);
   }
 
   /** Returns {@link #getPadLeft()} plus {@link #getPadRight()}. */
