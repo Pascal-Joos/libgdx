414a7ab66b9339beb6c0775497feacfb9325cc94
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 02:24:23 +0000

    fix: 446 - ASSIGN_FIELD_NULLABLE - com.badlogic.gdx.utils.compression.lz.BinTree _matchFinder = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/compression/lzma/Encoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lzma/Encoder.java
@@ -296,7 +296,7 @@ public class Encoder {
   ;
 
   Optimal[] _optimum = new Optimal[kNumOpts];
-  com.badlogic.gdx.utils.compression.lz.BinTree _matchFinder = null;
+  @Nullable com.badlogic.gdx.utils.compression.lz.BinTree _matchFinder = null;
   com.badlogic.gdx.utils.compression.rangecoder.Encoder _rangeEncoder =
       new com.badlogic.gdx.utils.compression.rangecoder.Encoder();
 
