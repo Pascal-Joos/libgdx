7f308e99638cceb602857623a28ab835147fa2e0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 01:01:51 +0000

    fix: 397 - ASSIGN_FIELD_NULLABLE - this.stringValue = stringValue; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1186,8 +1186,9 @@ public class JsonValue implements Iterable<JsonValue> {
   public void set(long value, @Nullable @Null String stringValue) {
     longValue = value;
     doubleValue = value;
-    this.stringValue = stringValue;
+    this.stringValue = stringValue != null ? stringValue : String.valueOf(value);
     type = ValueType.longValue;
+  }
   }
 
   public void set(boolean value) {
