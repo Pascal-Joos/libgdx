52680829b3ffc7bdb00b07ed861b1cf09429a356
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 00:39:46 +0000

    fix: 384 - METHOD_NO_INIT - public IntSet(int initialCapacity, float loadFactor) { - initializer method does not guarantee @NonNull fields iterator1 (line 75), iterator2 (line 75) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/utils/IntSet.java
+++ gdx/src/com/badlogic/gdx/utils/IntSet.java
@@ -72,7 +72,7 @@ public class IntSet {
    */
   protected int mask;
 
-  private transient IntSetIterator iterator1, iterator2;
+  @javax.annotation.Nullable private transient IntSetIterator iterator1, iterator2;
 
   /** Creates a new set with an initial capacity of 51 and a load factor of 0.8. */
   public IntSet() {
