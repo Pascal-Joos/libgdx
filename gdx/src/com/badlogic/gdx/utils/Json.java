fe3561b78e3dad24daba1e82ecea49746f283666
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 00:42:49 +0000

    fix: 386 - METHOD_NO_INIT - public Json(OutputType outputType) { - initializer method does not guarantee @NonNull field writer (line 52) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -46,6 +46,7 @@ import javax.annotation.Nullable;
  *
  * @author Nathan Sweet
  */
+  private static final java.io.Writer NULL_WRITER = new java.io.Writer() {     @Override public void write(char[] cbuf, int off, int len) {}     @Override public void flush() {}     @Override public void close() {}   };
 public class Json {
   private static final boolean debug = false;
 
@@ -68,10 +69,12 @@ public class Json {
   private final Object[] equals1 = {null}, equals2 = {null};
 
   public Json() {
+    this.writer = new JsonWriter(NULL_WRITER);
     outputType = OutputType.minimal;
   }
 
   public Json(OutputType outputType) {
+    this.writer = new JsonWriter(NULL_WRITER);
     this.outputType = outputType;
   }
 
