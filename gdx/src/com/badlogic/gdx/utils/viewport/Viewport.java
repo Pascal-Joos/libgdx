864725b44bfe686fe8f499d0734d4f9337d9cf7a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 02:37:16 +0000

    Revert "fix: 453 - FIELD_NO_INIT - private Camera camera; - @NonNull field camera not initialized"
    This reverts commit 552dc52825137d477e9478188fc3155d9d388a88.

--- gdx/src/com/badlogic/gdx/utils/viewport/Viewport.java
+++ gdx/src/com/badlogic/gdx/utils/viewport/Viewport.java
@@ -20,7 +20,6 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.HdpiUtils;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Rectangle;
@@ -39,10 +38,6 @@ public abstract class Viewport {
   private Camera camera;
   private float worldWidth, worldHeight;
   private int screenX, screenY, screenWidth, screenHeight;
-
-  protected Viewport() {
-    this.camera = new OrthographicCamera();
-  }
 
   private final Vector3 tmp = new Vector3();
 
