06dc46e637ba9ed28581e54f6a8efa055d33bc70
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 00:54:39 +0000

    Revert "fix: 393 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 131f20edae3df5bbdc41c08920fa53ca225db6f6.

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -242,7 +242,7 @@ public class JsonValue implements Iterable<JsonValue> {
    * @return May be null if this value is null.
    * @throws IllegalStateException if this an array or object.
    */
-  public @Nullable String asString() {
+  public @Null String asString() {
     switch (type) {
       case stringValue:
         return stringValue;
