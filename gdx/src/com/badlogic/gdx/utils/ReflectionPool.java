3edd88a0c09ea22e7d747d2109a1e40ba6e67966
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 01:46:16 +0000

    Revert "fix: 421 - PASS_NULLABLE - Constructor constructor = ClassReflection.getDeclaredConstructor(type, (Class[]) null); - passing @Nullable parameter '(Class[]) null' where @NonNull is required"
    This reverts commit b0d3547da5599f2c7c19457f76f5cc5dd04114e8.

--- gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
+++ gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  * Copyright 2011 See AUTHORS file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,12 +19,24 @@ package com.badlogic.gdx.utils;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import com.badlogic.gdx.utils.reflect.Constructor;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
+import javax.annotation.Nullable;
 
+/**
+ * Pool that creates new instances of a type using reflection. The type must have a zero argument
+ * constructor. {@link Constructor#setAccessible(boolean)} will be used if the class and/or
+ * constructor is not visible.
+ *
+ * @author Nathan Sweet
+ */
 public class ReflectionPool<T> extends Pool<T> {
   private final Constructor constructor;
 
   public ReflectionPool(Class<T> type) {
     this(type, 16, Integer.MAX_VALUE);
+  }
+
+  public ReflectionPool(Class<T> type, int initialCapacity) {
+    this(type, initialCapacity, Integer.MAX_VALUE);
   }
 
   public ReflectionPool(Class<T> type, int initialCapacity, int max) {
@@ -35,12 +47,13 @@ public class ReflectionPool<T> extends Pool<T> {
           "Class cannot be created (missing no-arg constructor): " + type.getName());
   }
 
-  private Constructor findConstructor(Class<T> type) {
+  @Nullable
+  private @Null Constructor findConstructor(Class<T> type) {
     try {
       return ClassReflection.getConstructor(type, (Class[]) null);
     } catch (Exception ex1) {
       try {
-        Constructor constructor = ClassReflection.getDeclaredConstructor(type, new Class[0]);
+        Constructor constructor = ClassReflection.getDeclaredConstructor(type, (Class[]) null);
         constructor.setAccessible(true);
         return constructor;
       } catch (ReflectionException ex2) {
