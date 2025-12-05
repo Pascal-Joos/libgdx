95e5a115f8b2d507e8dd39a84ca095a4f11108c8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 01:59:19 +0000

    Revert "fix: 429 - DEREFERENCE_NULLABLE - if (thread.pauseTimeMillis > 0) executeTimeMillis -= timeMillis - thread.pauseTimeMillis; - dereferenced expression thread is @Nullable"
    This reverts commit f2eb4dc1f0d059a02fd382f933512f1cc45b2c69.

--- gdx/src/com/badlogic/gdx/utils/Timer.java
+++ gdx/src/com/badlogic/gdx/utils/Timer.java
@@ -99,9 +99,7 @@ public class Timer {
           task.timer = this;
           long timeMillis = System.nanoTime() / 1000000;
           long executeTimeMillis = timeMillis + (long) (delaySeconds * 1000);
-          TimerThread currentThread = thread();
-          if (currentThread.pauseTimeMillis > 0)
-            executeTimeMillis -= timeMillis - currentThread.pauseTimeMillis;
+          if (thread.pauseTimeMillis > 0) executeTimeMillis -= timeMillis - thread.pauseTimeMillis;
           task.executeTimeMillis = executeTimeMillis;
           task.intervalMillis = (long) (intervalSeconds * 1000);
           task.repeatCount = repeatCount;
