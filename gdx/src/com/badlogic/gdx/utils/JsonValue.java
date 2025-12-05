36471dbcf9b188cdcc33a173e7310ff0112ec19c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 00:58:38 +0000

    fix: 395 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull fields stringValue (line 50), name (line 55) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1175,7 +1175,8 @@ public class JsonValue implements Iterable<JsonValue> {
   public void set(double value, @Nullable @Null String stringValue) {
     doubleValue = value;
     longValue = (long) value;
-    this.stringValue = stringValue;
+    this.stringValue = (stringValue != null ? stringValue : "");
+    if (name == null) name = "";
     type = ValueType.doubleValue;
   }
 
