72bafebd8525af4701a71447f58bc99c9b867666
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 00:48:19 +0000

    fix: 389 - RETURN_NULLABLE - if (jsonValue == null) return defaultValue; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -1124,7 +1124,7 @@ public class Json {
    * @return May be null.
    */
   public @Null <T> T readValue(
-      String name, @Null Class<T> type, @Nullable T defaultValue, JsonValue jsonMap) {
+      String name, @Null Class<T> type, @Null T defaultValue, JsonValue jsonMap) {
     JsonValue jsonValue = jsonMap.get(name);
     if (jsonValue == null) return defaultValue;
     return readValue(type, null, jsonValue);
