cbe81c159dbab2f8fbd0c935170b5a9743af9cd5
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 01:00:42 +0000

    fix: 396 - ASSIGN_FIELD_NULLABLE - stringValue = value; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -47,7 +47,7 @@ public class JsonValue implements Iterable<JsonValue> {
   private ValueType type;
 
   /** May be null. */
-  private String stringValue;
+  @Nullable private String stringValue;
 
   private double doubleValue;
   private long longValue;
@@ -335,23 +335,6 @@ public class JsonValue implements Iterable<JsonValue> {
   }
 
   /**
-   * Returns this value as a boolean.
-   *
-   * @throws IllegalStateException if this an array or object.
-   */
-  public boolean asBoolean() {
-    switch (type) {
-      case stringValue:
-        return stringValue.equalsIgnoreCase("true");
-      case doubleValue:
-        return doubleValue != 0;
-      case longValue:
-        return longValue != 0;
-      case booleanValue:
-        return longValue != 0;
-    }
-    throw new IllegalStateException("Value cannot be converted to boolean: " + type);
-  }
 
   /**
    * Returns this value as a byte.
@@ -399,7 +382,7 @@ public class JsonValue implements Iterable<JsonValue> {
   public char asChar() {
     switch (type) {
       case stringValue:
-        return stringValue.length() == 0 ? 0 : stringValue.charAt(0);
+        return (stringValue == null || stringValue.length() == 0) ? 0 : stringValue.charAt(0);
       case doubleValue:
         return (char) doubleValue;
       case longValue:
@@ -682,7 +665,7 @@ public class JsonValue implements Iterable<JsonValue> {
       char v;
       switch (value.type) {
         case stringValue:
-          v = value.stringValue.length() == 0 ? 0 : value.stringValue.charAt(0);
+          v = (value.stringValue == null || value.stringValue.length() == 0) ? 0 : value.stringValue.charAt(0);
           break;
         case doubleValue:
           v = (char) value.doubleValue;
@@ -1504,3 +1487,22 @@ public class JsonValue implements Iterable<JsonValue> {
     public boolean wrapNumericArrays;
   }
 }
+   * Returns this value as a boolean.
+  /**
+   * Returns this value as a boolean.
+   *
+   * @throws IllegalStateException if this an array or object.
+   */
+  public boolean asBoolean() {
+    switch (type) {
+      case stringValue:
+        return stringValue != null && stringValue.equalsIgnoreCase("true");
+      case doubleValue:
+        return doubleValue != 0;
+      case longValue:
+        return longValue != 0;
+      case booleanValue:
+        return longValue != 0;
+    }
+    throw new IllegalStateException("Value cannot be converted to boolean: " + type);
+  }
