6972d0ff9beb1db14322c18c5bf6b8c6ef711120
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 00:46:09 +0000

    fix: 388 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -1393,7 +1393,12 @@ public class Json {
           "Unable to convert value to required type: " + jsonData + " (" + type.getName() + ")");
     }
 
-    return null;
+    throw new SerializationException(
+        "Unable to convert value to required type: "
+            + jsonData
+            + " ("
+            + (type == null ? "unknown" : type.getName())
+            + ")");
   }
 
   /**
