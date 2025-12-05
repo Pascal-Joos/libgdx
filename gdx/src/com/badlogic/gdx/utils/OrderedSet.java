edbeb0624c7ee1e8dcc2365c31f3a29ad55fcc07
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 01:29:19 +0000

    fix: 411 - DEREFERENCE_NULLABLE - iterator2.reset(); - dereferenced expression iterator2 is @Nullable

--- gdx/src/com/badlogic/gdx/utils/OrderedSet.java
+++ gdx/src/com/badlogic/gdx/utils/OrderedSet.java
@@ -177,6 +177,7 @@ public class OrderedSet<T> extends ObjectSet<T> {
       iterator2.valid = false;
       return iterator1;
     }
+    if (iterator2 == null) iterator2 = new OrderedSetIterator(this);
     iterator2.reset();
     iterator2.valid = true;
     iterator1.valid = false;
