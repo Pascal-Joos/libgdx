7c29c0d77091bc96924100813e5b07d50bcae6ae
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 23:26:11 +0000

    fix: Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java:182     httpRequest = null;

--- gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java
+++ gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java
@@ -49,7 +49,7 @@ public class HttpRequestBuilder {
   /** Will be used for the object serialization in case {@link #jsonContent(Object)} is called. */
   public static Json json = new Json();
 
-  private HttpRequest httpRequest;
+  private HttpRequest httpRequest = Pools.obtain(HttpRequest.class);
 
   /** Initializes the builder and sets it up to build a new {@link HttpRequest} . */
   public HttpRequestBuilder newRequest() {
@@ -179,7 +179,8 @@ public class HttpRequestBuilder {
   public HttpRequest build() {
     validate();
     HttpRequest request = httpRequest;
-    httpRequest = null;
+    httpRequest = Pools.obtain(HttpRequest.class);
+    httpRequest.setTimeOut(defaultTimeout);
     return request;
   }
 
