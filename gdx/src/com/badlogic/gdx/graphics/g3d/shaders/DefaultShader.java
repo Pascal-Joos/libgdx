6c4c779adc081598cafb46f9078a54e515876c51
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 22:17:03 +0000

    fix: Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java:850     this.program = null;

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
@@ -847,7 +847,6 @@ public class DefaultShader extends BaseShader {
   @Override
   public void init() {
     final ShaderProgram program = this.program;
-    this.program = null;
     init(program, renderable);
     renderable = null;
 
