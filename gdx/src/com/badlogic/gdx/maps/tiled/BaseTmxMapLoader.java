cc62422d14672041f66853b3371e00e0a3ef1d2f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 23:02:15 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields root (line 66), map (line 75) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java:77   public BaseTmxMapLoader(FileHandleResolver resolver) {

--- gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
@@ -76,6 +76,8 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters>
 
   public BaseTmxMapLoader(FileHandleResolver resolver) {
     super(resolver);
+    this.root = new Element("map", null);
+    this.map = new TiledMap();
   }
 
   @Override
