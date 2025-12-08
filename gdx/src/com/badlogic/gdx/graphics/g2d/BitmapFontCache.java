51b9c32e09730f4a41071a112265aa24bb7e26eb
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 15:25:38 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field pageGlyphIndices (line 61) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java:74   public BitmapFontCache(BitmapFont font, boolean integer) {

--- gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java
@@ -82,11 +82,8 @@ public class BitmapFontCache {
 
     pageVertices = new float[pageCount][];
     idx = new int[pageCount];
-    if (pageCount > 1) {
-      // Contains the indices of the glyph in the cache as they are added.
-      pageGlyphIndices = new IntArray[pageCount];
-      for (int i = 0, n = pageGlyphIndices.length; i < n; i++) pageGlyphIndices[i] = new IntArray();
-    }
+    pageGlyphIndices = new IntArray[pageCount];
+    for (int i = 0, n = pageGlyphIndices.length; i < n; i++) pageGlyphIndices[i] = new IntArray();
     tempGlyphCount = new int[pageCount];
   }
 
