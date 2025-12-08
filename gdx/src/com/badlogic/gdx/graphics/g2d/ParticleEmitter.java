9791998c4bfdaf9c9e6a0d2e16705a60be31b60f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 15:29:19 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java:752     return name;

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -69,7 +69,7 @@ public class ParticleEmitter {
   private Particle[] particles;
   private int minParticleCount, maxParticleCount = 4;
   private float x, y;
-  @Nullable private String name;
+  private String name = "";
   private Array<String> imagePaths;
   private int activeCount;
   private boolean[] active;
@@ -753,6 +753,8 @@ public class ParticleEmitter {
   }
 
   public void setName(String name) {
+    if (name == null) name = "";
+    if (name == null) name = "";
     this.name = name;
   }
 
