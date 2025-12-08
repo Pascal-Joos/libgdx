f966511ef443e6f84bd771d03c1c4483677f6b57
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 23:16:06 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression matrix is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/math/Matrix4.java:195     return set(matrix.val);

--- gdx/src/com/badlogic/gdx/math/Matrix4.java
+++ gdx/src/com/badlogic/gdx/math/Matrix4.java
@@ -192,6 +192,7 @@ public class Matrix4 implements Serializable {
    * @return This matrix for the purpose of chaining methods together.
    */
   public Matrix4 set(@Nullable Matrix4 matrix) {
+    if (matrix == null) return idt();
     return set(matrix.val);
   }
 
