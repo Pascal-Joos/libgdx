9e6b4f6502e356efe0b7073de3671dee1c45b3b2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 16:18:13 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field texture (line 30) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java:36   public TextureRegion() {}

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
@@ -33,7 +33,9 @@ public class TextureRegion {
   int regionWidth, regionHeight;
 
   /** Constructs a region that cannot be used until a texture and texture coordinates are set. */
-  public TextureRegion() {}
+  public TextureRegion() {
+    this.texture = new Texture(1, 1, com.badlogic.gdx.graphics.Pixmap.Format.RGBA8888);
+  }
 
   /** Constructs a region the size of the specified texture. */
   public TextureRegion(@Nullable Texture texture) {
