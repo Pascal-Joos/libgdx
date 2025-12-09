8f6b564e7bfc77e6db5776708cf78d76e69acf0e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 00:58:53 +0000

    fix: Type='PASS_NULLABLE', message='passing @Nullable parameter 'actor' where @NonNull is required' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java:832     return spaceLeft.get(actor);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -829,7 +829,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getSpaceLeft() {
-    return spaceLeft.get(actor);
+    return spaceLeft.get(actor != null ? actor : table);
   }
 
   /**
