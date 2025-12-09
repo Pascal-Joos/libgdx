b34060cf8c6d9687067cedef1370c7ee1b15e61a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 05:21:23 +0000

    fix: Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/TimSort.java:169     this.c = null;

--- gdx/src/com/badlogic/gdx/utils/TimSort.java
+++ gdx/src/com/badlogic/gdx/utils/TimSort.java
@@ -175,7 +175,6 @@ class TimSort<T> {
     if (DEBUG) assert stackSize == 1;
 
     this.a = null;
-    this.c = null;
     T[] tmp = this.tmp;
     for (int i = 0, n = tmpCount; i < n; i++) tmp[i] = null;
   }
