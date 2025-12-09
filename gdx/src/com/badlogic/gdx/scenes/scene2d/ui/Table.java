777aff0276866c7fe76ab6fcead9a294f19c931d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 02:39:47 +0000

    fix: Type='UNBOX_NULLABLE', message='unboxing of a @Nullable value - c.align' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java:1210       align = c.align;

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1218,7 +1218,7 @@ public class Table extends WidgetGroup {
         if (maxHeight > 0) c.actorHeight = Math.min(c.actorHeight, maxHeight);
       }
 
-      align = c.align;
+      align = c.align != null ? c.align : com.badlogic.gdx.utils.Align.center;
       if ((align & Align.left) != 0) c.actorX = currentX;
       else if ((align & Align.right) != 0) c.actorX = currentX + spannedCellWidth - c.actorWidth;
       else c.actorX = currentX + (spannedCellWidth - c.actorWidth) / 2;
