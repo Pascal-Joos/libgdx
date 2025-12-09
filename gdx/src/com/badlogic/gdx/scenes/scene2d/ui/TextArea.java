9d560d9da90929ad06965144cd0fff873fe9aaba
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 02:44:09 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field linesBreak (line 38) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java:63   public TextArea(String text, Skin skin) {

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java
@@ -61,11 +61,11 @@ public class TextArea extends TextField {
   private float prefRows;
 
   public TextArea(String text, Skin skin) {
-    super(text, skin);
+    this(text, skin.get(TextFieldStyle.class));
   }
 
   public TextArea(String text, Skin skin, String styleName) {
-    super(text, skin, styleName);
+    this(text, skin.get(styleName, TextFieldStyle.class));
   }
 
   public TextArea(String text, TextFieldStyle style) {
