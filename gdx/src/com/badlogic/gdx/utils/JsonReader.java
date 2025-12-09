61cdc36832250663658d8f841d3f0fdae513c766
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 04:22:25 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/JsonReader.java:577     return root;

--- gdx/src/com/badlogic/gdx/utils/JsonReader.java
+++ gdx/src/com/badlogic/gdx/utils/JsonReader.java
@@ -574,6 +574,7 @@ public class JsonReader implements BaseJsonReader {
     }
     if (parseRuntimeEx != null)
       throw new SerializationException("Error parsing JSON: " + new String(data), parseRuntimeEx);
+    if (root == null) throw new SerializationException("Error parsing JSON: root is null.");
     return root;
   }
 
