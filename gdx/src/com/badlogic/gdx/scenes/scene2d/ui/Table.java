0da1f7a0d53d77f500ad2ff42b0ed034d11ea2ab
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 01:49:09 +0000

    fix: Type='NONNULL_FIELD_READ_BEFORE_INIT', message='read of @NonNull field expandHeight before initialization' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java:879     float[] expandHeight = this.expandHeight = ensureSize(this.expandHeight, rows);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -876,7 +876,7 @@ public class Table extends WidgetGroup {
     float[] columnWidth = this.columnWidth = ensureSize(null, columns);
     float[] rowHeight = this.rowHeight = ensureSize(this.rowHeight, rows);
     float[] expandWidth = this.expandWidth = ensureSize(this.expandWidth, columns);
-    float[] expandHeight = this.expandHeight = ensureSize(this.expandHeight, rows);
+    float[] expandHeight = this.expandHeight = ensureSize(null, rows);
 
     float spaceRightLast = 0;
     for (int i = 0; i < cellCount; i++) {
