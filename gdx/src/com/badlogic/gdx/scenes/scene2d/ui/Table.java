e460c9c3409f6164faa3771c63c11593b2b830b1
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 02:35:00 +0000

    fix: Type='PASS_NULLABLE', message='passing @Nullable parameter 'c.actor' where @NonNull is required' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java:1198         float maxWidth = c.maxWidth.get(c.actor);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1206,7 +1206,7 @@ public class Table extends WidgetGroup {
       float fillX = c.fillX, fillY = c.fillY;
       if (fillX > 0) {
         c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor));
-        float maxWidth = c.maxWidth.get(c.actor);
+        float maxWidth = c.actor == null ? 0 : c.maxWidth.get(c.actor);
         if (maxWidth > 0) c.actorWidth = Math.min(c.actorWidth, maxWidth);
       }
       if (fillY > 0) {
