1887ba89ff32786131f81d1a1824817457cf993e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 03:13:08 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields plus (line 974), minus (line 974) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java:978     public TreeStyle() {}

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
@@ -978,7 +978,63 @@ public class Tree<N extends Node, V> extends WidgetGroup {
     @Nullable public @Null Drawable plusOver, minusOver;
     @Nullable public @Null Drawable over, selection, background;
 
-    public TreeStyle() {}
+    public TreeStyle() {
+      Drawable empty =
+          new Drawable() {
+            @Override
+            public void draw(Batch batch, float x, float y, float width, float height) {}
+
+            @Override
+            public float getLeftWidth() {
+              return 0;
+            }
+
+            @Override
+            public void setLeftWidth(float leftWidth) {}
+
+            @Override
+            public float getRightWidth() {
+              return 0;
+            }
+
+            @Override
+            public void setRightWidth(float rightWidth) {}
+
+            @Override
+            public float getTopHeight() {
+              return 0;
+            }
+
+            @Override
+            public void setTopHeight(float topHeight) {}
+
+            @Override
+            public float getBottomHeight() {
+              return 0;
+            }
+
+            @Override
+            public void setBottomHeight(float bottomHeight) {}
+
+            @Override
+            public float getMinWidth() {
+              return 0;
+            }
+
+            @Override
+            public void setMinWidth(float minWidth) {}
+
+            @Override
+            public float getMinHeight() {
+              return 0;
+            }
+
+            @Override
+            public void setMinHeight(float minHeight) {}
+          };
+      this.plus = empty;
+      this.minus = empty;
+    }
 
     public TreeStyle(Drawable plus, Drawable minus, @Null Drawable selection) {
       this.plus = plus;
