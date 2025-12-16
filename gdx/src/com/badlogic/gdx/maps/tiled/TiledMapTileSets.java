a35d007be7c23837ba087b32937d1a2e10f37a81
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:24:25 +0000

    fix: 196 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/maps/tiled/TiledMapTileSets.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TiledMapTileSets.java
@@ -44,13 +44,17 @@ public class TiledMapTileSets implements Iterable<TiledMapTileSet> {
    * @param name Name of the {@link TiledMapTileSet} to retrieve.
    * @return tileset with matching name, null if it doesn't exist
    */
+  /**
+   * @param name Name of the {@link TiledMapTileSet} to retrieve.
+   * @return tileset with matching name, null if it doesn't exist
+   */
   public TiledMapTileSet getTileSet(String name) {
     for (TiledMapTileSet tileset : tilesets) {
       if (name.equals(tileset.getName())) {
         return tileset;
       }
     }
-    return null;
+    throw new IllegalArgumentException("No tileset found with name: " + name);
   }
 
   /**
