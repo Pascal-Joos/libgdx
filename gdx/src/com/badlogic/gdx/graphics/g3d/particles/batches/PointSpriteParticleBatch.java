f9e4dff59f553dcd3880256f950e49edc8728407
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:29:13 +0000

    Revert "fix: 122 - DEREFERENCE_NULLABLE - int rotationOffset = p * rotationChannel.strideSize; - dereferenced expression rotationChannel is @Nullable"
    This reverts commit 78b274a9105b32aa8fd7da34550600274485ebb4.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
@@ -162,7 +162,7 @@ public class PointSpriteParticleBatch
         int regionOffset = p * regionChannel.strideSize;
         int positionOffset = p * positionChannel.strideSize;
         int colorOffset = p * colorChannel.strideSize;
-        int rotationOffset = rotationChannel == null ? 0 : p * rotationChannel.strideSize;
+        int rotationOffset = p * rotationChannel.strideSize;
 
         vertices[offset + CPU_POSITION_OFFSET] =
             positionChannel.data[positionOffset + ParticleChannels.XOffset];
@@ -181,13 +181,9 @@ public class PointSpriteParticleBatch
         vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET] =
             scaleChannel.data[p * scaleChannel.strideSize];
         vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 1] =
-            rotationChannel == null
-                ? 1f
-                : rotationChannel.data[rotationOffset + ParticleChannels.CosineOffset];
+            rotationChannel.data[rotationOffset + ParticleChannels.CosineOffset];
         vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 2] =
-            rotationChannel == null
-                ? 0f
-                : rotationChannel.data[rotationOffset + ParticleChannels.SineOffset];
+            rotationChannel.data[rotationOffset + ParticleChannels.SineOffset];
         vertices[offset + CPU_REGION_OFFSET] =
             regionChannel.data[regionOffset + ParticleChannels.UOffset];
         vertices[offset + CPU_REGION_OFFSET + 1] =
