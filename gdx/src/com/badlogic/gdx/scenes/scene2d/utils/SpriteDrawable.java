e302c42818aa314bf5b8a6d2a5f9a6400c9ff768
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:39:59 +0000

    Revert "fix: 368 - DEREFERENCE_NULLABLE - Color spriteColor = sprite.getColor(); - dereferenced expression sprite is @Nullable"
    This reverts commit 4eef930f361e2b078be901db78c3a56c339b9d8b.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
@@ -67,10 +67,6 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
       float scaleY,
       float rotation) {
 
-    if (sprite == null) {
-      return;
-    }
-
     Color spriteColor = sprite.getColor();
     float oldColor = spriteColor.toFloatBits();
     sprite.setColor(spriteColor.mul(batch.getColor()));
