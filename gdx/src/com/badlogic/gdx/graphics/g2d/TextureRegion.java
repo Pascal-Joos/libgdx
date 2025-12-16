32e0e7607ab1d6b5010d3e4ca27d35f77507b870
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:22:27 +0000

    fix: 65 - METHOD_NO_INIT - public TextureRegion(TextureRegion region) { - initializer method does not guarantee @NonNull field texture (line 30) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
@@ -66,7 +66,9 @@ public class TextureRegion {
   }
 
   /** Constructs a region with the same texture and coordinates of the specified region. */
+  /** Constructs a region with the same texture and coordinates of the specified region. */
   public TextureRegion(TextureRegion region) {
+    this.texture = region.texture;
     setRegion(region);
   }
 
