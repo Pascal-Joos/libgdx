0494ea666f21b68fa6f537e9bb2d5e50933e44e3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:24:38 +0000

    Revert "fix: 431 - RETURN_NULLABLE - return root; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 59f14ed619891134c0aa4635342b96df8087fee1.

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -345,9 +345,6 @@ public class XmlReader {
       throw new SerializationException("Error parsing XML, unclosed element: " + element.getName());
     }
     Element root = this.root;
-    if (root == null) {
-      throw new SerializationException("Error parsing XML, no root element was produced.");
-    }
     this.root = null;
     return root;
   }
