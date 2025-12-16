ccf9613067c6eacfb5c999b078c13ea2a5cf75e3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:01:33 +0000

    Revert "fix: 93 - ASSIGN_FIELD_NULLABLE - this.mesh = mesh; - assigning @Nullable expression to @NonNull field"
    This reverts commit 977122ec62d0b76ff38fb1b0ef3367687778ce82.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/MeshPart.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/MeshPart.java
@@ -152,9 +152,6 @@ public class MeshPart {
       final int offset,
       final int size,
       final int type) {
-    if (mesh == null) {
-      throw new IllegalArgumentException("mesh must not be null");
-    }
     this.id = id;
     this.mesh = mesh;
     this.offset = offset;
