53a90a8365f21d211aa416154ce8bd1dc9843f28
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:00:26 +0000

    Revert "fix: 249 - ASSIGN_FIELD_NULLABLE - expandX = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 8d9c2689f08bb8b67cda1895ee58539062fc2771.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -979,7 +979,6 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   /** Sets all constraint fields to null. */
-  /** Sets all constraint fields to null. */
   void clear() {
     minWidth = null;
     minHeight = null;
@@ -998,8 +997,8 @@ public class Cell<T extends Actor> implements Poolable {
     fillX = null;
     fillY = null;
     align = null;
-    expandX = 0;
-    expandY = 0;
+    expandX = null;
+    expandY = null;
     colspan = null;
     uniformX = null;
     uniformY = null;
