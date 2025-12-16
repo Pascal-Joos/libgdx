568b7d980f09a68b36ea64b4a588d58345d2d98f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:58:01 +0000

    fix: 393 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -242,7 +242,13 @@ public class JsonValue implements Iterable<JsonValue> {
    * @return May be null if this value is null.
    * @throws IllegalStateException if this an array or object.
    */
-  public @Null String asString() {
+  /**
+   * Returns this value as a string.
+   *
+   * @return May be null if this value is null.
+   * @throws IllegalStateException if this an array or object.
+   */
+  public @Nullable String asString() {
     switch (type) {
       case stringValue:
         return stringValue;
