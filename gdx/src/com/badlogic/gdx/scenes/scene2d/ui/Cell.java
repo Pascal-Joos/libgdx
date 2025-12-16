1e351d08058e4dc6e3bca5f9fe9776e1036c8fe7
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:01:41 +0000

    Revert "fix: 251 - ASSIGN_FIELD_NULLABLE - colspan = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit a4da4d158fb62780e8a86f3aa8456635b307aa5e.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -999,7 +999,7 @@ public class Cell<T extends Actor> implements Poolable {
     align = null;
     expandX = null;
     expandY = null;
-    colspan = 1;
+    colspan = null;
     uniformX = null;
     uniformY = null;
   }
