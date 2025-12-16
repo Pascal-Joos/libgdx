d2db6785a539af5c62874e2b6a4bdc8448176ebc
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:58:35 +0000

    fix: 91 - DEREFERENCE_NULLABLE - if (other.id.equals(partId)) { - dereferenced expression other.id is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/loader/G3dModelLoader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/loader/G3dModelLoader.java
@@ -104,7 +104,7 @@ public class G3dModelLoader extends ModelLoader<ModelLoader.ModelParameters> {
             throw new GdxRuntimeException("Not id given for mesh part");
           }
           for (ModelMeshPart other : parts) {
-            if (other.id.equals(partId)) {
+            if (partId.equals(other.id)) {
               throw new GdxRuntimeException(
                   "Mesh part with id '" + partId + "' already in defined");
             }
