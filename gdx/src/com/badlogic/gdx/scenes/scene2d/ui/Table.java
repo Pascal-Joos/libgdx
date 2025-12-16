61aec0340947d3003cddcd7df704014158a2d6a2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:12:44 +0000

    fix: 333 - UNBOX_NULLABLE - float fillX = c.fillX, fillY = c.fillY; - unboxing of a @Nullable value - c.fillX

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1192,7 +1192,10 @@ public class Table extends WidgetGroup {
 
       currentX += c.computedPadLeft;
 
-      float fillX = c.fillX, fillY = c.fillY;
+      Float fillXObj = c.fillX;
+      Float fillYObj = c.fillY;
+      float fillX = fillXObj == null ? 0f : fillXObj;
+      float fillY = fillYObj == null ? 0f : fillYObj;
       if (fillX > 0) {
         c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor));
         float maxWidth = c.maxWidth.get(c.actor);
