63100b86fdd406d0b056c865ac73ad7a38738d86
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:26:36 +0000

    fix: 119 - NONNULL_FIELD_READ_BEFORE_INIT - renderable.shader = new ParticleShader(renderable, shaderConfig); - read of @NonNull field renderable before initialization

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
@@ -108,6 +108,7 @@ public class PointSpriteParticleBatch
     if (this.depthTestAttribute == null)
       this.depthTestAttribute = new DepthTestAttribute(GL20.GL_LEQUAL, false);
 
+    renderable = new Renderable();
     allocRenderable();
     ensureCapacity(capacity);
     renderable.shader = new ParticleShader(renderable, shaderConfig);
