69c71a1bc1988f9aad4366c22f3c3f43fe0cc367
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:34:14 +0000

    fix: 129 - METHOD_NO_INIT - public DynamicsModifier() {} - initializer method does not guarantee @NonNull field lifeChannel (line 579) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -578,7 +578,9 @@ public abstract class DynamicsModifier extends Influencer {
   public boolean isGlobal = false;
   protected FloatChannel lifeChannel;
 
-  public DynamicsModifier() {}
+  public DynamicsModifier() {
+    this.lifeChannel = null;
+  }
 
   public DynamicsModifier(DynamicsModifier modifier) {
     this.isGlobal = modifier.isGlobal;
