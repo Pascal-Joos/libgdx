ba907bae83deaca6e4e82f55636d9f5d60272501
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:13:11 +0000

    fix: 182 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.badlogic.gdx.graphics.TextureData.consumePixmap() returns @NonNull

--- gdx/src/com/badlogic/gdx/graphics/glutils/FileTextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/FileTextureData.java
@@ -71,6 +71,7 @@ public class FileTextureData implements TextureData {
     if (!isPrepared) throw new GdxRuntimeException("Call prepare() before calling getPixmap()");
     isPrepared = false;
     Pixmap pixmap = this.pixmap;
+    if (pixmap == null) throw new GdxRuntimeException("Pixmap is null");
     this.pixmap = null;
     return pixmap;
   }
