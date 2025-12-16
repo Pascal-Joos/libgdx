d745a7a719818ae3e9db7c4926cddeabaf89678e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:12:26 +0000

    Revert "fix: 181 - ASSIGN_FIELD_NULLABLE - data = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 85d35264c5dda661708496f6c0d4fdc7130123d0.

--- gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
@@ -76,10 +76,8 @@ public class ETC1TextureData implements TextureData {
     if (!isPrepared)
       throw new GdxRuntimeException("Call prepare() before calling consumeCompressedData()");
 
-    final ETC1.ETC1Data localData = data;
-
     if (!Gdx.graphics.supportsExtension("GL_OES_compressed_ETC1_RGB8_texture")) {
-      Pixmap pixmap = ETC1.decodeImage(localData, Format.RGB565);
+      Pixmap pixmap = ETC1.decodeImage(data, Format.RGB565);
       Gdx.gl.glTexImage2D(
           target,
           0,
@@ -102,11 +100,12 @@ public class ETC1TextureData implements TextureData {
           width,
           height,
           0,
-          localData.compressedData.capacity() - localData.dataOffset,
-          localData.compressedData);
+          data.compressedData.capacity() - data.dataOffset,
+          data.compressedData);
       if (useMipMaps()) Gdx.gl20.glGenerateMipmap(GL20.GL_TEXTURE_2D);
     }
-    localData.dispose();
+    data.dispose();
+    data = null;
     isPrepared = false;
   }
 
