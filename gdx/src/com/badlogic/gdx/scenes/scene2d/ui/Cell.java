0afe2b991eeb164367c725f8baa4d029640001a2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:12:11 +0000

    fix: 265 - PASS_NULLABLE - return prefHeight.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -785,6 +785,9 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPrefHeight() {
+    if (actor == null) {
+      return 0f;
+    }
     return prefHeight.get(actor);
   }
 
