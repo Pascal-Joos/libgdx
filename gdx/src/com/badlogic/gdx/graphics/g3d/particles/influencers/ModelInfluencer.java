d026473cdfd25272238db2663529affa0f1c9c32
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:41:25 +0000

    Revert "fix: 138 - METHOD_NO_INIT - public ModelInfluencer() { - initializer method does not guarantee @NonNull field modelChannel (line 122) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit fd6130156df4f511782c2a32b3adee4a49ac7f43.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ModelInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ModelInfluencer.java
@@ -123,7 +123,6 @@ public abstract class ModelInfluencer extends Influencer {
 
   public ModelInfluencer() {
     this.models = new Array<Model>(true, 1, Model.class);
-    this.modelChannel = null;
   }
 
   public ModelInfluencer(Model... models) {
