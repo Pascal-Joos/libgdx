3c4d57411b60756ba0bcf1c64cbbca6244474b23
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:39:50 +0000

    fix: 77 - ASSIGN_FIELD_NULLABLE - result.meshPart.mesh = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelCache.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelCache.java
@@ -267,7 +267,9 @@ public class ModelCache implements Disposable, RenderableProvider {
     result.bones = null;
     result.environment = null;
     result.material = material;
-    result.meshPart.mesh = null;
+    if (result.meshPart.mesh == null) {
+      result.meshPart.mesh = meshPool.obtain();
+    }
     result.meshPart.offset = 0;
     result.meshPart.size = 0;
     result.meshPart.primitiveType = primitiveType;
