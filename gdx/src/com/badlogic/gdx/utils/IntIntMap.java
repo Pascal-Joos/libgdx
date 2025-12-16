3bd2ebab41ac86e740f92b7e77adcf9c825c1557
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:49:54 +0000

    Revert "fix: 382 - METHOD_NO_INIT - public IntIntMap(int initialCapacity, float loadFactor) { - initializer method does not guarantee @NonNull fields entries1 (line 79), entries2 (line 79), values1 (line 80), values2 (line 80), keys1 (line 81), keys2 (line 81) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 3d2d15e990319560b99b7e78d71b43f5022003e7.

--- gdx/src/com/badlogic/gdx/utils/IntIntMap.java
+++ gdx/src/com/badlogic/gdx/utils/IntIntMap.java
@@ -114,12 +114,6 @@ public class IntIntMap implements Iterable<IntIntMap.Entry> {
 
     keyTable = new int[tableSize];
     valueTable = new int[tableSize];
-    entries1 = new Entries(this);
-    entries2 = new Entries(this);
-    values1 = new Values(this);
-    values2 = new Values(this);
-    keys1 = new Keys(this);
-    keys2 = new Keys(this);
   }
 
   /** Creates a new map identical to the specified map. */
