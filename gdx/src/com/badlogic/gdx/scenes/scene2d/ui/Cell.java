1f4a4d6f7d5d5f334bb8668d2a4b9631b98b8b3a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:15:16 +0000

    fix: 269 - PASS_NULLABLE - return spaceRight.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -851,6 +851,9 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getSpaceRight() {
+    if (actor == null) {
+      return 0f;
+    }
     return spaceRight.get(actor);
   }
 
