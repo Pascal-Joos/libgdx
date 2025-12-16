718f097365cebe94a88fd44a8c10344c1f17e966
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:33:16 +0000

    fix: 443 - FIELD_NO_INIT - byte[] _buffer; - @NonNull field _buffer not initialized

--- gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java
@@ -6,7 +6,21 @@ import java.io.IOException;
 import javax.annotation.Nullable;
 
 public class OutWindow {
-  byte[] _buffer;
+  public class OutWindow {
+    @Nullable byte[] _buffer;
+    int _pos;
+    int _windowSize = 0;
+    int _streamPos;
+    java.io.OutputStream _stream;
+
+    public void Create(int windowSize) {
+      if (_buffer == null || _windowSize != windowSize) _buffer = new byte[windowSize];
+      _windowSize = windowSize;
+      _pos = 0;
+      _streamPos = 0;
+    }
+  }
+
   int _pos;
   int _windowSize = 0;
   int _streamPos;
