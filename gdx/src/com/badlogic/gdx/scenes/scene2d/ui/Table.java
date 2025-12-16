8cf34527a744bde0764b7dfcbed355ff86bc5d2b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:52:19 +0000

    fix: 311 - PASS_NULLABLE - c.computedPadBottom = c.padBottom.get(a) + (row == rows - 1 ? 0 : c.spaceBottom.get(a)); - passing @Nullable parameter 'a' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -883,6 +883,7 @@ public class Table extends WidgetGroup {
       Cell c = (Cell) cells[i];
       int column = c.column, row = c.row, colspan = c.colspan;
       Actor a = c.actor;
+      if (a == null) continue;
 
       // Collect rows that expand and colspan=1 columns that expand.
       if (c.expandY != 0 && expandHeight[row] == 0) expandHeight[row] = c.expandY;
@@ -933,6 +934,8 @@ public class Table extends WidgetGroup {
     for (int i = 0; i < cellCount; i++) {
       Cell c = (Cell) cells[i];
       int column = c.column;
+      Actor a = c.actor;
+      if (a == null) continue;
 
       // Colspan with expand will expand all spanned columns if none of the spanned columns have
       // expand.
@@ -961,6 +964,8 @@ public class Table extends WidgetGroup {
     if (uniformPrefWidth > 0 || uniformPrefHeight > 0) {
       for (int i = 0; i < cellCount; i++) {
         Cell c = (Cell) cells[i];
+        Actor a = c.actor;
+        if (a == null) continue;
         if (uniformPrefWidth > 0 && c.uniformX == Boolean.TRUE && c.colspan == 1) {
           float hpadding = c.computedPadLeft + c.computedPadRight;
           columnMinWidth[c.column] = uniformMinWidth + hpadding;
@@ -983,6 +988,7 @@ public class Table extends WidgetGroup {
       int column = c.column;
 
       Actor a = c.actor;
+      if (a == null) continue;
       float minWidth = c.minWidth.get(a),
           prefWidth = c.prefWidth.get(a),
           maxWidth = c.maxWidth.get(a);
