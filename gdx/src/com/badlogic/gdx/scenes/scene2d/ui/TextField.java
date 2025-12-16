c8b36a9af37cfee8dd14a8474b6522761ad3d96d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:19:54 +0000

    fix: 342 - RETURN_NULLABLE - return style.background; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
@@ -312,7 +312,7 @@ public class TextField extends Widget implements Disableable {
     }
   }
 
-  protected @Null Drawable getBackgroundDrawable() {
+  protected @Nullable Drawable getBackgroundDrawable() {
     if (disabled && style.disabledBackground != null) return style.disabledBackground;
     if (style.focusedBackground != null && hasKeyboardFocus()) return style.focusedBackground;
     return style.background;
