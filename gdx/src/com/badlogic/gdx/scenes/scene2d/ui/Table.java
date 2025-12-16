949679ebaaff902428d3993c75cba6cb0a963da4
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:50:40 +0000

    Revert "fix: 309 - PASS_NULLABLE - c.computedPadRight = c.padRight.get(a) + ((column + colspan) == columns ? 0 : spaceRight); - passing @Nullable parameter 'a' where @NonNull is required"
    This reverts commit ba654d2e9c3f40a10bc8badf8498071913a90ea3.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -883,9 +883,6 @@ public class Table extends WidgetGroup {
       Cell c = (Cell) cells[i];
       int column = c.column, row = c.row, colspan = c.colspan;
       Actor a = c.actor;
-      if (a == null) {
-        continue;
-      }
 
       // Collect rows that expand and colspan=1 columns that expand.
       if (c.expandY != 0 && expandHeight[row] == 0) expandHeight[row] = c.expandY;
@@ -986,9 +983,6 @@ public class Table extends WidgetGroup {
       int column = c.column;
 
       Actor a = c.actor;
-      if (a == null) {
-        continue;
-      }
       float minWidth = c.minWidth.get(a),
           prefWidth = c.prefWidth.get(a),
           maxWidth = c.maxWidth.get(a);
