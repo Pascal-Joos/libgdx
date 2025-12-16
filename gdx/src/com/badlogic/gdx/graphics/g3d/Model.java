47c81b5c8655ce8fad0bdd40abd2b4770a06fcc8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:31:42 +0000

    Revert "fix: 71 - ASSIGN_FIELD_NULLABLE - animation.id = anim.id; - assigning @Nullable expression to @NonNull field"
    This reverts commit b6dedb02e047ee26654b3b238998434cbb8dd6ab.

--- gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
@@ -134,9 +134,6 @@ public class Model implements Disposable {
 
   protected void loadAnimations(Iterable<ModelAnimation> modelAnimations) {
     for (final ModelAnimation anim : modelAnimations) {
-      if (anim.id == null) {
-        continue;
-      }
       Animation animation = new Animation();
       animation.id = anim.id;
       for (ModelNodeAnimation nanim : anim.nodeAnimations) {
