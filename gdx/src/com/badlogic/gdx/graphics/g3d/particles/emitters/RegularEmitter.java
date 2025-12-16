3dd51a26b9ec69352e7aeb7f1124f3b64a83ecde
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:31:22 +0000

    fix: 125 - METHOD_NO_INIT - public RegularEmitter() { - initializer method does not guarantee @NonNull field lifeChannel (line 56) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/emitters/RegularEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/emitters/RegularEmitter.java
@@ -67,6 +67,7 @@ public class RegularEmitter extends Emitter implements Json.Serializable {
     lifeValue.setActive(true);
     continuous = true;
     emissionMode = EmissionMode.Enabled;
+    lifeChannel = new FloatChannel();
   }
 
   public RegularEmitter(RegularEmitter regularEmitter) {
