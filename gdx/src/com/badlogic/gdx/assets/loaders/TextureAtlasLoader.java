9e9d003ea1a792c74e9cdffa31c7b44f9a7d4b0b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:11:20 +0000

    fix: 17 - DEREFERENCE_NULLABLE - for (Page page : data.getPages()) { - dereferenced expression data is @Nullable

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureAtlasLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureAtlasLoader.java
@@ -49,6 +49,9 @@ public class TextureAtlasLoader
       String fileName,
       FileHandle file,
       @Nullable TextureAtlasParameter parameter) {
+    if (data == null) {
+      return null;
+    }
     for (Page page : data.getPages()) {
       Texture texture =
           assetManager.get(page.textureFile.path().replaceAll("\\\\", "/"), Texture.class);
