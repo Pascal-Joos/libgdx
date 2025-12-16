4191bee2c1ddd2d619c6b2782785953528ec12ae
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:21:07 +0000

    Revert "fix: 111 - FIELD_NO_INIT - private int[] particleIndices, particleOffsets; - @NonNull field ParticleSorter$Distance.particleOffsets not initialized"
    This reverts commit 26885ae6729bcc3a1e584c74b384e54d46cdf6d9.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
@@ -55,17 +55,7 @@ public abstract class ParticleSorter {
   /** This class will sort all the particles using the distance from camera. */
   public static class Distance extends ParticleSorter {
     private float[] distances;
-
-    @Override
-    public void ensureCapacity(int capacity) {
-      if (currentSize < capacity) {
-        distances = new float[capacity];
-        particleIndices = new int[capacity];
-        particleOffsets = new int[capacity];
-        currentSize = capacity;
-      }
-    }
-
+    private int[] particleIndices, particleOffsets;
     private int currentSize = 0;
 
     @Override
