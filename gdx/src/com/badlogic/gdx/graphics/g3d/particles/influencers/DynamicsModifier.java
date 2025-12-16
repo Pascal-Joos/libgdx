0a1595af311977547863dd123183e89b007a8d49
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:35:02 +0000

    Revert "fix: 130 - METHOD_NO_INIT - public DynamicsModifier(DynamicsModifier modifier) { - initializer method does not guarantee @NonNull field lifeChannel (line 579) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 0df6820ae903370919e31b84d4fde3eb18facb21.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -582,7 +582,6 @@ public abstract class DynamicsModifier extends Influencer {
 
   public DynamicsModifier(DynamicsModifier modifier) {
     this.isGlobal = modifier.isGlobal;
-    this.lifeChannel = modifier.lifeChannel;
   }
 
   @Override
