3bd653a4637e5608ade139661a9de224aa182428
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:20:08 +0000

    fix: 109 - FIELD_NO_INIT - private float[] distances; - @NonNull field ParticleSorter$Distance.distances not initialized

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
@@ -54,7 +54,20 @@ public abstract class ParticleSorter {
 
   /** This class will sort all the particles using the distance from camera. */
   public static class Distance extends ParticleSorter {
-    private float[] distances;
+    @Override
+    public void ensureCapacity(int capacity) {
+      if (currentSize < capacity) {
+        distances = new float[capacity];
+        particleIndices = new int[capacity];
+        particleOffsets = new int[capacity];
+        currentSize = capacity;
+      } else if (distances == null || particleIndices == null || particleOffsets == null) {
+        distances = new float[currentSize];
+        particleIndices = new int[currentSize];
+        particleOffsets = new int[currentSize];
+      }
+    }
+
     private int[] particleIndices, particleOffsets;
     private int currentSize = 0;
 
