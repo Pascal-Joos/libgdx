91f22cff77fb8b1875ed83979209ca65029a8c5e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:20:57 +0000

    Revert "fix: 64 - METHOD_NO_INIT - public TextureRegion() {} - initializer method does not guarantee @NonNull field texture (line 30) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 8a693309e3f77773af99538cedc7732c0ce8787a.

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
@@ -33,10 +33,7 @@ public class TextureRegion {
   int regionWidth, regionHeight;
 
   /** Constructs a region that cannot be used until a texture and texture coordinates are set. */
-  /** Constructs a region that cannot be used until a texture and texture coordinates are set. */
-  public TextureRegion() {
-    this.texture = null;
-  }
+  public TextureRegion() {}
 
   /** Constructs a region the size of the specified texture. */
   public TextureRegion(@Nullable Texture texture) {
