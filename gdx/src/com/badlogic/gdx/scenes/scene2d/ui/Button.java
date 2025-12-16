c82385bb10645f8521e6b76dca93a8c02f645162
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:49:02 +0000

    fix: 230 - METHOD_NO_INIT - public Button(Actor child, ButtonStyle style) { - initializer method does not guarantee @NonNull field style (line 50) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Button.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Button.java
@@ -73,6 +73,9 @@ public class Button extends Table implements Disableable {
   }
 
   public Button(Actor child, ButtonStyle style) {
+    if (style == null) {
+      throw new IllegalArgumentException("style cannot be null");
+    }
     initialize();
     add(child);
     setStyle(style);
