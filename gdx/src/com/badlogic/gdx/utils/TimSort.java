16435c51bd20924c4c1f6db19cfa67dc60119859
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:22:08 +0000

    fix: 428 - ASSIGN_FIELD_NULLABLE - this.c = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/TimSort.java
+++ gdx/src/com/badlogic/gdx/utils/TimSort.java
@@ -166,7 +166,9 @@ class TimSort<T> {
     if (DEBUG) assert stackSize == 1;
 
     this.a = null;
-    this.c = null;
+    if (c != null) {
+      this.c = c;
+    }
     T[] tmp = this.tmp;
     for (int i = 0, n = tmpCount; i < n; i++) tmp[i] = null;
   }
