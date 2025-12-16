58b7aef21bf90242f13b3673f97de748cad81304
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:26:09 +0000

    Revert "fix: 433 - RETURN_NULLABLE - if (children == null) return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit dfc61f94948cab5bb3323d81d683b58353f9d822.

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -621,11 +621,7 @@ public class XmlReader {
      * @param name the name of the child {@link Element}
      * @return the first child having the given name or null, does not recurse
      */
-    /**
-     * @param name the name of the child {@link Element}
-     * @return the first child having the given name or null, does not recurse
-     */
-    public Element getChildByName(String name) {
+    public @Null Element getChildByName(String name) {
       if (children == null) return null;
       for (int i = 0; i < children.size; i++) {
         Element element = children.get(i);
