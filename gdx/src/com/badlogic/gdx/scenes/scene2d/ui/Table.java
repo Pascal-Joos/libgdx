426d674cd29319629e0a261ae348f8d016cd5a07
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:07:18 +0000

    Revert "fix: 328 - PASS_NULLABLE - c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor)); - passing @Nullable parameter 'c.actor' where @NonNull is required"
    This reverts commit 6a0c4a2f32de722dddfc7b45ffaefef15677804f.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1194,18 +1194,16 @@ public class Table extends WidgetGroup {
 
       float fillX = c.fillX, fillY = c.fillY;
       if (fillX > 0) {
-        Actor actorForWidth = c.actor == null ? this : c.actor;
-        c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(actorForWidth));
-        float maxWidth = c.maxWidth.get(actorForWidth);
+        c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor));
+        float maxWidth = c.maxWidth.get(c.actor);
         if (maxWidth > 0) c.actorWidth = Math.min(c.actorWidth, maxWidth);
       }
       if (fillY > 0) {
-        Actor actorForHeight = c.actor == null ? this : c.actor;
         c.actorHeight =
             Math.max(
                 rowHeight[c.row] * fillY - c.computedPadTop - c.computedPadBottom,
-                c.minHeight.get(actorForHeight));
-        float maxHeight = c.maxHeight.get(actorForHeight);
+                c.minHeight.get(c.actor));
+        float maxHeight = c.maxHeight.get(c.actor);
         if (maxHeight > 0) c.actorHeight = Math.min(c.actorHeight, maxHeight);
       }
 
