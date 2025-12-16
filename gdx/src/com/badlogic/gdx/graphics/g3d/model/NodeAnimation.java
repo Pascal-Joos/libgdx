8167032f1fc090c04071a452c9ecc87107892e9d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:05:57 +0000

    fix: 96 - FIELD_NO_INIT - public Node node; - @NonNull field node not initialized

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/NodeAnimation.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/NodeAnimation.java
@@ -31,7 +31,8 @@ import javax.annotation.Nullable;
  */
 public class NodeAnimation {
   /** the Node affected by this animation * */
-  public Node node;
+  /** the Node affected by this animation * */
+  @Nullable public Node node;
 
   /** the translation keyframes if any (might be null), sorted by time ascending * */
   @Nullable public Array<NodeKeyframe<Vector3>> translation = null;
