97016d8267a19d02375d675627472bdedb3b79a9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:07:03 +0000

    fix: 258 - PASS_NULLABLE - return padLeft.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -873,7 +873,11 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPadLeft() {
-    return padLeft.get(actor);
+    Value localPadLeft = padLeft;
+    if (localPadLeft == null) {
+      return 0f;
+    }
+    return localPadLeft.get(actor);
   }
 
   /**
