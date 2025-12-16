b2d74525e5da1275769dc9fa120f3ace2c52120c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:06:07 +0000

    Revert "fix: 404 - METHOD_NO_INIT - public ObjectLongMap(int initialCapacity, float loadFactor) { - initializer method does not guarantee @NonNull fields entries1 (line 77), entries2 (line 77), values1 (line 78), values2 (line 78), keys1 (line 79), keys2 (line 79) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit ff90776d45a4884120b85dbbb90624d67dfb7d6d.

--- gdx/src/com/badlogic/gdx/utils/ObjectLongMap.java
+++ gdx/src/com/badlogic/gdx/utils/ObjectLongMap.java
@@ -112,13 +112,6 @@ public class ObjectLongMap<K> implements Iterable<ObjectLongMap.Entry<K>> {
 
     keyTable = (K[]) new Object[tableSize];
     valueTable = new long[tableSize];
-
-    entries1 = null;
-    entries2 = null;
-    values1 = null;
-    values2 = null;
-    keys1 = null;
-    keys2 = null;
   }
 
   /** Creates a new map identical to the specified map. */
