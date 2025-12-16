b7aa926b632a2076e6f629725a68deb3534421d4
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 20:23:51 +0000

    Revert "fix: 280 - METHOD_NO_INIT - public ImageButton(ImageButtonStyle style) { - initializer method does not guarantee @NonNull field style (line 36) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit f21cc809b27ceeb570da9789a5b2cfc5b0642780.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ImageButton.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ImageButton.java
@@ -47,7 +47,6 @@ public class ImageButton extends Button {
 
   public ImageButton(ImageButtonStyle style) {
     super(style);
-    this.style = style;
     image = newImage();
     add(image);
     setStyle(style);
