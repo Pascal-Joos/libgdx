f8c3037914bac0840511591d2b48d9f28d749eea
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:02:55 +0000

    Revert "fix: 11 - DEREFERENCE_NULLABLE - return get(assetDescriptor.fileName, assetDescriptor.type, true); - dereferenced expression assetDescriptor is @Nullable"
    This reverts commit 13d8c39adeeb298ca8f135b6eb3c42246fa2c3f6.

--- gdx/src/com/badlogic/gdx/assets/AssetManager.java
+++ gdx/src/com/badlogic/gdx/assets/AssetManager.java
@@ -204,15 +204,7 @@ public class AssetManager implements Disposable {
    * @return the asset
    * @throws GdxRuntimeException if the asset is not loaded
    */
-  /**
-   * @param assetDescriptor the asset descriptor
-   * @return the asset
-   * @throws GdxRuntimeException if the asset is not loaded
-   */
   public synchronized <T> T get(@Nullable AssetDescriptor<T> assetDescriptor) {
-    if (assetDescriptor == null) {
-      throw new GdxRuntimeException("AssetDescriptor must not be null");
-    }
     return get(assetDescriptor.fileName, assetDescriptor.type, true);
   }
 
