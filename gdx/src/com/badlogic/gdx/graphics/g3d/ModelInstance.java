fa800d50fcadf4b68db37b65b96cf08c68fdc8d1
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:40:41 +0000

    Revert "fix: 78 - PASS_NULLABLE - this(model, (String[]) null); - passing @Nullable parameter '(String[]) null' where @NonNull is required"
    This reverts commit 8ab811ed78d99a1b95c8f21c41a4053a9750e64c.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -72,13 +72,8 @@ public class ModelInstance implements RenderableProvider {
    *
    * @param model The {@link Model} to create an instance of.
    */
-  /**
-   * Constructs a new ModelInstance with all nodes and materials of the given model.
-   *
-   * @param model The {@link Model} to create an instance of.
-   */
   public ModelInstance(final Model model) {
-    this(model, new String[0]);
+    this(model, (String[]) null);
   }
 
   /**
