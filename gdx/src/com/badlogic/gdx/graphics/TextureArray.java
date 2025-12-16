213dfc6eee745decb4ad9d0a4a95454906eec91b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:38:21 +0000

    fix: 35 - DEREFERENCE_NULLABLE - return managedTextureArrays.get(Gdx.app).size; - dereferenced expression managedTextureArrays.get(Gdx.app) is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/TextureArray.java
+++ gdx/src/com/badlogic/gdx/graphics/TextureArray.java
@@ -165,6 +165,7 @@ public class TextureArray extends GLTexture {
    * @return the number of managed TextureArrays currently loaded
    */
   public static int getNumManagedTextureArrays() {
-    return managedTextureArrays.get(Gdx.app).size;
+    Array<TextureArray> arrays = managedTextureArrays.get(Gdx.app);
+    return arrays == null ? 0 : arrays.size;
   }
 }
