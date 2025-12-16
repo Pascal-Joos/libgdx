0b75695eef959905e17e136e5d48937c4c0cace2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:44:09 +0000

    Revert "fix: 374 - RETURN_NULLABLE - if (size == 0) return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 9661e9fb5d2b6f9dc2006c4f216af7afe66f726d.

--- gdx/src/com/badlogic/gdx/utils/Array.java
+++ gdx/src/com/badlogic/gdx/utils/Array.java
@@ -589,8 +589,7 @@ public class Array<T> implements Iterable<T> {
   }
 
   /** Returns a random item from the array, or null if the array is empty. */
-  /** Returns a random item from the array, or null if the array is empty. */
-  public @org.jetbrains.annotations.Nullable T random() {
+  public @Null T random() {
     if (size == 0) return null;
     return items[MathUtils.random(0, size - 1)];
   }
