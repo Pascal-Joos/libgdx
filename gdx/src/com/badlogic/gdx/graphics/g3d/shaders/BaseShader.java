9ea20f342c0a82a3ab906b6742cbf2ca653cdcc2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:55:42 +0000

    fix: 159 - PASS_NULLABLE - if (setters.get(u = globalUniforms.get(i)) != null) setters.get(u).set(this, u, null, null); - passing @Nullable parameter 'null' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/BaseShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/BaseShader.java
@@ -239,8 +239,10 @@ public abstract class BaseShader implements Shader {
     this.context = context;
     program.bind();
     currentMesh = null;
-    for (int u, i = 0; i < globalUniforms.size; ++i)
-      if (setters.get(u = globalUniforms.get(i)) != null) setters.get(u).set(this, u, null, null);
+    for (int u, i = 0; i < globalUniforms.size; ++i) {
+      final BaseShader.Setter setter = setters.get(u = globalUniforms.get(i));
+      if (setter != null) setter.set(this, u, null, null);
+    }
   }
 
   private final IntArray tempArray = new IntArray();
