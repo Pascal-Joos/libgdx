9ae286748b20a8123de9bc0f801e84b214e7a85e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 21:24:28 +0000

    Revert "fix: 348 - ASSIGN_FIELD_NULLABLE - this.manager = manager; - assigning @Nullable expression to @NonNull field"
    This reverts commit 706c8d7a621b368f55283e3919ae271dcf6e62e5.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tooltip.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tooltip.java
@@ -49,10 +49,7 @@ public class Tooltip<T extends Actor> extends InputListener {
   /**
    * @param contents May be null.
    */
-  public Tooltip(@Nullable @Null T contents, @Nullable @Null TooltipManager manager) {
-    if (manager == null) {
-      manager = TooltipManager.getInstance();
-    }
+  public Tooltip(@Nullable @Null T contents, @Nullable TooltipManager manager) {
     this.manager = manager;
 
     container =
