60d62484fc829aedfb899c01640c585c236c3a2d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:07:47 +0000

    Revert "fix: 407 - FIELD_NO_INIT - public K key; - @NonNull field ObjectMap$Entry.key not initialized"
    This reverts commit d6cbd2796f3362ddcaf71786c98f6bb478b4d141.

--- gdx/src/com/badlogic/gdx/utils/ObjectMap.java
+++ gdx/src/com/badlogic/gdx/utils/ObjectMap.java
@@ -526,15 +526,7 @@ public class ObjectMap<K, V> implements Iterable<ObjectMap.Entry<K, V>> {
   }
 
   public static class Entry<K, V> {
-    public static class Entry<K, V> {
-      public @Null K key;
-      public @Null V value;
-
-      public String toString() {
-        return key + "=" + value;
-      }
-    }
-
+    public K key;
     public @Null V value;
 
     public String toString() {
