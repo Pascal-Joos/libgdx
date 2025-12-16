e39f3ec88ffc7a063279e072c9b53a06f8e9d403
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:34:45 +0000

    Revert "fix: 73 - DEREFERENCE_NULLABLE - currentShader.render(renderable); - dereferenced expression currentShader is @Nullable"
    This reverts commit a4bc36160dc55cecb9db984cc5d946f68dd83273.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
@@ -269,22 +269,14 @@ public class ModelBatch implements Disposable {
    * Flushes the batch, causing all {@link Renderable}s in the batch to be rendered. Can only be
    * called after the call to {@link #begin(Camera)} and before the call to {@link #end()}.
    */
-  /**
-   * Flushes the batch, causing all {@link Renderable}s in the batch to be rendered. Can only be
-   * called after the call to {@link #begin(Camera)} and before the call to {@link #end()}.
-   */
   public void flush() {
     sorter.sort(camera, renderables);
     Shader currentShader = null;
     for (int i = 0; i < renderables.size; i++) {
       final Renderable renderable = renderables.get(i);
-      final Shader renderableShader = renderable.shader;
-      if (renderableShader == null) {
-        continue;
-      }
-      if (currentShader != renderableShader) {
+      if (currentShader != renderable.shader) {
         if (currentShader != null) currentShader.end();
-        currentShader = renderableShader;
+        currentShader = renderable.shader;
         currentShader.begin(camera, context);
       }
       currentShader.render(renderable);
