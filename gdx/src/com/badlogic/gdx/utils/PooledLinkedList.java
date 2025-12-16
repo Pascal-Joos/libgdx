38069b741aa4cc9318a7f91cad8d68dbedf95517
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 22:14:27 +0000

    Revert "fix: 417 - DEREFERENCE_NULLABLE - tail.next = null; - dereferenced expression tail is @Nullable"
    This reverts commit 682fddf2a8df153ee6d6a5fce5db96eed34efe58.

--- gdx/src/com/badlogic/gdx/utils/PooledLinkedList.java
+++ gdx/src/com/badlogic/gdx/utils/PooledLinkedList.java
@@ -168,7 +168,6 @@ public class PooledLinkedList<T> {
   }
 
   /** Removes the tail of the list regardless of iteration status */
-  /** Removes the tail of the list regardless of iteration status */
   @Nullable
   public @Null T removeLast() {
     if (tail == null) {
@@ -187,9 +186,7 @@ public class PooledLinkedList<T> {
       tail = null;
     } else {
       tail = p;
-      if (tail != null) {
-        tail.next = null;
-      }
+      tail.next = null;
     }
 
     return payload;
