eb58e7536fe4045a557fa4d1be8d3bfdcc69d3cb
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:51:45 +0000

    fix: 4 - FIELD_NO_INIT - public static GL20 gl; - @NonNull static field gl not initialized

--- gdx/src/com/badlogic/gdx/Gdx.java
+++ gdx/src/com/badlogic/gdx/Gdx.java
@@ -38,7 +38,7 @@ public class Gdx {
   public static Files files;
   @Nullable public static Net net;
 
-  public static GL20 gl;
+  @Nullable public static GL20 gl;
   public static GL20 gl20;
   public static GL30 gl30;
 }
