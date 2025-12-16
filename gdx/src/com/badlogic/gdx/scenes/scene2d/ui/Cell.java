70e0356c5cc40fe58d60ff98febb8a8d91ac27d7
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 19:58:39 +0000

    fix: 246 - ASSIGN_FIELD_NULLABLE - padLeft = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -979,6 +979,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
   void clear() {
     minWidth = null;
     minHeight = null;
@@ -991,7 +992,7 @@ public class Cell<T extends Actor> implements Poolable {
     spaceBottom = null;
     spaceRight = null;
     padTop = null;
-    padLeft = null;
+    // padLeft = null;
     padBottom = null;
     padRight = null;
     fillX = null;
