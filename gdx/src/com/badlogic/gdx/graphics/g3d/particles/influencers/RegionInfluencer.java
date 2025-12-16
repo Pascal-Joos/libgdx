26986a1b9ea02ead49e050026dd861077f72759f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:45:33 +0000

    Revert "fix: 144 - METHOD_NO_INIT - public RegionInfluencer(int regionsCount) { - initializer method does not guarantee @NonNull field regionChannel (line 216) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 543424db2468e44367d9372f4b9645ee6369c2b9.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
@@ -218,7 +218,6 @@ public abstract class RegionInfluencer extends Influencer {
 
   public RegionInfluencer(int regionsCount) {
     this.regions = new Array<AspectTextureRegion>(false, regionsCount, AspectTextureRegion.class);
-    this.regionChannel = new FloatChannel();
   }
 
   public RegionInfluencer() {
