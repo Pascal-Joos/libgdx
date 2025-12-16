4e8a0f64bace6a43f3f36d87c678b949fcc8a850
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:53:24 +0000

    Revert "fix: 156 - FIELD_NO_INIT - public FloatChannel positionChannel; - @NonNull field positionChannel not initialized"
    This reverts commit 46d4056c576befa48004c3d6e921ee97c0713910.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerRenderData.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerRenderData.java
@@ -26,9 +26,5 @@ import com.badlogic.gdx.graphics.g3d.particles.ParticleController;
  */
 public abstract class ParticleControllerRenderData {
   public ParticleController controller;
-
-  public abstract class ParticleControllerRenderData {
-    public ParticleController controller;
-    public FloatChannel positionChannel = new FloatChannel();
-  }
+  public FloatChannel positionChannel;
 }
