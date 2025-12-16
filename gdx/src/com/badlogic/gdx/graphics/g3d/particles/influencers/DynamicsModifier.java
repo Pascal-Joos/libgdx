57449917ea92f965759fcbbb49ecb7d47d471f2f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:40:37 +0000

    fix: 137 - METHOD_NO_INIT - public Strength(Strength rotation) { - initializer method does not guarantee @NonNull field strengthChannel (line 95) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -101,6 +101,7 @@ public abstract class DynamicsModifier extends Influencer {
 
     public Strength(Strength rotation) {
       super(rotation);
+      this.strengthChannel = rotation.strengthChannel;
       strengthValue = new ScaledNumericValue();
       strengthValue.load(rotation.strengthValue);
     }
