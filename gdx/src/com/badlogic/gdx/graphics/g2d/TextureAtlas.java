54e44febdd7e980c835c5108246a0548f116ebd6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:17:48 +0000

    fix: 62 - FIELD_NO_INIT - public @Null FileHandle textureFile; - @NonNull field TextureAtlas$TextureAtlasData$Page.textureFile not initialized

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -562,7 +562,23 @@ public class TextureAtlas implements Disposable {
        * May be null if this page isn't associated with a file. In that case, {@link #texture} must
        * be set.
        */
-      public @Null FileHandle textureFile;
+      public static class Page {
+        /**
+         * May be null if this page isn't associated with a file. In that case, {@link #texture}
+         * must be set.
+         */
+        public @Null FileHandle textureFile = null;
+
+        /** May be null if the texture is not yet loaded. */
+        public @Null Texture texture;
+
+        public float width, height;
+        public boolean useMipMaps;
+        public Format format = Format.RGBA8888;
+        public TextureFilter minFilter = TextureFilter.Nearest, magFilter = TextureFilter.Nearest;
+        public TextureWrap uWrap = TextureWrap.ClampToEdge, vWrap = TextureWrap.ClampToEdge;
+        public boolean pma;
+      }
 
       /** May be null if the texture is not yet loaded. */
       @Nullable public @Null Texture texture;
