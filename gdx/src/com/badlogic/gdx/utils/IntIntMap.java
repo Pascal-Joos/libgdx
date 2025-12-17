b231140c2940613249a07a7767a4f3cc3a0b0d70
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 16:06:34 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields entries1 (line 79), entries2 (line 79), values1 (line 80), values2 (line 80), keys1 (line 81), keys2 (line 81) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/IntIntMap.java:105   public IntIntMap(int initialCapacity, float loadFactor) {

--- gdx/src/com/badlogic/gdx/utils/IntIntMap.java
+++ gdx/src/com/badlogic/gdx/utils/IntIntMap.java
@@ -114,6 +114,12 @@ public class IntIntMap implements Iterable<IntIntMap.Entry> {
 
     keyTable = new int[tableSize];
     valueTable = new int[tableSize];
+    entries1 = new Entries(this);
+    entries2 = new Entries(this);
+    values1 = new Values(this);
+    values2 = new Values(this);
+    keys1 = new Keys(this);
+    keys2 = new Keys(this);
   }
 
   /** Creates a new map identical to the specified map. */
