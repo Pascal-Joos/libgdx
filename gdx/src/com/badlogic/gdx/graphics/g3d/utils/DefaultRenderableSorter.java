784593e1eb79be4959969a5faefeb1ef2c7b153f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 14:24:04 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression camera is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java:63         (int) (1000f * camera.position.dst2(tmpV1)) - (int) (1000f * camera.position.dst2(tmpV2));

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java
@@ -59,6 +59,7 @@ public class DefaultRenderableSorter implements RenderableSorter, Comparator<Ren
     // o1.material.equals(o2.material);
     getTranslation(o1.worldTransform, o1.meshPart.center, tmpV1);
     getTranslation(o2.worldTransform, o2.meshPart.center, tmpV2);
+    if (camera == null) return 0;
     final float dst =
         (int) (1000f * camera.position.dst2(tmpV1)) - (int) (1000f * camera.position.dst2(tmpV2));
     final int result = dst < 0 ? -1 : (dst > 0 ? 1 : 0);
