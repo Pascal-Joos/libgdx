672c0d843a359da247df7a0b5bb23c46ede84798
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 16:13:46 +0000

    fix: Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/JsonValue.java:1070     this.name = name;

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1066,7 +1066,13 @@ public class JsonValue implements Iterable<JsonValue> {
   /**
    * @param name May be null.
    */
+  /**
+   * @param name May be null.
+   */
   public void setName(@Nullable @Null String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("name cannot be null");
+    }
     this.name = name;
   }
 
