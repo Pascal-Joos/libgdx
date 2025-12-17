7a401d31b3ddb26a7143532c0916d2b1a8de6535
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 14:05:34 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields positionChannel (line 36), previousPositionChannel (line 36), rotationChannel (line 36) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsInfluencer.java:58   @Initializer

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsInfluencer.java
@@ -69,6 +69,9 @@ public class DynamicsInfluencer extends Influencer {
     if (hasAcceleration) {
       positionChannel = controller.particles.addChannel(ParticleChannels.Position);
       previousPositionChannel = controller.particles.addChannel(ParticleChannels.PreviousPosition);
+    } else {
+      positionChannel = controller.particles.addChannel(ParticleChannels.Position);
+      previousPositionChannel = controller.particles.addChannel(ParticleChannels.PreviousPosition);
     }
 
     // Angular velocity check
@@ -82,6 +85,7 @@ public class DynamicsInfluencer extends Influencer {
       has3dAngularVelocity = angularVelocityChannel != null;
       if (has3dAngularVelocity)
         rotationChannel = controller.particles.addChannel(ParticleChannels.Rotation3D);
+      else rotationChannel = controller.particles.addChannel(ParticleChannels.Rotation2D);
     }
   }
 
