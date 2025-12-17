a8cf4b687be50085cff60fb143e4175c6350bd54
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:07:25 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression loadFuture is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java:154     else if (loadFuture.isDone()) {

--- gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java
+++ gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java
@@ -151,7 +151,7 @@ class AssetLoadingTask implements AsyncTask<Void> {
       asset =
           asyncLoader.loadSync(
               manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
-    else if (loadFuture.isDone()) {
+    else if (loadFuture != null && loadFuture.isDone()) {
       try {
         loadFuture.get();
       } catch (Exception e) {
