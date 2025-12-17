ad2a0b1887ec4b428280119758d4fc60a8f68b4c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 14:58:18 +0000

    fix: Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java:1000     expandX = null;

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -986,6 +986,14 @@ public class Cell<T extends Actor> implements Poolable {
   /** Sets all constraint fields to null. */
   /** Sets all constraint fields to null. */
   /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
+  /** Sets all constraint fields to null. */
   void clear() {
     minWidth = null;
     minHeight = null;
@@ -1004,8 +1012,8 @@ public class Cell<T extends Actor> implements Poolable {
     fillX = null;
     fillY = null;
     align = null;
-    expandX = null;
-    expandY = null;
+    expandX = 0;
+    expandY = 0;
     colspan = null;
     uniformX = null;
     uniformY = null;
