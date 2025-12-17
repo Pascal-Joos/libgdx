d878f80f450d718f4e37ff62309ff8a2977ca786
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 14:09:05 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression rotationalVelocity2dChannel is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java:233               c = i + controller.particles.size * rotationalVelocity2dChannel.strideSize;

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -228,6 +228,11 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
+      if (rotationalVelocity2dChannel == null
+          || controller == null
+          || controller.particles == null) {
+        return;
+      }
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               s = 0,
