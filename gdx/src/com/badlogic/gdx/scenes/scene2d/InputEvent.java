2835d587ed1aa68b17d3628f6b60dc3c80f0d865
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 14:46:17 +0000

    fix: Type='FIELD_NO_INIT', message='@NonNull field type not initialized' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/InputEvent.java:31   private Type type;

--- gdx/src/com/badlogic/gdx/scenes/scene2d/InputEvent.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/InputEvent.java
@@ -28,7 +28,7 @@ import javax.annotation.Nullable;
  * @see InputListener
  */
 public class InputEvent extends Event {
-  private Type type;
+  private Type type = Type.touchDown;
   private float stageX, stageY, scrollAmountX, scrollAmountY;
   private int pointer, button, keyCode;
   private char character;
