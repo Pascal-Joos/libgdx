e67c3b68e58451ff0c9bff78ba6394eac251481a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 16:09:29 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/Json.java:1396     return null;

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -1394,7 +1394,7 @@ public class Json {
           "Unable to convert value to required type: " + jsonData + " (" + type.getName() + ")");
     }
 
-    return null;
+    throw new SerializationException("Unable to convert value to required type: " + jsonData);
   }
 
   /**
