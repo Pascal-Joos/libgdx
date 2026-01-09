7c5ca71a7e1b99271669137338c58bad7be08372
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 13:05:40 +0000

    fix: 180 - METHOD_NO_INIT - public ETC1TextureData(FileHandle file, boolean useMipMaps) { - initializer method does not guarantee @NonNull field data (line 31) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/ETC1TextureData.java
@@ -24,11 +24,12 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.graphics.glutils.ETC1.ETC1Data;
 import com.badlogic.gdx.utils.GdxRuntimeException;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 public class ETC1TextureData implements TextureData {
   @Nullable FileHandle file;
-  ETC1Data data;
+  @Nullable ETC1Data data;
   boolean useMipMaps;
   int width = 0;
   int height = 0;
@@ -66,8 +67,9 @@ public class ETC1TextureData implements TextureData {
     if (file != null) {
       data = new ETC1Data(file);
     }
-    width = data.width;
-    height = data.height;
+    ETC1Data nonnullData = Nullability.castToNonnull(data);
+    width = nonnullData.width;
+    height = nonnullData.height;
     isPrepared = true;
   }
 
@@ -75,6 +77,8 @@ public class ETC1TextureData implements TextureData {
   public void consumeCustomData(int target) {
     if (!isPrepared)
       throw new GdxRuntimeException("Call prepare() before calling consumeCompressedData()");
+
+    if (data == null) throw new GdxRuntimeException("ETC1Data must not be null");
 
     if (!Gdx.graphics.supportsExtension("GL_OES_compressed_ETC1_RGB8_texture")) {
       Pixmap pixmap = ETC1.decodeImage(data, Format.RGB565);
@@ -92,7 +96,7 @@ public class ETC1TextureData implements TextureData {
         MipMapGenerator.generateMipMap(target, pixmap, pixmap.getWidth(), pixmap.getHeight());
       pixmap.dispose();
       useMipMaps = false;
-    } else {
+    } else if (data != null) {
       Gdx.gl.glCompressedTexImage2D(
           target,
           0,
@@ -104,7 +108,7 @@ public class ETC1TextureData implements TextureData {
           data.compressedData);
       if (useMipMaps()) Gdx.gl20.glGenerateMipmap(GL20.GL_TEXTURE_2D);
     }
-    data.dispose();
+    if (data != null) Nullability.castToNonnull(data).dispose();
     data = null;
     isPrepared = false;
   }
