c11fe02a787fde8d50237c3b94d3314351bbdade
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 13:03:46 +0000

    fix: 179 - ASSIGN_FIELD_NULLABLE - renderable.material = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
@@ -41,6 +41,7 @@ import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -131,14 +132,25 @@ public class PointSpriteParticleBatch
   }
 
   public void setTexture(Texture texture) {
+    if (renderable.material == null) {
+      return;
+    }
     TextureAttribute attribute =
-        (TextureAttribute) renderable.material.get(TextureAttribute.Diffuse);
+        (TextureAttribute)
+            Nullability.castToNonnull(renderable.material).get(TextureAttribute.Diffuse);
+    if (attribute == null) {
+      return;
+    }
     attribute.textureDescription.texture = texture;
   }
 
   public Texture getTexture() {
+    if (renderable.material == null) {
+      return null;
+    }
     TextureAttribute attribute =
-        (TextureAttribute) renderable.material.get(TextureAttribute.Diffuse);
+        (TextureAttribute)
+            Nullability.castToNonnull(renderable.material).get(TextureAttribute.Diffuse);
     return attribute.textureDescription.texture;
   }
 
