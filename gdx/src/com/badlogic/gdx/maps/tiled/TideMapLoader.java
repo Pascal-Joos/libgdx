3cc958c1894d5bc94a1319da7cc8aa29148e5b0d
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 13:26:15 +0000

    Revert "fix: 194 - DEREFERENCE_NULLABLE - Element properties = root.getChildByName("Properties"); - dereferenced expression root is @Nullable"
    This reverts commit df6f38cb8c1d0d02a2e61bea3c684ec72541b41d.

--- gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java
@@ -37,7 +37,6 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.XmlReader;
 import com.badlogic.gdx.utils.XmlReader.Element;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import javax.annotation.Nullable;
@@ -114,15 +113,15 @@ public class TideMapLoader extends SynchronousAssetLoader<TiledMap, TideMapLoade
   private TiledMap loadMap(
       @Nullable Element root, FileHandle tmxFile, ImageResolver imageResolver) {
     TiledMap map = new TiledMap();
-    Element properties = Nullability.castToNonnull(root).getChildByName("Properties");
+    Element properties = root.getChildByName("Properties");
     if (properties != null) {
       loadProperties(map.getProperties(), properties);
     }
-    Element tilesheets = Nullability.castToNonnull(root).getChildByName("TileSheets");
+    Element tilesheets = root.getChildByName("TileSheets");
     for (Element tilesheet : tilesheets.getChildrenByName("TileSheet")) {
       loadTileSheet(map, tilesheet, tmxFile, imageResolver);
     }
-    Element layers = Nullability.castToNonnull(root).getChildByName("Layers");
+    Element layers = root.getChildByName("Layers");
     for (Element layer : layers.getChildrenByName("Layer")) {
       loadLayer(map, layer);
     }
