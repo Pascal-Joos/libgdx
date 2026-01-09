130f3aa9ba0732909a12d4a4a394f3794a0ba97e
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 19:43:56 +0000

    fix: 363 - DEREFERENCE_NULLABLE - target.actor.stageToLocalCoordinates(tmpVector.set(stageX, stageY)); - dereferenced expression target is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/DragAndDrop.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/DragAndDrop.java
@@ -133,8 +133,10 @@ public class DragAndDrop {
               if (oldDragActor != null && removeDragActor) oldDragActor.remove();
               dragActor = actor;
               removeDragActor =
-                  actor.getStage() == null; // Only remove later if not already in the stage now.
-              if (removeDragActor) stage.addActor(actor);
+                  actor != null
+                      && actor.getStage()
+                          == null; // Only remove later if not already in the stage now.
+              if (removeDragActor && actor != null) stage.addActor(actor);
             }
             if (actor == null) return;
 
@@ -165,7 +167,7 @@ public class DragAndDrop {
               isValidTarget = target.drag(source, payload, tmpVector.x, tmpVector.y, pointer);
             }
             if (dragActor != null && removeDragActor) dragActor.remove();
-            if (isValidTarget) {
+            if (isValidTarget && target != null) {
               float stageX = event.getStageX() + touchOffsetX,
                   stageY = event.getStageY() + touchOffsetY;
               target.actor.stageToLocalCoordinates(tmpVector.set(stageX, stageY));
