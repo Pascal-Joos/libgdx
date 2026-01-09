127920c26fb53d6a196d5a477d55529b0af86b0d
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 18:34:39 +0000

    fix: 313 - PASS_NULLABLE - float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a); - passing @Nullable parameter 'a' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -33,6 +33,7 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import javax.annotation.Nullable;
 
@@ -892,21 +893,35 @@ public class Table extends WidgetGroup {
       // Compute combined padding/spacing for cells.
       // Spacing between actors isn't additive, the larger is used. Also, no spacing around edges.
       c.computedPadLeft =
-          c.padLeft.get(a) + (column == 0 ? 0 : Math.max(0, c.spaceLeft.get(a) - spaceRightLast));
-      c.computedPadTop = c.padTop.get(a);
+          c.padLeft.get(Nullability.castToNonnull(a))
+              + (column == 0
+                  ? 0
+                  : Math.max(0, c.spaceLeft.get(Nullability.castToNonnull(a)) - spaceRightLast));
+      c.computedPadTop = c.padTop.get(Nullability.castToNonnull(a));
       if (c.cellAboveIndex != -1) {
         Cell above = (Cell) cells[c.cellAboveIndex];
-        c.computedPadTop += Math.max(0, c.spaceTop.get(a) - above.spaceBottom.get(a));
+        c.computedPadTop +=
+            Math.max(
+                0,
+                c.spaceTop.get(Nullability.castToNonnull(a))
+                    - above.spaceBottom.get(Nullability.castToNonnull(a)));
       }
-      float spaceRight = c.spaceRight.get(a);
-      c.computedPadRight = c.padRight.get(a) + ((column + colspan) == columns ? 0 : spaceRight);
-      c.computedPadBottom = c.padBottom.get(a) + (row == rows - 1 ? 0 : c.spaceBottom.get(a));
+      float spaceRight = c.spaceRight.get(Nullability.castToNonnull(a));
+      c.computedPadRight =
+          c.padRight.get(Nullability.castToNonnull(a))
+              + ((column + colspan) == columns ? 0 : spaceRight);
+      c.computedPadBottom =
+          c.padBottom.get(Nullability.castToNonnull(a))
+              + (row == rows - 1 ? 0 : c.spaceBottom.get(Nullability.castToNonnull(a)));
       spaceRightLast = spaceRight;
 
       // Determine minimum and preferred cell sizes.
-      float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
-      float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
-      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
+      float prefWidth = c.prefWidth.get(Nullability.castToNonnull(a)),
+          prefHeight = c.prefHeight.get(Nullability.castToNonnull(a));
+      float minWidth = c.minWidth.get(Nullability.castToNonnull(a)),
+          minHeight = c.minHeight.get(Nullability.castToNonnull(a));
+      float maxWidth = c.maxWidth.get(Nullability.castToNonnull(a)),
+          maxHeight = c.maxHeight.get(Nullability.castToNonnull(a));
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
@@ -983,9 +998,9 @@ public class Table extends WidgetGroup {
       int column = c.column;
 
       Actor a = c.actor;
-      float minWidth = c.minWidth.get(a),
-          prefWidth = c.prefWidth.get(a),
-          maxWidth = c.maxWidth.get(a);
+      float minWidth = c.minWidth.get(Nullability.castToNonnull(a)),
+          prefWidth = c.prefWidth.get(Nullability.castToNonnull(a)),
+          maxWidth = c.maxWidth.get(Nullability.castToNonnull(a));
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
       if (round) {
@@ -1025,7 +1040,7 @@ public class Table extends WidgetGroup {
     tablePrefHeight = vpadding;
     for (int i = 0; i < rows; i++) {
       tableMinHeight += rowMinHeight[i];
-      tablePrefHeight += Math.max(rowMinHeight[i], rowPrefHeight[i]);
+      tablePrefHeight = Math.max(tablePrefHeight, rowPrefHeight[i] + vpadding);
     }
     tablePrefWidth = Math.max(tableMinWidth, tablePrefWidth);
     tablePrefHeight = Math.max(tableMinHeight, tablePrefHeight);
