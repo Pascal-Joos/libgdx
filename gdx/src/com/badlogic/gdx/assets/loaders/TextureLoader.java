4c585255356a0c982225b1fb715d7cdbb170c26b
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 08:53:51 +0000

    Revert "fix: 18 - DEREFERENCE_NULLABLE - if (!info.data.isPrepared()) info.data.prepare(); - dereferenced expression info.data is @Nullable"
    This reverts commit bff0ec50a2702919278f679f43bdd786729b6c11.

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
@@ -75,7 +75,7 @@ public class TextureLoader
       info.data = parameter.textureData;
       info.texture = parameter.texture;
     }
-    if (info != null && info.data != null && !info.data.isPrepared()) info.data.prepare();
+    if (!info.data.isPrepared()) info.data.prepare();
   }
 
   @Nullable
@@ -85,7 +85,7 @@ public class TextureLoader
       String fileName,
       FileHandle file,
       @Nullable TextureParameter parameter) {
-    if (info == null || info.data == null) return null;
+    if (info == null) return null;
     Texture texture = info.texture;
     if (texture != null) {
       texture.load(info.data);
