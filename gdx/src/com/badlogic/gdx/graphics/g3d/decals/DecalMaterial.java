80f4fc162f5ee7fecc472ddab06eaade16e1eed3
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:39:21 +0000

    Revert "fix: 87 - FIELD_NO_INIT - protected TextureRegion textureRegion; - @NonNull field textureRegion not initialized"
    This reverts commit 86c72947bf2024031f477641aa6ca0a8e9060bb4.

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/DecalMaterial.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/DecalMaterial.java
@@ -18,12 +18,11 @@ package com.badlogic.gdx.graphics.g3d.decals;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import javax.annotation.Nullable;
 
 /** Material used by the {@link Decal} class */
 public class DecalMaterial {
   public static final int NO_BLEND = -1;
-  @Nullable protected TextureRegion textureRegion;
+  protected TextureRegion textureRegion;
   protected int srcBlendFactor;
   protected int dstBlendFactor;
 
@@ -32,9 +31,6 @@ public class DecalMaterial {
    * used by it.
    */
   public void set() {
-    if (textureRegion == null) {
-      return;
-    }
     textureRegion.getTexture().bind(0);
     if (!isOpaque()) {
       Gdx.gl.glBlendFunc(srcBlendFactor, dstBlendFactor);
@@ -65,19 +61,11 @@ public class DecalMaterial {
 
     return dstBlendFactor == material.dstBlendFactor
         && srcBlendFactor == material.srcBlendFactor
-        && ((textureRegion == null && material.textureRegion == null)
-            || (textureRegion != null
-                && material.textureRegion != null
-                && textureRegion.getTexture() != null
-                && material.textureRegion.getTexture() != null
-                && textureRegion.getTexture() == material.textureRegion.getTexture()));
+        && textureRegion.getTexture() == material.textureRegion.getTexture();
   }
 
   @Override
   public int hashCode() {
-    if (textureRegion == null) {
-      return 1;
-    }
     int result = textureRegion.getTexture() != null ? textureRegion.getTexture().hashCode() : 0;
     result = 31 * result + srcBlendFactor;
     result = 31 * result + dstBlendFactor;
