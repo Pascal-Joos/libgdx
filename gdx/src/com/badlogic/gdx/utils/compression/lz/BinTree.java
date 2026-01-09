d9b05de92a6f0d260749fa4bbc9ead3f3d47633c
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 22:53:39 +0000

    Revert "fix: 439 - ASSIGN_FIELD_NULLABLE - _bufferBase = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit fbbf48314429673c42413754e2cc5ecdc3752c2c.

--- gdx/src/com/badlogic/gdx/utils/compression/lz/BinTree.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/BinTree.java
@@ -2,7 +2,6 @@
 
 package com.badlogic.gdx.utils.compression.lz;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 
 public class BinTree extends InWindow {
@@ -94,10 +93,6 @@ public class BinTree extends InWindow {
   }
 
   public int GetMatches(int[] distances) throws IOException {
-    if (_bufferBase == null) {
-      return 0;
-    }
-
     int lenLimit;
     if (_pos + _matchMaxLen <= _streamPos) lenLimit = _matchMaxLen;
     else {
@@ -114,18 +109,13 @@ public class BinTree extends InWindow {
     int maxLen = kStartMaxLen; // to avoid items for len < hashSize;
     int hashValue, hash2Value = 0, hash3Value = 0;
 
-    byte[] bufferBase = Nullability.castToNonnull(_bufferBase);
-
     if (HASH_ARRAY) {
-      int temp = CrcTable[bufferBase[cur] & 0xFF] ^ (bufferBase[cur + 1] & 0xFF);
+      int temp = CrcTable[_bufferBase[cur] & 0xFF] ^ (_bufferBase[cur + 1] & 0xFF);
       hash2Value = temp & (kHash2Size - 1);
-      temp ^= ((int) (bufferBase[cur + 2] & 0xFF) << 8);
+      temp ^= ((int) (_bufferBase[cur + 2] & 0xFF) << 8);
       hash3Value = temp & (kHash3Size - 1);
-      hashValue = (temp ^ (CrcTable[bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
-    } else
-      hashValue =
-          ((Nullability.castToNonnull(_bufferBase)[cur] & 0xFF)
-              ^ ((int) (Nullability.castToNonnull(_bufferBase)[cur + 1] & 0xFF) << 8));
+      hashValue = (temp ^ (CrcTable[_bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
+    } else hashValue = ((_bufferBase[cur] & 0xFF) ^ ((int) (_bufferBase[cur + 1] & 0xFF) << 8));
 
     int curMatch = _hash[kFixHashSize + hashValue];
     if (HASH_ARRAY) {
@@ -134,12 +124,12 @@ public class BinTree extends InWindow {
       _hash[hash2Value] = _pos;
       _hash[kHash3Offset + hash3Value] = _pos;
       if (curMatch2 > matchMinPos)
-        if (bufferBase[_bufferOffset + curMatch2] == bufferBase[cur]) {
+        if (_bufferBase[_bufferOffset + curMatch2] == _bufferBase[cur]) {
           distances[offset++] = maxLen = 2;
           distances[offset++] = _pos - curMatch2 - 1;
         }
       if (curMatch3 > matchMinPos)
-        if (bufferBase[_bufferOffset + curMatch3] == bufferBase[cur]) {
+        if (_bufferBase[_bufferOffset + curMatch3] == _bufferBase[cur]) {
           if (curMatch3 == curMatch2) offset -= 2;
           distances[offset++] = maxLen = 3;
           distances[offset++] = _pos - curMatch3 - 1;
@@ -161,8 +151,8 @@ public class BinTree extends InWindow {
 
     if (kNumHashDirectBytes != 0) {
       if (curMatch > matchMinPos) {
-        if (bufferBase[_bufferOffset + curMatch + kNumHashDirectBytes]
-            != bufferBase[cur + kNumHashDirectBytes]) {
+        if (_bufferBase[_bufferOffset + curMatch + kNumHashDirectBytes]
+            != _bufferBase[cur + kNumHashDirectBytes]) {
           distances[offset++] = maxLen = kNumHashDirectBytes;
           distances[offset++] = _pos - curMatch - 1;
         }
@@ -185,19 +175,19 @@ public class BinTree extends InWindow {
 
       int pby1 = _bufferOffset + curMatch;
       int len = Math.min(len0, len1);
-      if (bufferBase[pby1 + len] == bufferBase[cur + len]) {
-        while (++len != lenLimit) if (bufferBase[pby1 + len] != bufferBase[cur + len]) break;
+      if (_bufferBase[pby1 + len] == _bufferBase[cur + len]) {
+        while (++len != lenLimit) if (_bufferBase[pby1 + len] != _bufferBase[cur + len]) break;
         if (maxLen < len) {
           distances[offset++] = maxLen = len;
           distances[offset++] = delta - 1;
           if (len == lenLimit) {
             _son[ptr1] = _son[cyclicPos];
-            _son[ptr0] = _son[cylicPos + 1];
+            _son[ptr0] = _son[cyclicPos + 1];
             break;
           }
         }
       }
-      if ((bufferBase[pby1 + len] & 0xFF) < (bufferBase[cur + len] & 0xFF)) {
+      if ((_bufferBase[pby1 + len] & 0xFF) < (_bufferBase[cur + len] & 0xFF)) {
         _son[ptr1] = curMatch;
         ptr1 = cyclicPos + 1;
         curMatch = _son[ptr1];
@@ -214,8 +204,6 @@ public class BinTree extends InWindow {
   }
 
   public void Skip(int num) throws IOException {
-    if (_bufferBase == null) return;
-    byte[] nonNullBufferBase = Nullability.castToNonnull(_bufferBase);
     do {
       int lenLimit;
       if (_pos + _matchMaxLen <= _streamPos) lenLimit = _matchMaxLen;
@@ -233,19 +221,14 @@ public class BinTree extends InWindow {
       int hashValue;
 
       if (HASH_ARRAY) {
-        int temp = CrcTable[nonNullBufferBase[cur] & 0xFF] ^ (nonNullBufferBase[cur + 1] & 0xFF);
+        int temp = CrcTable[_bufferBase[cur] & 0xFF] ^ (_bufferBase[cur + 1] & 0xFF);
         int hash2Value = temp & (kHash2Size - 1);
         _hash[hash2Value] = _pos;
-        temp ^= ((int) (nonNullBufferBase[cur + 2] & 0xFF) << 8);
+        temp ^= ((int) (_bufferBase[cur + 2] & 0xFF) << 8);
         int hash3Value = temp & (kHash3Size - 1);
         _hash[kHash3Offset + hash3Value] = _pos;
-        hashValue = (temp ^ (CrcTable[nonNullBufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
-      } else if (nonNullBufferBase != null) {
-        hashValue =
-            ((nonNullBufferBase[cur] & 0xFF) ^ ((int) (nonNullBufferBase[cur + 1] & 0xFF) << 8));
-      } else {
-        return;
-      }
+        hashValue = (temp ^ (CrcTable[_bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
+      } else hashValue = ((_bufferBase[cur] & 0xFF) ^ ((int) (_bufferBase[cur + 1] & 0xFF) << 8));
 
       int curMatch = _hash[kFixHashSize + hashValue];
       _hash[kFixHashSize + hashValue] = _pos;
@@ -272,16 +255,15 @@ public class BinTree extends InWindow {
 
         int pby1 = _bufferOffset + curMatch;
         int len = Math.min(len0, len1);
-        if (nonNullBufferBase[pby1 + len] == nonNullBufferBase[cur + len]) {
-          while (++len != lenLimit)
-            if (nonNullBufferBase[pby1 + len] != nonNullBufferBase[cur + len]) break;
+        if (_bufferBase[pby1 + len] == _bufferBase[cur + len]) {
+          while (++len != lenLimit) if (_bufferBase[pby1 + len] != _bufferBase[cur + len]) break;
           if (len == lenLimit) {
             _son[ptr1] = _son[cyclicPos];
             _son[ptr0] = _son[cyclicPos + 1];
             break;
           }
         }
-        if ((nonNullBufferBase[pby1 + len] & 0xFF) < (nonNullBufferBase[cur + len] & 0xFF)) {
+        if ((_bufferBase[pby1 + len] & 0xFF) < (_bufferBase[cur + len] & 0xFF)) {
           _son[ptr1] = curMatch;
           ptr1 = cyclicPos + 1;
           curMatch = _son[ptr1];
--- gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
@@ -2,12 +2,11 @@
 
 package com.badlogic.gdx.utils.compression.lz;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import javax.annotation.Nullable;
 
 public class InWindow {
-  @Nullable public byte[] _bufferBase; // pointer to buffer with data
+  public byte[] _bufferBase; // pointer to buffer with data
   @Nullable java.io.InputStream _stream;
   int _posLimit; // offset (from _buffer) of first byte when new block reading must be done
   boolean _streamEndWasReached; // if (true) then _streamPos shows real end of stream
@@ -23,7 +22,6 @@ public class InWindow {
   public int _streamPos; // offset (from _buffer) of first not read byte from Stream
 
   public void MoveBlock() {
-    if (_bufferBase == null) return;
     int offset = _bufferOffset + _pos - _keepSizeBefore;
     // we need one additional byte, since MovePos moves on 1 byte.
     if (offset > 0) offset--;
@@ -31,10 +29,7 @@ public class InWindow {
     int numBytes = _bufferOffset + _streamPos - offset;
 
     // check negative offset ????
-    byte[] bufferBase = Nullability.castToNonnull(_bufferBase);
-    for (int i = 0; i < numBytes; i++)
-      Nullability.castToNonnull(_bufferBase)[i] =
-          Nullability.castToNonnull(_bufferBase)[offset + i];
+    for (int i = 0; i < numBytes; i++) _bufferBase[i] = _bufferBase[offset + i];
     _bufferOffset -= offset;
   }
 
@@ -100,15 +95,11 @@ public class InWindow {
   }
 
   public byte GetIndexByte(int index) {
-    if (_bufferBase == null) {
-      throw new IllegalStateException("Buffer not initialized");
-    }
-    return Nullability.castToNonnull(_bufferBase)[_bufferOffset + _pos + index];
+    return _bufferBase[_bufferOffset + _pos + index];
   }
 
   // index + limit have not to exceed _keepSizeAfter;
   public int GetMatchLen(int index, int distance, int limit) {
-    if (_bufferBase == null) return 0;
     if (_streamEndWasReached)
       if ((_pos + index) + limit > _streamPos) limit = _streamPos - (_pos + index);
     distance++;
@@ -116,11 +107,7 @@ public class InWindow {
     int pby = _bufferOffset + _pos + index;
 
     int i;
-    for (i = 0;
-        i < limit
-            && Nullability.castToNonnull(_bufferBase)[pby + i]
-                == Nullability.castToNonnull(_bufferBase)[pby + i - distance];
-        i++)
+    for (i = 0; i < limit && _bufferBase[pby + i] == _bufferBase[pby + i - distance]; i++)
       ;
     return i;
   }
