8e97c80476c649d65f9a4a1905e333d71ac49247
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 13:01:46 +0000

    Revert "fix: 177 - FIELD_NO_INIT - private static short[] indices; - @NonNull static field indices not initialized"
    This reverts commit b4babf2e7b75ec7b1fe7d30d0fec0670e2c68e06.

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java
@@ -24,8 +24,6 @@ import com.badlogic.gdx.graphics.g3d.RenderableProvider;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.FlushablePool;
-import edu.ucr.cs.riple.annotator.util.Nullability;
-import javax.annotation.Nullable;
 
 /**
  * RenderableShapeBuilder builds various properties of a renderable.
@@ -56,7 +54,7 @@ public class RenderableShapeBuilder extends BaseShapeBuilder {
     }
   }
 
-  @Nullable private static short[] indices;
+  private static short[] indices;
   private static float[] vertices;
   private static final RenderablePool renderablesPool = new RenderablePool();
   private static final Array<Renderable> renderables = new Array<Renderable>();
@@ -155,12 +153,7 @@ public class RenderableShapeBuilder extends BaseShapeBuilder {
     if (mesh.getNumIndices() > 0) {
       // Get min vertice to max vertice in indices array
       ensureIndicesCapacity(mesh.getNumIndices());
-      if (indices == null) return;
-      mesh.getIndices(
-          renderable.meshPart.offset,
-          renderable.meshPart.size,
-          Nullability.castToNonnull(indices),
-          0);
+      mesh.getIndices(renderable.meshPart.offset, renderable.meshPart.size, indices, 0);
 
       short minVertice = minVerticeInIndices();
       short maxVertice = maxVerticeInIndices();
@@ -243,17 +236,14 @@ public class RenderableShapeBuilder extends BaseShapeBuilder {
   }
 
   private static short minVerticeInIndices() {
-    if (indices == null) return (short) 32767;
     short min = (short) 32767;
     for (int i = 0; i < indices.length; i++) if (indices[i] < min) min = indices[i];
     return min;
   }
 
   private static short maxVerticeInIndices() {
-    if (indices == null) return (short) -32768;
     short max = (short) -32768;
-    for (int i = 0; i < Nullability.castToNonnull(indices).length; i++)
-      if (Nullability.castToNonnull(indices)[i] > max) max = Nullability.castToNonnull(indices)[i];
+    for (int i = 0; i < indices.length; i++) if (indices[i] > max) max = indices[i];
     return max;
   }
 }
