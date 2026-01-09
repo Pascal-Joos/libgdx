fa87aac057581b33fc774496586be6ab65b51208
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 14:19:06 +0000

    Revert "fix: 224 - ASSIGN_FIELD_NULLABLE - color = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 9c93c5c4a6b3756d6848dc98fe3f6ad139e973d0.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AlphaAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AlphaAction.java
@@ -19,7 +19,6 @@ package com.badlogic.gdx.scenes.scene2d.actions;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Null;
-import javax.annotation.Nullable;
 
 /**
  * Sets the alpha for an actor's color (or a specified color), from the current alpha to the new
@@ -30,7 +29,7 @@ import javax.annotation.Nullable;
  */
 public class AlphaAction extends TemporalAction {
   private float start, end;
-  @Nullable private @Null Color color;
+  private @Null Color color;
 
   protected void begin() {
     if (color == null) color = target.getColor();
@@ -38,7 +37,6 @@ public class AlphaAction extends TemporalAction {
   }
 
   protected void update(float percent) {
-    if (color == null) color = target.getColor();
     if (percent == 0) color.a = start;
     else if (percent == 1) color.a = end;
     else color.a = start + (end - start) * percent;
@@ -49,7 +47,6 @@ public class AlphaAction extends TemporalAction {
     color = null;
   }
 
-  @Nullable
   public @Null Color getColor() {
     return color;
   }
