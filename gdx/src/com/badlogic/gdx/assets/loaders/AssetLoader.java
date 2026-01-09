8026a28aafd1745af9cabb688697740b62450bd0
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 08:48:13 +0000

    fix: 14 - DEREFERENCE_NULLABLE - return resolver.resolve(fileName); - dereferenced expression resolver is @Nullable

--- gdx/src/com/badlogic/gdx/assets/loaders/AssetLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/AssetLoader.java
@@ -50,6 +50,9 @@ public abstract class AssetLoader<T, P extends AssetLoaderParameters<T>> {
    * @return handle to the file, as resolved by the {@link FileHandleResolver} set on the loader
    */
   public FileHandle resolve(String fileName) {
+    if (resolver == null) {
+      throw new IllegalStateException("resolver cannot be null");
+    }
     return resolver.resolve(fileName);
   }
 
