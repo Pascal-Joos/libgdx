0dbfaf1b2c1f4899f4acaa964ba5d1481a80714b
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:40:43 +0000

    Revert "fix: 88 - DEREFERENCE_NULLABLE - plugs.get(group).afterGroup(); - dereferenced expression plugs.get(group) is @Nullable"
    This reverts commit 2300c03685815afce0cfe1cb4323680ff18d052c.

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
@@ -34,7 +34,7 @@ public abstract class PluggableGroupStrategy implements GroupStrategy {
 
   @Override
   public void afterGroup(int group) {
-    if (plugs.get(group) != null) plugs.get(group).afterGroup();
+    plugs.get(group).afterGroup();
   }
 
   /**
