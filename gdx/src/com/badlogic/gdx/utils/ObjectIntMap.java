78050dea500cc565d801db053628b4f1013ff739
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 21:42:04 +0000

    Revert "fix: 403 - METHOD_NO_INIT - public ObjectIntMap(int initialCapacity, float loadFactor) { - initializer method does not guarantee @NonNull fields entries1 (line 77), entries2 (line 77), values1 (line 78), values2 (line 78), keys1 (line 79), keys2 (line 79) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 6ab59bcf67d398d507b2f1b02bd83b0dcbf79706.

--- gdx/src/com/badlogic/gdx/utils/ObjectIntMap.java
+++ gdx/src/com/badlogic/gdx/utils/ObjectIntMap.java
@@ -18,7 +18,6 @@ package com.badlogic.gdx.utils;
 
 import static com.badlogic.gdx.utils.ObjectSet.tableSize;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -75,9 +74,9 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
    */
   protected int mask;
 
-  @Nullable transient Entries entries1, entries2;
-  @Nullable transient Values values1, values2;
-  @Nullable transient Keys keys1, keys2;
+  transient Entries entries1, entries2;
+  transient Values values1, values2;
+  transient Keys keys1, keys2;
 
   /** Creates a new map with an initial capacity of 51 and a load factor of 0.8. */
   public ObjectIntMap() {
@@ -433,7 +432,6 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
    * each time this method is called. Use the {@link Entries} constructor for nested or
    * multithreaded iteration.
    */
-  @SuppressWarnings("NullAway")
   public Entries<K> entries() {
     if (Collections.allocateIterators) return new Entries(this);
     if (entries1 == null) {
@@ -443,11 +441,11 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
     if (!entries1.valid) {
       entries1.reset();
       entries1.valid = true;
-      Nullability.castToNonnull(entries2).valid = false;
+      entries2.valid = false;
       return entries1;
     }
-    Nullability.castToNonnull(entries2).reset();
-    Nullability.castToNonnull(entries2).valid = true;
+    entries2.reset();
+    entries2.valid = true;
     entries1.valid = false;
     return entries2;
   }
@@ -459,7 +457,6 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
    * each time this method is called. Use the {@link Values} constructor for nested or multithreaded
    * iteration.
    */
-  @SuppressWarnings("NullAway")
   public Values values() {
     if (Collections.allocateIterators) return new Values(this);
     if (values1 == null) {
@@ -469,11 +466,11 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
     if (!values1.valid) {
       values1.reset();
       values1.valid = true;
-      Nullability.castToNonnull(values2).valid = false;
+      values2.valid = false;
       return values1;
     }
-    Nullability.castToNonnull(values2).reset();
-    Nullability.castToNonnull(values2).valid = true;
+    values2.reset();
+    values2.valid = true;
     values1.valid = false;
     return values2;
   }
@@ -485,7 +482,6 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
    * each time this method is called. Use the {@link Keys} constructor for nested or multithreaded
    * iteration.
    */
-  @Nullable
   public Keys<K> keys() {
     if (Collections.allocateIterators) return new Keys(this);
     if (keys1 == null) {
@@ -495,11 +491,11 @@ public class ObjectIntMap<K> implements Iterable<ObjectIntMap.Entry<K>> {
     if (!keys1.valid) {
       keys1.reset();
       keys1.valid = true;
-      Nullability.castToNonnull(keys2).valid = false;
+      keys2.valid = false;
       return keys1;
     }
-    Nullability.castToNonnull(keys2).reset();
-    Nullability.castToNonnull(keys2).valid = true;
+    keys2.reset();
+    keys2.valid = true;
     keys1.valid = false;
     return keys2;
   }
