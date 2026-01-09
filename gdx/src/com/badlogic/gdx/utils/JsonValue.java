b523b426b7e741371349e6157b6e40353f9036ef
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 18:14:10 +0000

    Revert "fix: 290 - DEREFERENCE_NULLABLE - se.addTrace(jsonMap.child.trace()); - dereferenced expression jsonMap.child is @Nullable"
    This reverts commit a7e40ff69d3cebbbfb95e4ceec286da958dca942.

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1469,7 +1469,6 @@ public class JsonValue implements Iterable<JsonValue> {
     }
 
     public void remove() {
-      if (current == null) return;
       if (current.prev == null) {
         child = current.next;
         if (child != null) child.prev = null;
