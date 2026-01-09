7a4848e583469b5863992f0654e74eba9d250795
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 09:35:11 +0000

    fix: 41 - DEREFERENCE_NULLABLE - float spriteWidth = sprite.getWidth(); - dereferenced expression sprite is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -23,6 +23,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.utils.Array;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Writer;
@@ -439,8 +440,8 @@ public class ParticleEmitter {
       particle.angleSin = MathUtils.sinDeg(angle);
     }
 
-    float spriteWidth = sprite.getWidth();
-    float spriteHeight = sprite.getHeight();
+    float spriteWidth = Nullability.castToNonnull(sprite).getWidth();
+    float spriteHeight = Nullability.castToNonnull(sprite).getHeight();
 
     particle.xScale = xScaleValue.newLowValue() / spriteWidth;
     particle.xScaleDiff = xScaleValue.newHighValue() / spriteWidth;
