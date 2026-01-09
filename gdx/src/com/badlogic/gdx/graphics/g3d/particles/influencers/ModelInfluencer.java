4725bc62ef2039170c92f1b04de434260afc24de
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 12:06:16 +0000

    Revert "fix: 138 - METHOD_NO_INIT - public ModelInfluencer() { - initializer method does not guarantee @NonNull field modelChannel (line 122) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 523a6ba6749fc9726154a4dc943a212e7db5a99d.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ModelInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ModelInfluencer.java
@@ -26,8 +26,6 @@ import com.badlogic.gdx.graphics.g3d.particles.ResourceData;
 import com.badlogic.gdx.graphics.g3d.particles.ResourceData.SaveData;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
-import edu.ucr.cs.riple.annotator.util.Nullability;
-import javax.annotation.Nullable;
 
 /**
  * It's an {@link Influencer} which controls which {@link Model} will be assigned to the particles
@@ -54,7 +52,6 @@ public abstract class ModelInfluencer extends Influencer {
 
     @Override
     public void init() {
-      allocateChannels();
       Model first = models.first();
       for (int i = 0, c = controller.emitter.maxParticleCount; i < c; ++i) {
         modelChannel.data[i] = new ModelInstance(first);
@@ -102,7 +99,6 @@ public abstract class ModelInfluencer extends Influencer {
 
     @Override
     public void activateParticles(int startIndex, int count) {
-      if (modelChannel == null) return;
       for (int i = startIndex, c = startIndex + count; i < c; ++i) {
         modelChannel.data[i] = pool.obtain();
       }
@@ -110,10 +106,9 @@ public abstract class ModelInfluencer extends Influencer {
 
     @Override
     public void killParticles(int startIndex, int count) {
-      if (modelChannel == null) return;
       for (int i = startIndex, c = startIndex + count; i < c; ++i) {
-        pool.free(Nullability.castToNonnull(modelChannel).data[i]);
-        Nullability.castToNonnull(modelChannel).data[i] = null;
+        pool.free(modelChannel.data[i]);
+        modelChannel.data[i] = null;
       }
     }
 
@@ -124,7 +119,7 @@ public abstract class ModelInfluencer extends Influencer {
   }
 
   public Array<Model> models;
-  @Nullable ObjectChannel<ModelInstance> modelChannel;
+  ObjectChannel<ModelInstance> modelChannel;
 
   public ModelInfluencer() {
     this.models = new Array<Model>(true, 1, Model.class);
