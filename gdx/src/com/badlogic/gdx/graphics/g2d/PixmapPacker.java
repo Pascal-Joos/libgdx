bb2e29f628695a8240e0ed09a34688f372026092
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 09:39:34 +0000

    Revert "fix: 45 - PASS_NULLABLE - atlas.getTextures().add(page.texture); - passing @Nullable parameter 'page.texture' where @NonNull is required"
    This reverts commit 79125989a47490c1996342269e4e70dcfea5228e.

--- gdx/src/com/badlogic/gdx/graphics/g2d/PixmapPacker.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PixmapPacker.java
@@ -30,7 +30,6 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.OrderedMap;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.regex.Matcher;
@@ -457,11 +456,7 @@ public class PixmapPacker implements Disposable {
           PixmapPackerRectangle rect = page.rects.get(name);
           TextureAtlas.AtlasRegion region =
               new TextureAtlas.AtlasRegion(
-                  Nullability.castToNonnull(page.texture),
-                  (int) rect.x,
-                  (int) rect.y,
-                  (int) rect.width,
-                  (int) rect.height);
+                  page.texture, (int) rect.x, (int) rect.y, (int) rect.width, (int) rect.height);
 
           if (rect.splits != null) {
             region.names = new String[] {"split", "pad"};
@@ -489,7 +484,7 @@ public class PixmapPacker implements Disposable {
           atlas.getRegions().add(region);
         }
         page.addedRects.clear();
-        atlas.getTextures().add(Nullability.castToNonnull(page.texture));
+        atlas.getTextures().add(page.texture);
       }
     }
   }
