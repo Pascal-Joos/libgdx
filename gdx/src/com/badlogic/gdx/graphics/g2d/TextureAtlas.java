a9282154bf6403cf8c5fdbfc9425da5f6e7adc7e
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:01:56 +0000

    Revert "fix: 61 - DEREFERENCE_NULLABLE - region.values = values.toArray(int[].class); - dereferenced expression values is @Nullable"
    This reverts commit 6a1a952d384c80abdb4a4eb9fa82f7d085ebe838.

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -32,7 +32,6 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.StreamUtils;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -500,7 +499,7 @@ public class TextureAtlas implements Disposable {
             }
             if (names != null && names.size > 0) {
               region.names = names.toArray(String.class);
-              region.values = Nullability.castToNonnull(values).toArray(int[].class);
+              region.values = values.toArray(int[].class);
               names.clear();
               values.clear();
             }
