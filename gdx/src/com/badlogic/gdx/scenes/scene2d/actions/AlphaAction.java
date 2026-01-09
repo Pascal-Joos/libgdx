585f66b15b87a862d85826880fbe932eef13c0c9
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 14:18:26 +0000

    fix: 223 - FIELD_NO_INIT - private @Null Color color; - @NonNull field color not initialized

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AlphaAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AlphaAction.java
@@ -19,6 +19,7 @@ package com.badlogic.gdx.scenes.scene2d.actions;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Null;
+import javax.annotation.Nullable;
 
 /**
  * Sets the alpha for an actor's color (or a specified color), from the current alpha to the new
@@ -29,7 +30,7 @@ import com.badlogic.gdx.utils.Null;
  */
 public class AlphaAction extends TemporalAction {
   private float start, end;
-  private @Null Color color;
+  @Nullable private @Null Color color;
 
   protected void begin() {
     if (color == null) color = target.getColor();
@@ -37,6 +38,7 @@ public class AlphaAction extends TemporalAction {
   }
 
   protected void update(float percent) {
+    if (color == null) color = target.getColor();
     if (percent == 0) color.a = start;
     else if (percent == 1) color.a = end;
     else color.a = start + (end - start) * percent;
@@ -47,6 +49,7 @@ public class AlphaAction extends TemporalAction {
     color = null;
   }
 
+  @Nullable
   public @Null Color getColor() {
     return color;
   }
