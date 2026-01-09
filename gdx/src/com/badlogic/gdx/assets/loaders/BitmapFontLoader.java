dbb432c425d6ad267b2a94c2290ea22b2fafd128
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 08:49:37 +0000

    Revert "fix: 15 - METHOD_NO_INIT - public BitmapFontLoader(FileHandleResolver resolver) { - initializer method does not guarantee @NonNull field data (line 49) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 43766e768c769a658ce083e3d4c25816a954e20e.

--- gdx/src/com/badlogic/gdx/assets/loaders/BitmapFontLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/BitmapFontLoader.java
@@ -30,7 +30,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -47,7 +46,7 @@ public class BitmapFontLoader
     super(resolver);
   }
 
-  @Nullable BitmapFontData data;
+  BitmapFontData data;
 
   @Override
   public Array<AssetDescriptor> getDependencies(
@@ -95,34 +94,22 @@ public class BitmapFontLoader
       String fileName,
       FileHandle file,
       @Nullable BitmapFontParameter parameter) {
-    if (parameter != null && parameter.bitmapFontData != null) {
-      data = parameter.bitmapFontData;
-    } else if (data == null) {
-      data = new BitmapFontData(file, parameter != null && parameter.flip);
-    }
-
-    if (parameter != null && parameter.atlasName != null && data != null) {
+    if (parameter != null && parameter.atlasName != null) {
       TextureAtlas atlas = manager.get(parameter.atlasName, TextureAtlas.class);
-      String name =
-          file.sibling(Nullability.castToNonnull(data).imagePaths[0])
-              .nameWithoutExtension()
-              .toString();
+      String name = file.sibling(data.imagePaths[0]).nameWithoutExtension().toString();
       AtlasRegion region = atlas.findRegion(name);
 
       if (region == null)
         throw new GdxRuntimeException(
             "Could not find font region " + name + " in atlas " + parameter.atlasName);
       return new BitmapFont(file, region);
-    } else if (data != null) {
-      BitmapFontData nonNullData = Nullability.castToNonnull(data);
-      int n = nonNullData.getImagePaths().length;
+    } else {
+      int n = data.getImagePaths().length;
       Array<TextureRegion> regs = new Array(n);
       for (int i = 0; i < n; i++) {
-        regs.add(new TextureRegion(manager.get(nonNullData.getImagePath(i), Texture.class)));
+        regs.add(new TextureRegion(manager.get(data.getImagePath(i), Texture.class)));
       }
-      return new BitmapFont(nonNullData, regs, true);
-    } else {
-      throw new GdxRuntimeException("BitmapFontData is null");
+      return new BitmapFont(data, regs, true);
     }
   }
 
