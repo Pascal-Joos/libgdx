132629151997a5a99cb18c74ec9186e4eeb9ec41
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:54:20 +0000

    Revert "fix: 101 - FIELD_NO_INIT - public String fileName; - @NonNull field fileName not initialized"
    This reverts commit 9427ace78a4f67eef8049702b7f408cdab28f8a8.

--- gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
@@ -54,7 +54,6 @@ import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.nio.Buffer;
 import javax.annotation.Nullable;
 
@@ -319,12 +318,11 @@ public class Model implements Disposable {
     if (mtl.textures != null) {
       for (ModelTexture tex : mtl.textures) {
         Texture texture;
-        String texFileName = Nullability.castToNonnull(tex.fileName);
-        if (textures.containsKey(texFileName)) {
-          texture = textures.get(Nullability.castToNonnull(tex.fileName));
+        if (textures.containsKey(tex.fileName)) {
+          texture = textures.get(tex.fileName);
         } else {
-          texture = textureProvider.load(texFileName);
-          textures.put(texFileName, texture);
+          texture = textureProvider.load(tex.fileName);
+          textures.put(tex.fileName, texture);
           disposables.add(texture);
         }
 
