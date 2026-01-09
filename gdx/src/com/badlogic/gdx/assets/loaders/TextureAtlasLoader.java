ec5ae62be83d390cd3f3d14c8fd018c85c8c52fd
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:03:15 +0000

    fix: 62 - FIELD_NO_INIT - public @Null FileHandle textureFile; - @NonNull field TextureAtlas$TextureAtlasData$Page.textureFile not initialized

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureAtlasLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureAtlasLoader.java
@@ -26,6 +26,7 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.TextureAtlasData;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.TextureAtlasData.Page;
 import com.badlogic.gdx.utils.Array;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -50,6 +51,7 @@ public class TextureAtlasLoader
       FileHandle file,
       @Nullable TextureAtlasParameter parameter) {
     for (Page page : data.getPages()) {
+      if (page.textureFile == null) continue;
       Texture texture =
           assetManager.get(page.textureFile.path().replaceAll("\\\\", "/"), Texture.class);
       page.texture = texture;
@@ -72,12 +74,14 @@ public class TextureAtlasLoader
 
     Array<AssetDescriptor> dependencies = new Array();
     for (Page page : data.getPages()) {
+      if (page.textureFile == null) continue;
       TextureParameter params = new TextureParameter();
       params.format = page.format;
       params.genMipMaps = page.useMipMaps;
       params.minFilter = page.minFilter;
       params.magFilter = page.magFilter;
-      dependencies.add(new AssetDescriptor(page.textureFile, Texture.class, params));
+      dependencies.add(
+          new AssetDescriptor(Nullability.castToNonnull(page.textureFile), Texture.class, params));
     }
     return dependencies;
   }
