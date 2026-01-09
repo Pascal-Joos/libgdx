f8b340a690298f4c99c67ec35252d843e7976d5c
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 18:55:25 +0000

    Revert "fix: 333 - UNBOX_NULLABLE - float fillX = c.fillX, fillY = c.fillY; - unboxing of a @Nullable value - c.fillX"
    This reverts commit 1eb51c41b9a7e0e903b2b5caa553502ab43ab87a.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -33,7 +33,6 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import javax.annotation.Nullable;
 
@@ -1193,7 +1192,7 @@ public class Table extends WidgetGroup {
 
       currentX += c.computedPadLeft;
 
-      float fillX = Nullability.castToNonnull(c.fillX), fillY = c.fillY;
+      float fillX = c.fillX, fillY = c.fillY;
       if (fillX > 0) {
         c.actorWidth = Math.max(spannedCellWidth * fillX, c.minWidth.get(c.actor));
         float maxWidth = c.maxWidth.get(c.actor);
