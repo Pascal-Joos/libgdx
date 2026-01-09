4c6a2e470c963204c72a7837490c21af260b10e7
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 09:06:53 +0000

    Revert "fix: 23 - DEREFERENCE_NULLABLE - instances.dispose(); - dereferenced expression instances is @Nullable"
    This reverts commit b9942ccca4e18de100db223e004031c2949f1d08.

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -242,7 +242,7 @@ public class Mesh implements Disposable {
   public Mesh disableInstancedRendering() {
     if (isInstanced) {
       isInstanced = false;
-      if (instances != null) instances.dispose();
+      instances.dispose();
       instances = null;
     }
     return this;
@@ -335,12 +335,7 @@ public class Mesh implements Disposable {
    * @param count the number of floats to update
    */
   public Mesh updateInstanceData(int targetOffset, float[] source, int sourceOffset, int count) {
-    if (instances != null) {
-      this.instances.updateInstanceData(targetOffset, source, sourceOffset, count);
-    } else {
-      throw new GdxRuntimeException(
-          "An InstanceBufferObject must be set before updating instance data!");
-    }
+    this.instances.updateInstanceData(targetOffset, source, sourceOffset, count);
     return this;
   }
 
@@ -364,12 +359,7 @@ public class Mesh implements Disposable {
    */
   public Mesh updateInstanceData(
       int targetOffset, FloatBuffer source, int sourceOffset, int count) {
-    if (instances != null) {
-      this.instances.updateInstanceData(targetOffset, source, sourceOffset, count);
-    } else {
-      throw new GdxRuntimeException(
-          "An InstanceBufferObject must be set before updating instance data!");
-    }
+    this.instances.updateInstanceData(targetOffset, source, sourceOffset, count);
     return this;
   }
 
@@ -762,7 +752,7 @@ public class Mesh implements Disposable {
       }
     } else {
       int numInstances = 0;
-      if (isInstanced && instances != null) numInstances = instances.getNumInstances();
+      if (isInstanced) numInstances = instances.getNumInstances();
 
       if (indices.getNumIndices() > 0) {
         if (count + offset > indices.getNumMaxIndices()) {
