483a803dc2a6d41d6b3aab559c1b77946a4f0378
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:41:35 +0000

    fix: 89 - DEREFERENCE_NULLABLE - plugs.get(group).beforeGroup(contents); - dereferenced expression plugs.get(group) is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
@@ -29,7 +29,9 @@ public abstract class PluggableGroupStrategy implements GroupStrategy {
 
   @Override
   public void beforeGroup(int group, Array<Decal> contents) {
-    plugs.get(group).beforeGroup(contents);
+    DecalRendererGroupPlug plug = plugs.get(group);
+    if (plug == null) return;
+    plug.beforeGroup(contents);
   }
 
   @Override
