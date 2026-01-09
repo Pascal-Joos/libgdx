8289ec9cbd04c700bb0184d51e9abc9480e0ed9f
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 10:18:02 +0000

    fix: 71 - ASSIGN_FIELD_NULLABLE - animation.id = anim.id; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
@@ -54,6 +54,7 @@ import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.nio.Buffer;
 import javax.annotation.Nullable;
 
@@ -468,11 +469,18 @@ public class Model implements Disposable {
     final int n = animations.size;
     Animation animation;
     if (ignoreCase) {
-      for (int i = 0; i < n; i++)
-        if ((animation = animations.get(i)).id.equalsIgnoreCase(id)) return animation;
+      for (int i = 0; i < n; i++) {
+        animation = animations.get(i);
+        if (animation == null || animation.id == null) return null;
+        if (Nullability.castToNonnull(animation.id).equalsIgnoreCase(id)) return animation;
+      }
     } else {
-      for (int i = 0; i < n; i++)
-        if ((animation = animations.get(i)).id.equals(id)) return animation;
+      for (int i = 0; i < n; i++) {
+        animation = animations.get(i);
+        if (animation == null || animation.id == null) return null;
+        if (Nullability.castToNonnull((animation = animations.get(i)).id).equals(id))
+          return animation;
+      }
     }
     return null;
   }
--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -28,6 +28,7 @@ import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.badlogic.gdx.utils.Pool;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -570,11 +571,18 @@ public class ModelInstance implements RenderableProvider {
     final int n = animations.size;
     Animation animation;
     if (ignoreCase) {
-      for (int i = 0; i < n; i++)
-        if ((animation = animations.get(i)).id.equalsIgnoreCase(id)) return animation;
+      for (int i = 0; i < n; i++) {
+        animation = animations.get(i);
+        if (animation == null || animation.id == null) return null;
+        if (Nullability.castToNonnull(animation.id).equalsIgnoreCase(id)) return animation;
+      }
     } else {
-      for (int i = 0; i < n; i++)
-        if ((animation = animations.get(i)).id.equals(id)) return animation;
+      for (int i = 0; i < n; i++) {
+        animation = animations.get(i);
+        if (animation == null || animation.id == null) return null;
+        if (Nullability.castToNonnull((animation = animations.get(i)).id).equals(id))
+          return animation;
+      }
     }
     return null;
   }
