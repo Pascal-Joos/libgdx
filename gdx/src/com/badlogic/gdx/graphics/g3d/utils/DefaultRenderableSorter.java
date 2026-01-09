f26531d05f42e14490effeaab75f385c8b88b409
Pascal-Joos <pascal.joos@outlook.de>
2026-01-09 12:53:25 +0000

    fix: 169 - DEREFERENCE_NULLABLE - (int) (1000f * camera.position.dst2(tmpV1)) - (int) (1000f * camera.position.dst2(tmpV2)); - dereferenced expression camera is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java
@@ -46,6 +46,7 @@ public class DefaultRenderableSorter implements RenderableSorter, Comparator<Ren
 
   @Override
   public int compare(final Renderable o1, final Renderable o2) {
+    if (camera == null) return 0;
     final boolean b1 =
         o1.material.has(BlendingAttribute.Type)
             && ((BlendingAttribute) o1.material.get(BlendingAttribute.Type)).blended;
