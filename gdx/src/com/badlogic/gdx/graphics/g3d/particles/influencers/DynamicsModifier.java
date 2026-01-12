b6651e660195fd94ea5434c4e6b784aeb207e50d
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 12:19:00 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field lifeChannel (line 579) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java:581   public DynamicsModifier() {}

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -27,6 +27,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -227,6 +228,9 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
+      if (lifeChannel == null) {
+        lifeChannel = controller.particles.addChannel(ParticleChannels.Life);
+      }
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               s = 0,
@@ -293,6 +297,10 @@ public abstract class DynamicsModifier extends Influencer {
       // Consider a channel which represent a simple angular momentum L
       // Proceed as Algorithm 2
 
+      if (lifeChannel == null) {
+        lifeChannel = controller.particles.addChannel(ParticleChannels.Life);
+      }
+
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               s = 0,
@@ -356,6 +364,9 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
+      if (lifeChannel == null) {
+        allocateChannels();
+      }
       float cx = 0, cy = 0, cz = 0;
       if (!isGlobal) {
         float[] val = controller.transform.val;
@@ -414,6 +425,7 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
+      if (lifeChannel == null) return;
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               s = 0,
@@ -479,6 +491,9 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
+      if (lifeChannel == null) {
+        return;
+      }
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               s = 0,
@@ -490,7 +505,7 @@ public abstract class DynamicsModifier extends Influencer {
               a += angularChannel.strideSize, l += lifeChannel.strideSize,
               positionOffset += positionChannel.strideSize) {
 
-        float lifePercent = lifeChannel.data[l],
+        float lifePercent = Nullability.castToNonnull(lifeChannel).data[l],
             strength =
                 strengthChannel.data[s + ParticleChannels.VelocityStrengthStartOffset]
                     + strengthChannel.data[s + ParticleChannels.VelocityStrengthDiffOffset]
@@ -549,6 +564,7 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
+      if (lifeChannel == null) return;
       int lifeOffset = ParticleChannels.LifePercentOffset, strengthOffset = 0, forceOffset = 0;
       for (int i = 0, c = controller.particles.size;
           i < c;
@@ -558,7 +574,8 @@ public abstract class DynamicsModifier extends Influencer {
         float strength =
             strengthChannel.data[strengthOffset + ParticleChannels.VelocityStrengthStartOffset]
                 + strengthChannel.data[strengthOffset + ParticleChannels.VelocityStrengthDiffOffset]
-                    * strengthValue.getScale(lifeChannel.data[lifeOffset]);
+                    * strengthValue.getScale(
+                        Nullability.castToNonnull(lifeChannel).data[lifeOffset]);
         TMP_V3
             .set(MathUtils.random(-1, 1f), MathUtils.random(-1, 1f), MathUtils.random(-1, 1f))
             .nor()
@@ -576,7 +593,7 @@ public abstract class DynamicsModifier extends Influencer {
   }
 
   public boolean isGlobal = false;
-  protected FloatChannel lifeChannel;
+  @Nullable protected FloatChannel lifeChannel;
 
   public DynamicsModifier() {}
 
