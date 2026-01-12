f2fbb971a115aa9064e1288fbf0a63206066f237
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 12:34:24 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field positionChannel (line 34) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java:40   @Initializer

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java
@@ -22,6 +22,8 @@ import com.badlogic.gdx.graphics.g3d.particles.ParticleChannels;
 import com.badlogic.gdx.graphics.g3d.particles.ParticleController;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
+import javax.annotation.Nullable;
 
 /**
  * It's an {@link Influencer} which updates the simulation of particles containing a {@link
@@ -31,7 +33,7 @@ import com.uber.nullaway.annotations.Initializer;
  * @author Inferno
  */
 public class ParticleControllerFinalizerInfluencer extends Influencer {
-  FloatChannel positionChannel, scaleChannel, rotationChannel;
+  @Nullable FloatChannel positionChannel, scaleChannel, rotationChannel;
   ObjectChannel<ParticleController> controllerChannel;
   boolean hasScale, hasRotation;
 
@@ -57,18 +59,30 @@ public class ParticleControllerFinalizerInfluencer extends Influencer {
 
   @Override
   public void update() {
+    if (positionChannel == null
+        || controllerChannel == null
+        || (hasScale && scaleChannel == null)
+        || (hasRotation && rotationChannel == null)) return;
     for (int i = 0, positionOffset = 0, c = controller.particles.size;
         i < c;
         ++i, positionOffset += positionChannel.strideSize) {
       ParticleController particleController = controllerChannel.data[i];
-      float scale = hasScale ? scaleChannel.data[i] : 1;
+      float scale = hasScale ? Nullability.castToNonnull(scaleChannel).data[i] : 1;
       float qx = 0, qy = 0, qz = 0, qw = 1;
       if (hasRotation) {
-        int rotationOffset = i * rotationChannel.strideSize;
-        qx = rotationChannel.data[rotationOffset + ParticleChannels.XOffset];
-        qy = rotationChannel.data[rotationOffset + ParticleChannels.YOffset];
-        qz = rotationChannel.data[rotationOffset + ParticleChannels.ZOffset];
-        qw = rotationChannel.data[rotationOffset + ParticleChannels.WOffset];
+        int rotationOffset = i * Nullability.castToNonnull(rotationChannel).strideSize;
+        qx =
+            Nullability.castToNonnull(rotationChannel)
+                .data[rotationOffset + ParticleChannels.XOffset];
+        qy =
+            Nullability.castToNonnull(rotationChannel)
+                .data[rotationOffset + ParticleChannels.YOffset];
+        qz =
+            Nullability.castToNonnull(rotationChannel)
+                .data[rotationOffset + ParticleChannels.ZOffset];
+        qw =
+            Nullability.castToNonnull(rotationChannel)
+                .data[rotationOffset + ParticleChannels.WOffset];
       }
       particleController.setTransform(
           positionChannel.data[positionOffset + ParticleChannels.XOffset],
