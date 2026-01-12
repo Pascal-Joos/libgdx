58a809738f727487a881a3f84ceaa23c8868a558
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 18:50:20 +0000

    fix: Type='PASS_NULLABLE', message='passing @Nullable parameter '(Class[]) null' where @NonNull is required' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/ReflectionPool.java:53       return ClassReflection.getConstructor(type, (Class[]) null);

--- gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
+++ gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
@@ -51,7 +51,7 @@ public class ReflectionPool<T> extends Pool<T> {
   @Nullable
   private @Null Constructor findConstructor(Class<T> type) {
     try {
-      return ClassReflection.getConstructor(type, (Class[]) null);
+      return ClassReflection.getConstructor(type, Nullability.castToNonnull((Class[]) null));
     } catch (Exception ex1) {
       try {
         Constructor constructor = ClassReflection.getDeclaredConstructor(type, (Class[]) null);
