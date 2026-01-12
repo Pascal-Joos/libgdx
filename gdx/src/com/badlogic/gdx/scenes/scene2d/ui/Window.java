81afc7ac102c8963dc8b0522b6a5adcc1e24e888
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 17:03:58 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression stage is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java:179               if (clampPosition && windowX + width + amountX > stage.getWidth())

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -177,15 +177,17 @@ public class Window extends Table {
             if ((edge & Align.right) != 0) {
               float amountX = x - lastX - width;
               if (width + amountX < minWidth) amountX = minWidth - width;
-              if (clampPosition && windowX + width + amountX > stage.getWidth())
-                amountX = stage.getWidth() - windowX - width;
+              if (clampPosition
+                  && windowX + width + amountX > Nullability.castToNonnull(stage).getWidth())
+                amountX = Nullability.castToNonnull(stage).getWidth() - windowX - width;
               width += amountX;
             }
             if ((edge & Align.top) != 0) {
               float amountY = y - lastY - height;
               if (height + amountY < minHeight) amountY = minHeight - height;
-              if (clampPosition && windowY + height + amountY > stage.getHeight())
-                amountY = stage.getHeight() - windowY - height;
+              if (clampPosition
+                  && windowY + height + amountY > Nullability.castToNonnull(stage).getHeight())
+                amountY = Nullability.castToNonnull(stage).getHeight() - windowY - height;
               height += amountY;
             }
             setBounds(
