fcf4f02a19250924b3535d4332c9ef001b92c118
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 11:06:47 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression managedTextures.get(Gdx.app) is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/Texture.java:368     return managedTextures.get(Gdx.app).size;

--- gdx/src/com/badlogic/gdx/graphics/Texture.java
+++ gdx/src/com/badlogic/gdx/graphics/Texture.java
@@ -366,6 +366,6 @@ public class Texture extends GLTexture {
    * @return the number of managed textures currently loaded
    */
   public static int getNumManagedTextures() {
-    return managedTextures.get(Gdx.app).size;
+    return Nullability.castToNonnull(managedTextures.get(Gdx.app)).size;
   }
 }
