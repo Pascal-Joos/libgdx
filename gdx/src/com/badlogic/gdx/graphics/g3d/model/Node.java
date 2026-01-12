9348687bec4bc75a6eeb71c0a693959772582561
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 11:47:47 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression (node = nodes.get(i)).id is @Nullable' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/model/Node.java:398       for (int i = 0; i < n; i++) if ((node = nodes.get(i)).id.equalsIgnoreCase(id)) return node;

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/Node.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/Node.java
@@ -395,9 +395,17 @@ public class Node {
     final int n = nodes.size;
     Node node;
     if (ignoreCase) {
-      for (int i = 0; i < n; i++) if ((node = nodes.get(i)).id.equalsIgnoreCase(id)) return node;
+      for (int i = 0; i < n; i++) {
+        node = nodes.get(i);
+        if (node == null || node.id == null) return null;
+        if (node.id.equalsIgnoreCase(id)) return node;
+      }
     } else {
-      for (int i = 0; i < n; i++) if ((node = nodes.get(i)).id.equals(id)) return node;
+      for (int i = 0; i < n; i++) {
+        node = nodes.get(i);
+        if (node == null || node.id == null) return null;
+        if (node.id.equals(id)) return node;
+      }
     }
     if (recursive) {
       for (int i = 0; i < n; i++)
