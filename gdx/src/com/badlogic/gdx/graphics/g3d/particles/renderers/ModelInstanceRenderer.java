717fa50e3f92150bc318eff4edb3cd025147a791
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 12:51:52 +0000

    fix: Type='FIELD_NO_INIT', message='@NonNull field modelInstanceChannel not initialized' /home/vscode/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ModelInstanceControllerRenderData.java:30   public ObjectChannel<ModelInstance> modelInstanceChannel;

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ModelInstanceRenderer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ModelInstanceRenderer.java
@@ -64,6 +64,7 @@ public class ModelInstanceRenderer
 
   @Override
   public void update() {
+    if (renderData.modelInstanceChannel == null) return;
     for (int i = 0, positionOffset = 0, c = controller.particles.size;
         i < c;
         ++i, positionOffset += renderData.positionChannel.strideSize) {
